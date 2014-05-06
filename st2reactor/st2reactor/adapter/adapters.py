45a34d56750fccbe8184d427cc507fbc1e4e24da
Manas Kelshikar <manas@stackstorm.com>
2014-05-06 11:04:47 -0700

    [STORM-15] Use logging instead of print * Setup python logging for reactor. * etc/kandra.conf is now the global config file. * Location of the logging config file is picked from the global config. * Going forward each component will come with its own logging configuration. * Assumed some basic elements of logging infrastructure viz log file location, formatters etc. * Replaced all occurences of print with LOG.
    Closes: STORM-15

--- st2reactor/st2reactor/adapter/adapters.py
+++ st2reactor/st2reactor/adapter/adapters.py
@@ -1,9 +1,11 @@
 import eventlet
+import logging
 import random
 import thread
 
 from st2reactor.adapter import AdapterBase
 
+LOG = logging.getLogger('st2reactor.adapter.adapters')
 
 class FixedRunAdapter(AdapterBase):
     def __init__(self, iterations=10):
@@ -13,7 +15,7 @@ class FixedRunAdapter(AdapterBase):
         count = 0
         while self.__iterations > count:
             count += 1
-            print "[{0}] iter: {1}".format(thread.get_ident(), count)
+            LOG.info("[{0}] iter: {1}".format(thread.get_ident(), count))
             eventlet.sleep(random.randint(1,100)*0.01)
 
     def stop(self):
