32611de1791e29a7059c88ec083e5c4f99d8ffca
Manas Kelshikar <manas@stackstorm.com>
2014-05-07 12:42:20 -0700

    DB integration * Config and connection to db. * API to access reactor models.

--- st2reactor/st2reactor/config.py
+++ st2reactor/st2reactor/config.py
@@ -8,6 +8,13 @@ logging_opts = [
 ]
 CONF.register_opts(logging_opts, group='reactor_logging')
 
+db_opts = [
+    cfg.StrOpt('host', default='0.0.0.0', help='host of db server'),
+    cfg.IntOpt('port', default=27017, help='port of db server'),
+    cfg.StrOpt('db_name', default='kandra', help='name of database')
+]
+CONF.register_opts(db_opts, group='database')
+
 use_debugger = cfg.BoolOpt(
     'use-debugger', default=False,
     help='Enables debugger. Note that using this option changes how the '
