b88cae5ec3668d5f6541565da9c4a45134e19216
Michael Richmond <mar@stackstorm.com>
2014-05-09 10:25:54 -0700

    Updating StactionController DB models.
    * Adding URI field to BaseDB object.
    * Fleshing out StactionDB object.
    * Fleshing out StactionExecutionDB object.

--- st2common/st2common/models/db/stactioncontroller.py
+++ st2common/st2common/models/db/stactioncontroller.py
@@ -2,14 +2,14 @@ import mongoengine as me
 
 from st2common.models.db.stormbase import BaseDB
 
+__all__ = ['StactionDB',
+           'StactionExecution',
+           ]
+
 class StactionDB(BaseDB):
     """The system entity that represents a Stack Action/Automation in
        the system.
     Attribute:
-        name : name of the entity.
-        description : description of the entity.
-        id : unique identifier for the entity. If none is provided it
-        will be auto generate by the system.
         enabled: flag indicating whether this staction is enabled in the system.
         repo_path: relative path to the staction artifact. Relative to the root
                    of the repo.
@@ -17,7 +17,40 @@ class StactionDB(BaseDB):
         parameter_names: flat list of strings required as key names when running
                    the staction.
     """
-    enabled = me.BooleanField()
-    repo_path = me.StringField(required=True)
-    run_type = me.StringField()
-    parameter_names = me.ListField()
+    enabled = me.BooleanField(required=True, default=True, description=u'Flag indicating whether the staction is enabled.')
+    repo_path = me.StringField(required=True), description=u'Path to staction content relative to repository base.')
+    run_type = me.StringField(required=True, description=u'Execution environment to use when invoking the staction.')
+    parameter_names = me.ListField(required=True, description=u'List of required parameter names.')
+
+class StactionExecutionDB(BaseDB):
+    """
+        The databse entity that represents a Stack Action/Automation in
+        the system.
+
+        Attributes:
+            status: the most recently observed status of the execution.
+                    One of "starting", "running", "completed", "error".
+            result: an embedded document structure that holds the 
+                    output and exit status code from the stack action.
+    """
+    status = me.fields.StringField(required=True)
+    # Initially deny any delete request that will leave a staction_execution in
+    # the DB without an assocaited staction. The constraint might be relaxed to
+    # "NULLIFY" if we implement the right handling in stactioncontroller.
+    staction = me.fields.ReferenceField(StactionDB, reverse_delete_rule=DENY,
+                description=u'The staction executed by this instance.')
+    target = me.fields.StringField(required=True, default=NULL
+                description=u'The target selection string.')
+    parameters = me.fields.DictField(required=True, default={},
+                description=u'The key-value pairs passed as parameters to the execution.')
+#    TODO: Determine whether I need to store the execution result values.
+#    result = me.fields.EmbeddedDocumentField(ExecutionResultDB, **kwargs)
+
+class StactionExecutionResultDB(me.EmbeddedDocument):
+    """
+    TODO: fill-in
+    Not sure if I will need this to be persisted.
+    """
+    exit_code = me.fields.IntField()
+    std_out = me.fields.StringField()
+    std_err = me.fields.StringField()
