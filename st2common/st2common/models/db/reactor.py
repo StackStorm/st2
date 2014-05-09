3ee65664d4747a728ad514ced05154fa89029a57
Michael Richmond <mar@stackstorm.com>
2014-05-09 11:01:23 -0700

    Fixing imports after renaming a model.

--- st2common/st2common/models/db/reactor.py
+++ st2common/st2common/models/db/reactor.py
@@ -1,7 +1,7 @@
 import mongoengine as me
 from st2common.models.db import MongoDBAccess
 from st2common.models.db.stormbase import BaseDB
-from st2common.models.db.stactioncontroller import StactionDB
+from st2common.models.db.staction import StactionDB
 
 
 class TriggerSourceDB(BaseDB):
