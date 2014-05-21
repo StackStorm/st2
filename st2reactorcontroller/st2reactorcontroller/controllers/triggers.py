c6497bcc2cead4f2ccd66877cb17579d723a2f6b
Manas Kelshikar <manas@stackstorm.com>
2014-05-20 17:25:48 -0700

    Reactor controller skeleton
    - Pecan app with stub controller methods
    - Server startup script
    - Centralized configuration
    - Rename etc -> conf

---
+++ st2reactorcontroller/st2reactorcontroller/controllers/triggers.py
@@ -0,0 +1,91 @@
+from pecan import expose
+from pecan.rest import RestController
+
+
+class TriggerController(RestController):
+    """
+        Implements the RESTful web endpoint that handles
+        the lifecycle of StactionExecutions in the system.
+    """
+
+    @expose('json')
+    def get_one(self, id):
+        """
+            List stactionexecution by id.
+
+            Handle:
+                GET /stactionexecutions/1
+        """
+        return {"dummy": "execution_value"}
+
+    @expose('json')
+    def get_all(self):
+        """ 
+            List all stactionexecutions.
+
+            Handles requests:
+                GET /stactionexecutions/
+        """
+        return {"dummy": "get_all"}
+
+    @expose('json')
+    def post(self, **kwargs):
+        """ 
+            Create a new stactionexecution.
+
+            Handles requests:
+                POST /stactionexecutions/
+        """
+        print kwargs
+        return {"dummy": "post"}
+
+    @expose('json')
+    def put(self, id, **kwargs):
+        # TODO: Update probably does not make any sense on an execution.
+        """
+            Update a stactionexecution.
+
+            Handles requests:
+                POST /stactionexecutions/1?_method=put
+                PUT /stactionexecutions/1
+        """
+        return {"dummy": "put"}
+
+    @expose('json')
+    def delete(self, id):
+        """ 
+            Delete a stactionexecution.
+
+            Handles requests:
+                POST /stactionexecutions/1?_method=delete
+                DELETE /stactionexecutions/1
+        """
+        # TODO: Delete should migrate the execution data to a history collection.
+        return {"dummy": "delete stactionexecution"}
+
+
+class TriggerInstanceController(RestController):
+    """
+        Implements the RESTful web endpoint that handles
+        the lifecycle of StactionExecutions in the system.
+    """
+
+    @expose('json')
+    def get_one(self, id):
+        """
+            List stactionexecution by id.
+
+            Handle:
+                GET /stactionexecutions/1
+        """
+        return {"dummy": "execution_value"}
+
+    @expose('json')
+    def get_all(self):
+        """
+            List all stactionexecutions.
+
+            Handles requests:
+                GET /stactionexecutions/
+        """
+        return {"dummy": "get_all"}
