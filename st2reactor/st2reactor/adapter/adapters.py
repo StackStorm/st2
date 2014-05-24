e9d5eb015c24a791377f0f328ea8e7397a8a148d
Manas Kelshikar <manas@stackstorm.com>
2014-05-23 17:11:43 -0700

    Move bootstrap to st2reactor module - adapter_container is starting to do more than expected. - moved all db init, adapter load and container start code to the st2reactor module. - validated the startup script.

--- st2reactor/st2reactor/adapter/adapters.py
+++ st2reactor/st2reactor/adapter/adapters.py
@@ -7,6 +7,7 @@ from st2reactor.adapter import AdapterBase
 
 LOG = logging.getLogger('st2reactor.adapter.adapters')
 
+
 class FixedRunAdapter(AdapterBase):
     def __init__(self, iterations=10):
         self.__iterations = iterations
@@ -16,7 +17,7 @@ class FixedRunAdapter(AdapterBase):
         while self.__iterations > count:
             count += 1
             LOG.info("[{0}] iter: {1}".format(thread.get_ident(), count))
-            eventlet.sleep(random.randint(1,100)*0.01)
+            eventlet.sleep(random.randint(1, 100)*0.01)
 
     def stop(self):
         pass
