1d9edf51d77bd39b9a0d662647f721da1b93651e
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-06-13 12:08:15 -0700

    Add: Update demo samples to implement setup method; add dispatcher dependency

--- st2reactor/tests/test_container.py
+++ st2reactor/tests/test_container.py
@@ -4,18 +4,23 @@ from st2reactor.container.base import SensorContainer
 
 class ContainerTest(unittest2.TestCase):
 
-    def test_sensor_start(self):
+    def test_sensor_methods(self):
         """
         Verify start of sensors is called.
         """
         class RunTestSensor(object):
             start_call_count = 0
+            setup_call_count = 0
+            stop_call_count = 0
+
+            def setup(self):
+                RunTestSensor.setup_call_count += 1
 
             def start(self):
                 RunTestSensor.start_call_count += 1
 
             def stop(self):
-                pass
+                RunTestSensor.stop_call_count += 1
 
             def get_trigger_types(self):
                 return [
@@ -27,4 +32,11 @@ class ContainerTest(unittest2.TestCase):
         container.run()
         self.assertEqual(RunTestSensor.start_call_count, len(sensor_modules),
                          'Not all Sensor.start called.')
+        self.assertEqual(RunTestSensor.setup_call_count, len(sensor_modules),
+                         'Not all Sensor.setup called.')
+
+        # Now invoke shutdown and see if stop() method on sensors were called.
+        container.shutdown()
+        self.assertEqual(RunTestSensor.stop_call_count, len(sensor_modules),
+                         'Not all Sensor.stop called.')
 
