abadced68f0e99eec43d9df3aae63de2184a4a06
Michael Richmond <mar@stackstorm.com>
2014-06-17 16:44:18 -0700

    [STORM-5] Implement Action Runner * Working on actionrunner controller and passing in fields to POST call.

--- st2common/st2common/models/api/actionrunner.py
+++ st2common/st2common/models/api/actionrunner.py
@@ -1,24 +1,34 @@
 from wsme import types as wstypes
 
-from st2common.models.api.stormbase import StormBaseAPI
+from st2common.models.api.stormbase import (StormFoundationAPI, StormBaseAPI)
 
 __all__ = ['LiveActionAPI',
            'ActionRunnerAPI',
            ]
 
 
-class LiveActionAPI(StormBaseAPI):
+class LiveActionAPI(StormFoundationAPI):
     """The system entity that represents an Action process running in
        an ActionRunner execution environment.
     Attribute:
        pid: the OS process id for the LiveAction process. 
     """
-    pid = wstypes.text
+#    action_name = wstypes.text
+#    runner_parameters = wstypes.DictType(str, str)
+#    action_parameters = wstypes.DictType(str, str)
 
     @classmethod
-    def from_model(cls, model):
-        action_runner = cls()
-        action_runner.id = str(model.id)
+    def from_model(kls, model):
+        live_action = StormFoundationAPI.from_model(kls, model)
+#        live_action.action_name = model.action_name
+#        live_action.runner_parameters = dict(model.runner_parameters)
+#        live_action.action_parameters = dict(model.action_parameters)
+        return live_action
+
+    @classmethod
+    def to_model(kls, model):
+        liveaction = StormFoundationAPI.to_model(LiveActionDB, model)
+        return liveaction
 
 
 class ActionRunnerAPI(StormBaseAPI):
