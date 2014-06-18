7351ece43b3f88ac0711b745783296af905752a6
Michael Richmond <mar@stackstorm.com>
2014-06-17 20:20:17 -0700

    [STORM-5] Implement Action Runner * Adding StackStorm base exception classes. * Adding DB/Persistence object not found exception class.

---
+++ st2common/st2common/exceptions/__init__.py
@@ -0,0 +1,26 @@
+
+class StackStormBaseException(Exception):
+    """
+        The root of the exception class hierarchy for all
+        StackStorm server exceptions.
+
+        For exceptions raised by plug-ins, see StackStormPluginException
+        class.
+    """
+    def __init__(self, msg=''):
+        self.message = msg
+
+
+class StackStormPluginException(StackStormBaseException):
+    """
+        The root of the exception class hierarchy for all
+        exceptions that are defined as part of a StackStorm
+        plug-in API.
+
+        It is recommended that each API define a root exception
+        class for the API. This root exception class for the
+        API should inherit from the StackStormPluginException
+        class.
+    """
+    def __init__(self, msg=''):
+        self.message = msg
---
+++ st2common/st2common/exceptions/db.py
@@ -0,0 +1,4 @@
+from st2common.exceptions import StackStormBaseException
+
+class StackStormDBObjectNotFoundError(StackStormBaseException):
+    pass
