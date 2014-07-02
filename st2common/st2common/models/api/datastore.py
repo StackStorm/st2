320d143052f60eff717afaef8e4d4924170aa73e
Winson Chan <m4d.coder@gmail.com>
2014-07-02 16:43:32 -0700

    [STORM-250] Key Value Store - REST API
    Implemented REST controller for the key value store.

---
+++ st2common/st2common/models/api/datastore.py
@@ -0,0 +1,22 @@
+from wsme import types as wstypes
+
+from st2common.models.api.stormbase import StormBaseAPI
+from st2common.models.db.datastore import KeyValuePairDB
+from st2common.persistence.datastore import KeyValuePair
+
+
+class KeyValuePairAPI(StormBaseAPI):
+
+    value = wstypes.text
+
+    @classmethod
+    def from_model(cls, model):
+        kvp = StormBaseAPI.from_model(cls, model)
+        kvp.value = model.value
+        return kvp
+
+    @classmethod
+    def to_model(cls, kvp):
+        model = StormBaseAPI.to_model(KeyValuePairDB, kvp)
+        model.value = kvp.value
+        return model
--- st2datastore/st2datastore/controllers/root.py
+++ st2datastore/st2datastore/controllers/root.py
@@ -1,8 +1,12 @@
 from pecan import expose
 from webob.exc import status_map
 
+from st2datastore.controllers import datastore
+
 
 class RootController(object):
+
+    keys = datastore.KeyValuePairController()
 
     @expose(generic=True, template='index.html')
     def index(self):
