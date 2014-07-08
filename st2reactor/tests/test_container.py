64194e1bd00ce8da697c5eafa96c551896a8aaf8
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-07-08 15:11:13 -0700

    Add: Test cases for adding/removing sensors from container

--- st2reactor/tests/test_container.py
+++ st2reactor/tests/test_container.py
@@ -1,41 +1,71 @@
-import unittest2
 from st2reactor.container.base import SensorContainer
+from st2tests.base import EventletTestCase
 
 
-class ContainerTest(unittest2.TestCase):
+class ContainerTest(EventletTestCase):
+    class RunTestSensor(object):
+        def __init__(self):
+            self.start_call_count = 0
+            self.setup_call_count = 0
+            self.stop_call_count = 0
 
-    def test_sensor_methods(self):
-        """
-        Verify start of sensors is called.
-        """
-        class RunTestSensor(object):
-            start_call_count = 0
-            setup_call_count = 0
-            stop_call_count = 0
+        def setup(self):
+            self.setup_call_count += 1
 
-            def setup(self):
-                RunTestSensor.setup_call_count += 1
+        def start(self):
+            self.start_call_count += 1
 
-            def start(self):
-                RunTestSensor.start_call_count += 1
+        def stop(self):
+            self.stop_call_count += 1
 
-            def stop(self):
-                RunTestSensor.stop_call_count += 1
+        def get_trigger_types(self):
+            return [
+                {'name': 'st2.dummy.t1', 'description': 'some desc', 'payload_info': ['a', 'b']}
+            ]
 
-            def get_trigger_types(self):
-                return [
-                    {'name': 'st2.dummy.t1', 'description': 'some desc', 'payload_info': ['a', 'b']}
-                ]
-
-        sensor_modules = [RunTestSensor(), RunTestSensor()]
+    def test_sensor_methods_are_called(self):
+        sensor_modules = [ContainerTest.RunTestSensor(), ContainerTest.RunTestSensor()]
         container = SensorContainer(sensor_instances=sensor_modules)
         container.run()
-        self.assertEqual(RunTestSensor.start_call_count, len(sensor_modules),
-                         'Not all Sensor.start called.')
-        self.assertEqual(RunTestSensor.setup_call_count, len(sensor_modules),
-                         'Not all Sensor.setup called.')
+
+        for sensor in sensor_modules:
+            self.assertEqual(sensor.setup_call_count, 1, 'Sensor.setup() not called.')
+            self.assertEqual(sensor.start_call_count, 1, 'Sensor.start() not called.')
 
         # Now invoke shutdown and see if stop() method on sensors were called.
         container.shutdown()
-        self.assertEqual(RunTestSensor.stop_call_count, len(sensor_modules),
-                         'Not all Sensor.stop called.')
+        for sensor in sensor_modules:
+            self.assertEqual(sensor.stop_call_count, 1, 'Sensor.stop() not called.')
+
+        # Check count of running sensors in container is zero.
+        self.assertEqual(container.running(), 0, 'Leak in container.')
+
+    def test_dynamic_add_remove_sensors(self):
+        container = SensorContainer()
+        container.run()
+
+        # Add a new sensor to container.
+        test_sensor = ContainerTest.RunTestSensor()
+        ret = container.add_sensor(test_sensor)
+        self.assertEqual(ret, True, "Container must accept a new instance.")
+
+        # Test duplicate add.
+        ret = container.add_sensor(test_sensor)
+        self.assertEqual(ret, False, "Container must have rejected instance.")
+
+        # Remove the sensor added.
+        ret = container.remove_sensor(test_sensor)
+        self.assertEqual(ret, True, "Container must have removed this instance.")
+
+        # Remove a sensor that doesn't exist.
+        other_test_sensor = ContainerTest.RunTestSensor()
+        ret = container.remove_sensor(other_test_sensor)
+        self.assertEqual(ret, False, "Container shouldn\'t' have removed this instance.")
+
+        # Wait until the greenthreads finish.
+        container._pool.waitall()
+        # Test that sensor methods were invoked.
+        self.assertEqual(test_sensor.setup_call_count, 1, 'Sensor.setup() not called.')
+        self.assertEqual(test_sensor.start_call_count, 1, 'Sensor.start() not called.')
+        self.assertEqual(test_sensor.stop_call_count, 1, 'Sensor.stop() not called.')
+        container.shutdown()
