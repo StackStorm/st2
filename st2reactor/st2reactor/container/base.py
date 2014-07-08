4dd218734cc88f3bcc144c9fcacd49e8d3d68423
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-07-08 15:10:38 -0700

    Add: Ability to dynamically add/remove sensors from container

--- st2reactor/st2reactor/container/base.py
+++ st2reactor/st2reactor/container/base.py
@@ -19,16 +19,18 @@ LOG = logging.getLogger('st2reactor.sensorcontainer')
 
 class SensorContainer(object):
     def __init__(self, sensor_instances=[]):
-        self.__pool = eventlet.GreenPool(len(sensor_instances))
-        self.__sensors = sensor_instances
-        self.__threads = {}
+        self._pool_size = len(sensor_instances)
+        self._pool = eventlet.GreenPool(self._pool_size)
+        self._sensors = sensor_instances
+        self._threads = {}
         LOG.info('Container setup to run %d sensors.' % len(sensor_instances))
 
     def _run_sensor(self, sensor):
         try:
             sensor.setup()
         except Exception, e:
-            LOG.error('Error calling setup on sensor: %s. Exception: %s', sensor.__class__, e)
+            LOG.error('Error calling setup on sensor: %s. Exception: %s',
+                      sensor.__class__.__name__, e)
         else:
             sensor.start()
 
@@ -36,23 +38,62 @@ class SensorContainer(object):
         try:
             sensor.stop()
         except Exception, e:
-            LOG.error('Error cleaning up sensor: %s. Exception: %s', sensor.__class__, e)
+            LOG.error('Error cleaning up sensor: %s. Exception: %s',
+                      sensor.__class__.__name__, e)
+
+    def _spawn_sensor_in_thread(self, sensor):
+        try:
+            gt = self._pool.spawn(self._run_sensor, sensor)
+            self._threads[sensor] = gt
+            eventlet.sleep(0)
+        except Exception as e:
+            LOG.error('Pool doesn\'t have enough threads to run sensor: %s. Exception: %s',
+                      sensor.__class__, e)
+
+    def _run_all_sensors(self):
+        for sensor in self._sensors:
+            LOG.info('Running sensor %s' % sensor.__class__.__name__)
+            self._spawn_sensor_in_thread(sensor)
+
+    def add_sensor(self, sensor):
+        if sensor in self._sensors:
+            LOG.warning('Sensor %s already exists and running.', sensor.__class__.__name__)
+            return False
+        else:
+            self._pool_size += 1
+            self._pool.resize(self._pool_size)
+            self._spawn_sensor_in_thread(sensor)
+            self._sensors.append(sensor)
+
+        return True
+
+    def remove_sensor(self, sensor):
+        if sensor not in self._sensors:
+            LOG.warning('Sensor %s isn\'t running in this container.', sensor.__class__.__name__)
+            return False
+        else:
+            self._threads[sensor].kill()
+            self._sensor_cleanup(sensor)
+            self._sensors.remove(sensor)
+            self._pool_size -= 1
+            self._pool.resize(self._pool_size)
+
+        return True
+
+    def run(self):
+        self._run_all_sensors()
+        self._pool.waitall()
+        LOG.info('Container has no active sensors running.')
+        return SUCCESS_EXIT_CODE
+
+    def running(self):
+        return len(self._sensors)
 
     def shutdown(self):
         LOG.info('Container shutting down. Invoking cleanup on sensors.')
-        for sensor, gt in self.__threads.iteritems():
+        for sensor, gt in self._threads.iteritems():
             gt.kill()
             self._sensor_cleanup(sensor)
         LOG.info('All sensors are shut down.')
-
-    def run(self):
-        for sensor in self.__sensors:
-            LOG.info('Running sensor %s' % sensor.__class__.__name__)
-            gt = self.__pool.spawn(self._run_sensor, sensor)
-            self.__threads[sensor] = gt
-        self.__pool.waitall()
-
-    def main(self):
-        self.run()
-        LOG.info('Container has no active sensors running.')
-        return SUCCESS_EXIT_CODE
+        self._sensors = {}
+        self._threads = {}
