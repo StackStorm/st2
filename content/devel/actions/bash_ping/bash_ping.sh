09c4aab3e34daffc21f79854eb30f2edd401041a
Michael Richmond <mar@stackstorm.com>
2014-07-11 11:01:39 -0700

    [STORM-271] Test and verify shell runner environment access
    * Updated ping action to use an optional "count" argument.
    * Updated development create script to specify count argument.

--- content/devel/actions/bash_ping/bash_ping.sh
+++ content/devel/actions/bash_ping/bash_ping.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ping -c 3 ${1}
+if [[ ! $count ]]; then
+    # count variable not set so default to 3
+    PING_COUNT=3
+else
+    PING_COUNT=${count}
+fi
+
+ping -c ${PING_COUNT} ${1}
