553f27fa06a0fd8033b28e5dbd756077d69d3a7a
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-07-21 14:39:58 -0700

    Introduce Paramiko SSH runner   * [Add] Abstractions viz SSHCommand, RemoteAction, RemoteScriptAction   * [Add] Abstractions viz ParamikoSSHCommand, FabricRemoteAction, FabricRemoteScriptAction   * [Add] A Paramiko SSH client   * [Fix] Refactor common classes to st2common

---
+++ st2actionrunnercontroller/st2actionrunner/runners/paramiko_runner.py
@@ -0,0 +1,62 @@
+import uuid
+
+from st2common import log as logging
+from st2common.util.ssh import SSHClient
+from st2common.models.system.action import ParamikoSSHCommandAction
+
+LOG = logging.getLogger('st2.actions.runner.paramiko_runner')
+
+
+class SSHRunner(object):
+    def __init__(self, id):
+        self.runner_id = id
+
+    def run(self, ssh_action):
+        LOG.info('Executing action via SSHRunner :%s for user: %s.' %
+              (self.runner_id, ssh_action.get_user()))
+        LOG.info('Action info:: name: %s, Id: %s, command: %s, actual user: %s' % (ssh_action.name,
+              ssh_action.id, ssh_action.get_command(), ssh_action.get_user()))
+
+        results = {}
+        for host in ssh_action.get_hosts():
+            result = {'succeeded': True, 'failed': False}
+            try:
+                ssh_client = self._get_ssh_client(host, user=ssh_action.get_user(),
+                                                  pkey=ssh_action.get_pkey(),
+                                                  password=ssh_action.get_password())
+
+                result_stdout, result_stderr, ret_code = ssh_client.execute_sync(
+                    ssh_action.get_command(),
+                    sudo=remote_action.is_sudo())
+
+                result['stdout'] = result_stdout
+                result['stderr'] = result_stderr
+                result['return_code'] = ret_code
+
+                if ret_code != 0:
+                    result['succeeded'] = False
+                    result['failed'] = True
+            except Exception as e:
+                LOG.error('Exception: %s performing ssh action: %s', e, ssh_action)
+                result['failed'] = True
+            finally:
+                results[host] = result
+        return results
+
+    def _get_ssh_client(self, host, user=None, pkey=None, password=None):
+        if pkey is not None:
+            return SSHClient(host, user=user, pkey=pkey)
+        return SSHClient(host, user=user, password=password)
+
+
+# XXX: Write proper tests.
+if __name__ == '__main__':
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print('!!!!!!!!!!!!!!!!!!!!! NORMAL CMD !!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    runner = SSHRunner(str(uuid.uuid4()))
+    remote_action = ParamikoSSHCommandAction('UNAME', 'action_exec_id' + str(uuid.uuid4()), 'unam -a',
+                                             'lakshmi', hosts=['54.191.85.86', '54.200.102.55'],
+                                             sudo=True)
+    results = runner.run(remote_action)
+    print(results)
---
+++ st2common/st2common/models/system/action.py
@@ -0,0 +1,155 @@
+import os
+
+from fabric.api import (put, run, sudo)
+from fabric.tasks import WrappedCallableTask
+
+# XXX: This should be read from the config file.
+# Set the default user for remote actions.
+ENV_USER = 'lakshmi'
+
+
+class SSHCommandAction(object):
+    def __init__(self, name, action_exec_id, command, user, password=None, pkey=None, hosts=None,
+                 parallel=True, sudo=False):
+        self.name = name
+        self.command = command
+        self.id = action_exec_id
+        self.hosts = hosts
+        self.parallel = parallel
+        self.sudo = sudo
+        self.user = user
+        self.pkey = pkey
+        self.password = password
+
+    def is_parallel(self):
+        return self.parallel
+
+    def is_sudo(self):
+        return self.sudo
+
+    def get_user(self):
+        return self.user
+
+    def get_hosts(self):
+        return self.hosts
+
+    def is_pkey_authentication(self):
+        return self.pkey is not None
+
+    def get_pkey(self):
+        return self.pkey
+
+    def get_password(self):
+        return self.password
+
+    def get_command(self):
+        return self.command
+
+    # XXX: needs a __str__ method
+
+
+class RemoteAction(SSHCommandAction):
+    def __init__(self, name, action_exec_id, command, on_behalf_user, hosts=None, parallel=True,
+                 sudo=False):
+        super(RemoteAction, self).__init__(name, action_exec_id, command, ENV_USER,
+                                           hosts=hosts, parallel=parallel, sudo=sudo)
+        self.on_behalf_user = on_behalf_user  # Used for audit purposes.
+
+    def get_on_behalf_user(self):
+        return self.user
+
+    # XXX: needs a __str__ method
+
+
+class RemoteScriptAction(RemoteAction):
+    def __init__(self, name, action_exec_id, script_local_path_abs,
+                 on_behalf_user, remote_dir=None, hosts=None, parallel=True, sudo=False):
+        super(RemoteScriptAction, self).__init__(name, action_exec_id, '', ENV_USER,
+                                           hosts=hosts, parallel=parallel, sudo=sudo)
+        self.script_local_path_abs = script_local_path_abs
+        self.script_local_dir, self.script_name = os.path.split(self.script_local_path_abs)
+
+        self.remote_dir = '/tmp'
+        if remote_dir is not None:
+            self.remote_dir = remote_dir
+        self.command = os.path.join(self.remote_dir, self.script_name)
+
+
+class ParamikoSSHCommandAction(SSHCommandAction):
+    pass
+
+
+class FabricRemoteAction(RemoteAction):
+    def get_fabric_task(self):
+        action_method = self._get_action_method()
+        return WrappedCallableTask(action_method, name=self.name, alias=self.id,
+            parallel=self.parallel, sudo=self.sudo)
+
+    def _get_action_method(self):
+        if (self.sudo):
+            return self._sudo
+        return self._run
+
+    def _run(self):
+            output = run(self.command, combine_stderr=False, pty=False, quiet=True)
+            result = {
+                'stdout': output.stdout,
+                'stderr': output.stderr,
+                'return_code': output.return_code,
+                'succeeded': output.succeeded,
+                'failed': output.failed
+            }
+            return result
+
+    def _sudo(self):
+            output = sudo(self.command, combine_stderr=False, pty=True, quiet=True)
+            result = {
+                'stdout': output.stdout,
+                'stderr': output.stderr,
+                'return_code': output.return_code,
+                'succeeded': output.succeeded,
+                'failed': output.failed
+            }
+
+            # XXX: For sudo, fabric requires to set pty=True. This basically combines stdout and
+            # stderr into a single stdout stream. So if the command fails, we explictly set stderr
+            # to stdout and stdout to ''.
+            if result['failed']:
+                result['stderr'] = result['stdout']
+                result['stdout'] = ''
+
+            return result
+
+
+class FabricRemoteScriptAction(RemoteScriptAction, FabricRemoteAction):
+    def get_fabric_task(self):
+        action_method = self._get_script_action_method()
+        return WrappedCallableTask(action_method, name=self.name, alias=self.id,
+            parallel=self.parallel, sudo=self.sudo)
+
+    def _get_script_action_method(self):
+        return WrappedCallableTask(self._run_script, name=self.name, alias=self.id,
+            parallel=self.parallel, sudo=self.sudo)
+
+    def _run_script(self):
+        output_put = self._put()
+        if output_put.get('failed'):
+            return output_put
+        action_method = self._get_action_method()
+        return action_method()
+
+    def _put(self):
+        output = put(self.script_local_path_abs, self.remote_dir, use_sudo=self.sudo,
+                     mirror_local_mode=True)
+
+        result = {
+            'succeeded': output.succeeded,
+            'failed': output.failed
+        }
+
+        if output.failed:
+            result['error'] = 'Failed copying file %s to %s on remote box' % (
+                self.script_local_path_abs, self.remote_path)
+        return result
+
+    # XXX: needs a __str__ method
