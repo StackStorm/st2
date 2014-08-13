b4a8490d3d334ffcc39990b3faa1b84ce5c191be
Kirill Izotov <enykeev@stackstorm.com>
2014-08-13 16:05:58 +0700

    Revert "Disable broken tests."
    This reverts commit 9f081106a34e17117acf6e8b43785f40e41cf17c.

--- st2common/tests/test_watcher.py
+++ st2common/tests/test_watcher.py
@@ -2,7 +2,6 @@ import bson
 import eventlet
 import mock
 import mongoengine as me
-import unittest2
 from oslo.config import cfg
 
 from st2tests import DbTestCase
@@ -55,7 +54,6 @@ class WatcherTest(DbTestCase):
 
         self.assertRaises(Exception, watcher.stop)
 
-    @unittest2.skip('Test fails on jenkins. Perhaps a race caused by eventlet.sleep.')
     def test_watcher_watches_default(self):
         watcher = watch.get_watcher()
         func = mock.MagicMock()
@@ -73,7 +71,6 @@ class WatcherTest(DbTestCase):
         self._assert_call_args(func.call_args_list[0][0], dict(doc.to_mongo()), watch.INSERT)
         self._assert_call_args(func.call_args_list[1][0], {'_id': doc.id}, watch.DELETE)
 
-    @unittest2.skip('Test fails on jenkins. Perhaps a race caused by eventlet.sleep.')
     def test_watcher_watches_operation(self):
         watcher = watch.get_watcher()
         func = mock.MagicMock()
