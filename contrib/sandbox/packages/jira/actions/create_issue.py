6e44d3ff1012893d9e8f617d64a6aad3881139bf
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-08-13 17:34:40 -0700

    Add sample JIRA sensor and action     * Add JIRA sensor that would watch for new projects.     * Add JIRA action that would create an issue.     * Add JIRA action metadata.

---
+++ contrib/sandbox/packages/jira/actions/create_issue.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# Requirements
+# pip install jira
+
+import os
+import sys
+from jira.client import JIRA
+
+RSA_CERT_FILE = '/home/vagrant/jira.pem'
+
+
+class AuthedJiraClient(object):
+    def __init__(self, jira_server, oauth_creds):
+        self._client = JIRA(options={'server': jira_server},
+                            oauth=oauth_creds)
+
+    def is_project_exists(self, project):
+        projs = self._client.projects()
+        project_names = [proj.key for proj in projs]
+        if project not in project_names:
+            return False
+        return True
+
+    def create_issue(self, project=None, summary=None, desc=None, issuetype=None):
+        issue_dict = {
+            'project': {'key': project},
+            'summary': summary,
+            'description': desc,
+            'issuetype': {'name': issuetype},
+        }
+        new_issue = self._client.create_issue(fields=issue_dict)
+        return new_issue
+
+
+def _read_cert(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+def _parse_args(args):
+    params = {}
+    params['jira_server'] = args[1]
+    params['oauth_token'] = args[2]
+    params['oauth_secret'] = args[3]
+    params['consumer_key'] = args[4]
+    params['project_name'] = args[5]
+    params['issue_summary'] = args[6]
+    params['issue_description'] = args[7]
+    params['issue_type'] = args[8]
+    return params
+
+
+def _get_jira_client(params):
+    if not os.path.exists(RSA_CERT_FILE):
+        raise Exception('Cert file required for JIRA OAuth.')
+    rsa_key = _read_cert(RSA_CERT_FILE)
+    oauth_creds = {
+        'access_token': params['oauth_token'],
+        'access_token_secret': params['oauth_secret'],
+        'consumer_key': params['consumer_key'],
+        'key_cert': rsa_key
+    }
+    jira_client = AuthedJiraClient(params['jira_server'], oauth_creds)
+    return jira_client
+
+
+def main(args):
+    params = _parse_args(args)
+    client = _get_jira_client(params)
+    proj = params['project_name']
+
+    try:
+        if not client.is_project_exists(proj):
+            raise Exception('Project ' + proj + ' does not exist.')
+        issue = client.create_issue(project=params['project_name'],
+                                    summary=params['issue_summary'],
+                                    desc=params['issue_description'],
+                                    issuetype=params['issue_type'])
+    except Exception as e:
+        sys.stderr.write(e.message + '\n')
+    else:
+        sys.stdout.write('Issue ' + issue + ' created.\n')
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ contrib/sandbox/packages/jira/sensors/jira_sensor.py
@@ -0,0 +1,80 @@
+import os
+import time
+
+from jira.client import JIRA
+
+RSA_CERT_FILE = '/home/vagrant/jira.pem'
+
+
+class JIRASensor(object):
+    '''
+    Sensor will monitor for any new projects created in JIRA and
+    emit trigger instance when one is created.
+    '''
+    def __init__(self, container_service):
+        self._container_service = container_service
+        self._jira_server = 'https://stackstorm.atlassian.net'
+        # The Consumer Key created while setting up the "Incoming Authentication" in
+        # JIRA for the Application Link.
+        self._consumer_key = u''
+        self._rsa_key = None
+        self._jira_client = None
+        self._access_token = u''
+        self._access_secret = u''
+        self._projects_available = None
+        self._sleep_time = 30
+
+    def _read_cert(self, file_path):
+        with open(file_path) as f:
+            return f.read()
+
+    def setup(self):
+        global RSA_CERT_FILE
+        if not os.path.exists(RSA_CERT_FILE):
+            raise Exception('Cert file required for JIRA OAuth.')
+
+        # The contents of the rsa.pem file generated (the private RSA key)
+        self._rsa_key = self._read_cert(RSA_CERT_FILE)
+        oauth_creds = {
+            'access_token': self._access_token,
+            'access_token_secret': self._access_secret,
+            'consumer_key': self._consumer_key,
+            'key_cert': self._rsa_key
+        }
+
+        self._jira_client = JIRA(options={'server': self._jira_server},
+                                 oauth=oauth_creds)
+        if self._projects_available is None:
+            self._projects_available = set()
+            for proj in self._jira_client.projects():
+                self._projects_available.add(proj.key)
+
+    def start(self):
+        while True:
+            for proj in self._jira_client.projects():
+                if proj.key not in self._projects_available:
+                    self._dispatch_trigger(proj)
+                    self._projects_available.add(proj)
+            time.sleep(self._sleep_time)
+
+    def stop(self):
+        pass
+
+    def get_trigger_types(self):
+        return [
+            {
+                'name': 'st2.jira.project_tracker',
+                'description': 'Stackstorm JIRA projects tracker',
+                'payload_info': ['project_name', 'project_url']
+            }
+        ]
+
+    def _dispatch_trigger(self, proj):
+        trigger = {}
+        trigger['name'] = 'st2.jira.projects-tracker'
+        payload = {}
+        payload['project_name'] = proj.key
+        payload['project_url'] = proj.self
+        trigger['payload'] = payload
+        self._container_service.dispatch(trigger)
+
