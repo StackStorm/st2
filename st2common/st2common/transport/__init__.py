f7c7cf55b6536fd5646272b700ce42cc02936fbb
manasdk <manas@stackstorm.com>
2014-09-03 02:02:14 +0000

    Add TODO to capture the fact that design discussion is pending.

--- st2common/st2common/transport/__init__.py
+++ st2common/st2common/transport/__init__.py
@@ -1,3 +1,5 @@
 from st2common.transport import actionexecution, publishers
 
+# TODO(manas) : Exchanges, Queues and RoutingKey design discussion pending.
+
 __all__ = ['actionexecution', 'publishers']
