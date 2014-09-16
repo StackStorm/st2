f3e1253250569dbf641205286a58b538f3546458
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-09-16 23:00:00 +0000

    Fix: reactor logging config file

--- st2reactor/st2reactor/cmd/sensormanager.py
+++ st2reactor/st2reactor/cmd/sensormanager.py
@@ -22,7 +22,7 @@ def _setup():
     config.parse_args()
 
     # 2. setup logging.
-    logging.setup(cfg.CONF.reactor_logging.config_file)
+    logging.setup(cfg.CONF.reactor.logging)
 
     # 3. all other setup which requires config to be parsed and logging to
     # be correctly setup.
