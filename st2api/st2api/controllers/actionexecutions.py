77618117ea0363d0291c85d725d526d416c49a48
manasdk <manas@stackstorm.com>
2014-09-26 21:58:06 +0000

    Include parent execution id in the workflow execution.
    - Parent execution id is included in the execution context of a child
      execution. This keep a link between parent and child executions.
    - Shuffle around some actionexecution.post and schedule code around.
    - move echochain into examples as opposed to core.

--- st2api/st2api/controllers/actionexecutions.py
+++ st2api/st2api/controllers/actionexecutions.py
@@ -106,13 +106,6 @@ class ActionExecutionsController(RestController):
                 context = pecan.request.headers['st2-context'].replace("'", "\"")
                 execution.context.update(json.loads(context))
 
-            # Use the user context from the parent action execution. Subtasks in a workflow
-            # action can be invoked by a system user and so we want to use the user context
-            # from the original workflow action.
-            if getattr(execution, 'context', None) and 'parent' in execution.context:
-                parent = ActionExecution.get_by_id(execution.context['parent'])
-                execution.context['user'] = getattr(parent, 'context', dict()).get('user')
-
             # Schedule the action execution.
             return action_service.schedule(execution)
         except ValueError as e:
