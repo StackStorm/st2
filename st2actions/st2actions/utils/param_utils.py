5c0aacd28f28c4c4ee76c81c847dc8244accf13d
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-10-02 23:52:30 +0000

    Add API to get merged action and runner parameters.
    Add: params_utils file to merge action and runner params meta.
    Add: Test cases for params_utils.
    Add: Add an ActionViewsController and a ParametersViewsController.
    Add: ParametersAPI model.

---
+++ st2actions/st2actions/utils/param_utils.py
@@ -0,0 +1,60 @@
+import copy
+
+
+def _merge_param_meta_values(action_meta=None, runner_meta=None):
+    runner_meta_keys = runner_meta.keys() if runner_meta else []
+    action_meta_keys = action_meta.keys() if action_meta else []
+    all_keys = set(runner_meta_keys).union(set(action_meta_keys))
+
+    merged_meta = {}
+
+    # ?? Runner immutable param's meta shouldn't be allowed to be modified by action whatsoever.
+    if runner_meta and runner_meta.get('immutable', False):
+        merged_meta = runner_meta
+
+    for key in all_keys:
+        if key in action_meta_keys and key not in runner_meta_keys:
+            merged_meta[key] = action_meta[key]
+        elif key in runner_meta_keys and key not in action_meta_keys:
+            merged_meta[key] = runner_meta[key]
+        else:
+            if key == 'immutable':
+                merged_meta[key] = runner_meta.get(key, False) or action_meta.get(key, False)
+            else:
+                merged_meta[key] = action_meta.get(key)
+    return merged_meta
+
+
+def get_params_view(action_db=None, runner_db=None, merged_only=False):
+    runner_params = copy.deepcopy(runner_db.runner_parameters) if runner_db else {}
+    action_params = copy.deepcopy(action_db.parameters) if action_db else {}
+
+    parameters = set(runner_params.keys()).union(set(action_params.keys()))
+
+    merged_params = {}
+    for param in parameters:
+        merged_params[param] = _merge_param_meta_values(action_meta=action_params.get(param),
+                                                        runner_meta=runner_params.get(param))
+
+    if merged_only:
+        return merged_params
+
+    required = set((getattr(runner_db, 'required_parameters', list()) +
+                    getattr(action_db, 'required_parameters', list())))
+
+    def is_immutable(param_meta):
+        return param_meta.get('immutable', False)
+
+    immutable = set()
+    for param in parameters:
+        if is_immutable(merged_params.get(param)):
+            immutable.add(param)
+
+    required = required - immutable
+    optional = parameters - required - immutable
+
+    required_params = {k: merged_params[k] for k in required}
+    optional_params = {k: merged_params[k] for k in optional}
+    immutable_params = {k: merged_params[k] for k in immutable}
+
+    return required_params, optional_params, immutable_params
---
+++ st2api/st2api/controllers/actionviews.py
@@ -0,0 +1,114 @@
+from mongoengine import ValidationError
+from pecan import abort
+from pecan.rest import RestController
+import six
+
+import st2actions.utils.param_utils as param_utils
+from st2common import log as logging
+from st2common.models.api.action import ActionAPI
+from st2common.models.base import jsexpose
+from st2common.persistence.action import (Action, RunnerType)
+
+http_client = six.moves.http_client
+
+LOG = logging.getLogger(__name__)
+
+
+class LookupUtils(object):
+
+    @staticmethod
+    def _get_action_by_id(id):
+        try:
+            return Action.get_by_id(id)
+        except Exception as e:
+            msg = 'Database lookup for id="%s" resulted in exception. %s' % (id, e)
+            LOG.exception(msg)
+            abort(http_client.NOT_FOUND, msg)
+
+    @staticmethod
+    def _get_runner_by_id(id):
+        try:
+            return RunnerType.get_by_id(id)
+        except (ValueError, ValidationError) as e:
+            msg = 'Database lookup for id="%s" resulted in exception. %s' % (id, e)
+            LOG.exception(msg)
+            abort(http_client.NOT_FOUND, msg)
+
+    @staticmethod
+    def _get_runner_by_name(name):
+        try:
+            return RunnerType.get_by_name(name)
+        except (ValueError, ValidationError) as e:
+            msg = 'Database lookup for name="%s" resulted in exception. %s' % (id, e)
+            LOG.exception(msg)
+            abort(http_client.NOT_FOUND, msg)
+
+
+class ParametersViewController(RestController):
+
+    @jsexpose(str, status_code=http_client.OK)
+    def get_one(self, action_id):
+        return self._get_one(action_id)
+
+    @staticmethod
+    def _get_one(action_id):
+        """
+            List merged action & runner parameters by action id.
+
+            Handle:
+                GET /actions/views/parameters/1
+        """
+        action_db = LookupUtils._get_action_by_id(action_id)
+        LOG.info('Found action: %s, runner: %s', action_db, action_db.runner_type['name'])
+        runner_db = LookupUtils._get_runner_by_name(action_db.runner_type['name'])
+
+        required, optional, immutable = param_utils.get_params_view(
+            action_db=action_db, runner_db=runner_db, merged_only=False)
+
+        param_types = {}
+        param_types['required'] = required
+        param_types['optional'] = optional
+        param_types['immutable'] = immutable
+
+        return {'parameters': param_types}
+
+
+class OverviewController(RestController):
+    @jsexpose(str)
+    def get_one(self, action_id):
+        """
+            List action by id.
+
+            Handle:
+                GET /actions/views/overview/1
+        """
+
+        LOG.info('GET /actions/ with id=%s', action_id)
+        action_db = LookupUtils._get_action_by_id(action_id)
+        action_api = ActionAPI.from_model(action_db)
+        return self._transform_action_api(action_api)
+
+    @jsexpose(str)
+    def get_all(self, name=None, **kw):
+        """
+            List all actions.
+
+            Handles requests:
+                GET /actions/views/overview
+        """
+
+        LOG.info('GET all /actions/ and name=%s', str(name))
+        action_dbs = Action.get_all(**kw) if name is None else OverviewController._get_by_name(name)
+        action_apis = [ActionAPI.from_model(action_db) for action_db in action_dbs]
+        return map(self._transform_action_api, action_apis)
+
+    def _transform_action_api(self, action_api):
+        action_id = action_api.id
+        action_api.parameters = ParametersViewController._get_one(action_id).get('parameters')
+        del action_api.required_parameters
+        return action_api
+
+
+class ActionViewsController(RestController):
+    parameters = ParametersViewController()
+    overview = OverviewController()
