7606128192e88c434561870ae4e121ecccb15d46
Tomaz Muraus <tomaz@tomaz.me>
2014-10-17 15:48:09 +0800

    Add "content_pack" attribute to TriggerType API model.

--- st2common/st2common/models/api/reactor.py
+++ st2common/st2common/models/api/reactor.py
@@ -23,6 +23,10 @@ class TriggerTypeAPI(BaseAPI):
                 'default': None
             },
             'name': {
+                'description': 'The content pack this trigger belongs to.',
+                'type': 'string'
+            },
+            'content_pack': {
                 'type': 'string'
             },
             'description': {
