92ea652497e332c1cd031c338eea58ab9a25210a
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-10-22 21:06:06 +0000

    Fix: Rules registrar should not muck around with triggers manually

--- st2reactor/st2reactor/bootstrap/rulesregistrar.py
+++ st2reactor/st2reactor/bootstrap/rulesregistrar.py
@@ -6,7 +6,6 @@ import six
 
 from st2common import log as logging
 from st2common.content.loader import ContentPackLoader
-from st2common.models.api.reactor import TriggerAPI
 from st2common.models.api.rule import RuleAPI
 from st2common.persistence.reactor import Rule
 from st2common.services import triggers as TriggerService
@@ -30,18 +29,11 @@ def _register_rules_from_pack(pack, rules):
                     LOG.exception('Unable to load rule from %s.', rule)
                     continue
                 rule_api = RuleAPI(**content)
-                trigger_api = TriggerAPI(**rule_api.trigger)
-
                 rule_db = RuleAPI.to_model(rule_api)
-                trigger_db = TriggerService.create_trigger_db(trigger_api)
-
                 try:
                     rule_db.id = Rule.get_by_name(rule_api.name).id
                 except ValueError:
                     LOG.info('Rule %s not found. Creating new one.', rule)
-
-                rule_db.trigger = reference.get_str_resource_ref_from_model(trigger_db)
-
                 try:
                     rule_db = Rule.add_or_update(rule_db)
                     LOG.audit('Rule updated. Rule %s from %s.', rule_db, rule)
