8735eb670e503d843dd0221d15e99b74f3784a21
Tomaz Muraus <tomaz@tomaz.me>
2014-11-03 11:21:01 +0800

    Fix boolean values handling when building local / remote script command line arguments string.
    Boolean arguments need to be passed in form of "--<arg>" if the value is True.

--- st2common/st2common/models/system/action.py
+++ st2common/st2common/models/system/action.py
@@ -112,10 +112,15 @@ class RemoteScriptAction(RemoteAction):
         # add all named_args in the format name=value
         if self.named_args is not None:
             for (arg, value) in six.iteritems(self.named_args):
-                if value is None or len(value) < 1:
+                if value is None or (isinstance(value, (str, unicode)) and len(value) < 1):
                     LOG.debug('Ignoring arg %s as its value is %s.', arg, value)
                     continue
-                command_parts.append('%s=%s' % (arg, pipes.quote(value)))
+
+                if isinstance(value, bool):
+                    if value is True:
+                        command_parts.append(arg)
+                else:
+                    command_parts.append('%s=%s' % (arg, pipes.quote(str(value))))
         # add the positional args
         if self.positional_args:
             command_parts.append(self.positional_args)
