fcd710b2a3f2cee897b699e44834bfcd88b02b95
Tomaz Muraus <tomaz@tomaz.me>
2014-11-05 17:23:47 +0800

    Use action-specific logger instead.

--- contrib/packs/actions/pack_mgmt/unload.py
+++ contrib/packs/actions/pack_mgmt/unload.py
@@ -1,9 +1,6 @@
 from st2actions.runners.pythonrunner import Action
 import st2common.config as config
 from st2common.persistence import action
-from st2common import log as logging
-
-LOG = logging.getLogger(__name__)
 
 
 class UnregisterPackAction(Action):
@@ -11,37 +8,32 @@ class UnregisterPackAction(Action):
     def run(self, packs=None):
         self._setup()
         for pack in packs:
-            LOG.debug('Removing pack %s.', pack)
+            self.logger.debug('Removing pack %s.', pack)
             self._unregister_rules(pack)
             self._unregister_triggers(pack)
             self._unregister_trigger_types(pack)
             self._unregister_actions(pack)
-            LOG.info('Removed pack %s.', pack)
+            self.logger.info('Removed pack %s.', pack)
 
-    @staticmethod
-    def _setup():
+    def _setup(self):
         try:
             config.parse_args()
         except:
             pass
 
-    @staticmethod
-    def _unregister_rules(pack):
+    def _unregister_rules(self, pack):
         pass
 
-    @staticmethod
-    def _unregister_triggers(pack):
+    def _unregister_triggers(self, pack):
         pass
 
-    @staticmethod
-    def _unregister_trigger_types(pack):
+    def _unregister_trigger_types(self, pack):
         pass
 
-    @staticmethod
-    def _unregister_actions(pack):
+    def _unregister_actions(self, pack):
         action_dbs = action.Action.get_all(pack=pack)
         for action_db in action_dbs:
             try:
                 action.Action.delete(action_db)
             except:
-                LOG.exception('Failed to remove action %s.', action_db)
+                self.logger.exception('Failed to remove action %s.', action_db)
