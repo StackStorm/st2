5866d85d12808d045b542cbf9d8657228ecaad81
Tomaz Muraus <tomaz@tomaz.me>
2014-11-10 18:40:31 +0800

    Stub out new container class which runs each sensor in a separate process.

---
+++ st2reactor/st2reactor/container/process_container.py
@@ -0,0 +1,203 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import time
+import subprocess
+
+import six
+
+from st2common import log as logging
+
+__all__ = [
+    'MultiProcessSensorContainer'
+]
+
+LOG = logging.getLogger('st2reactor.multi_process_sensor_container')
+
+PROCESS_EXIT_TIMEOUT = 5  # how long to wait for process to exit after sending SIGKILL (in seconds)
+SUCCESS_EXIT_CODE = 0
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+WRAPPER_SCRIPT_NAME = 'sensor_wrapper.py'
+WRAPPER_SCRIPT_PATH = os.path.join(BASE_DIR, WRAPPER_SCRIPT_NAME)
+
+# TODO: Allow multiple instances of the same sensor with different configuration
+# options - we need to update sensors for that and add "get_id" or similar
+# method to the sensor class
+
+
+class MultiProcessSensorContainer(object):
+    """
+    Sensor container which runs sensors in a separate process.
+    """
+
+    def __init__(self, sensors):
+        """
+        :param sensors: A list of sensor dicts.
+        :type sensors: ``list`` of ``dict``
+        """
+        self._sensors = {}  # maps sensor_id -> sensor object
+        self._processes = {}  # maps sensor_id -> sensor process
+
+        for sensor_obj in sensors:
+            sensor_id = self._get_sensor_id(sensor=sensor_obj)
+            self._sensors[sensor_id] = sensor_obj
+
+    def run(self):
+        self._run_all_sensors()
+
+        # Poll for all running processes
+        sensor_ids = self._sensors.keys()
+        poll_interval = 5
+
+        while len(sensor_ids) >= 1:
+            for sensor_id in sensor_ids:
+                process = self._processes[sensor_id]
+                status = process.poll()
+
+                if status is not None:
+                    # Dead process detected
+                    LOG.debug('Process for sensor %s has exited', sensor_id)
+
+                    del self._processes[sensor_id]
+                    del self._sensors[sensor_id]
+
+            sensor_ids = self._sensors.keys()
+            time.sleep(poll_interval)
+
+        LOG.info('Container has no active sensors running.')
+        return SUCCESS_EXIT_CODE
+
+    def running(self):
+        return len(self._processes)
+
+    def shutdown(self):
+        LOG.info('Container shutting down. Invoking cleanup on sensors.')
+
+        for sensor_id in self._sensors:
+            self._stop_sensor_process(sensor_id=sensor_id)
+
+        LOG.info('All sensors are shut down.')
+
+        self._sensors = {}
+        self._processes = {}
+
+    def add_sensor(self, sensor):
+        """
+        Add a new sensor to the container.
+
+        :type sensor: ``dict``
+        """
+        sensor_id = self._get_sensor_id(sensor=sensor)
+
+        if sensor_id in self._sensors:
+            LOG.warning('Sensor %s already exists and running.', sensor_id)
+            return False
+
+        self._spawn_sensor_process(sensor=sensor)
+        self._sensors[sensor_id] = sensor
+        return True
+
+    def remove_sensor(self, sensor):
+        """
+        Remove an existing sensor from the container.
+
+        :type sensor: ``dict``
+        """
+        sensor_id = self._get_sensor_id(sensor=sensor)
+
+        if sensor_id not in self._sensors:
+            LOG.warning('Sensor %s isn\'t running in this container.', sensor_id)
+            return False
+
+        self._stop_sensor_process(sensor_id=sensor_id)
+        return True
+
+    def _run_all_sensors(self):
+        for sensor_id, sensor_obj in six.iteritems(self._sensors):
+            LOG.info('Running sensor %s' % sensor_id)
+            self._spawn_sensor_process(sensor=sensor_obj)
+
+    def _spawn_sensor_process(self, sensor):
+        """
+        Spawn a new process for the provided sensor.
+
+        New process uses isolated Python binary from a virtual environment
+        belonging to the sensor pack.
+        """
+        python_path = os.path.join(sensor['virtualenv_path'], 'bin/python')
+
+        args = [
+            python_path,
+            WRAPPER_SCRIPT_PATH,
+            '--sensor-file-path=%s' % (sensor['filename']),
+            '--sensor-class-name=%s' % (sensor['class_name']),
+            '--sensor-config-path=%s' % (sensor['config_path'])
+        ]
+
+        # TODO: Intercept stdout and stderr for aggregated logging purposes
+        process = subprocess.Popen(args=args, stdin=None, stdout=None,
+                                   stderr=None, shell=False)
+        sensor_id = self._get_sensor_id(sensor=sensor)
+        self._processes[sensor_id] = process
+        return process
+
+    def _stop_sensor_process(self, sensor_id, exit_timeout=5):
+        """
+        Stop a sensor process for the provided sensor.
+
+        :param sensor_id: Sensor ID.
+        :type sensor_id: ``str``
+
+        :param exit_timeout: How long to wait for process to exit after
+                             sending SIGTERM signal. If the process doesn't
+                             exit in this amount of seconds, SIGTERM signal
+                             will be sent to the process.
+        :type exit__timeout: ``int``
+        """
+        process = self._processes[sensor_id]
+
+        # Terminate the process and wait for up to stop_timeout seconds for the
+        # process to exit
+        process.terminate()
+
+        timeout = 0
+        sleep_delay = 1
+        while timeout < exit_timeout:
+            status = process.poll()
+
+            if status is not None:
+                # Process has exited
+                break
+
+            timeout += sleep_delay
+            time.sleeps(sleep_delay)
+
+        if status is None:
+            # Process hasn't exited yet, forcefully kill it
+            process.kill()
+
+        del self._processes[sensor_id]
+        del self._sensors[sensor_id]
+
+    def _get_sensor_id(self, sensor):
+        """
+        Return unique identifier for the provider sensor dict.
+
+        :type sensor: ``dict``
+        """
+        sensor_id = sensor['class_name']
+        return sensor_id
