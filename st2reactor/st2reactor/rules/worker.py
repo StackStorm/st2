9ca02abf1678dd671478920f54027c52312252ce
Tomaz Muraus <tomaz@tomaz.me>
2014-11-17 21:47:37 +0800

    Indicate which queue needs to be used inside the rules engine.

--- st2reactor/st2reactor/rules/worker.py
+++ st2reactor/st2reactor/rules/worker.py
@@ -1,2 +1,8 @@
+from st2common.transport.reactor import get_trigger_queue
+
+
 def work():
+    # TODO Listen on this queue and dispatch message to the rules engine
+    queue = get_trigger_queue(name='st2.trigger_dispatch.rules_engine',
+                              routing_key='#')
     pass
