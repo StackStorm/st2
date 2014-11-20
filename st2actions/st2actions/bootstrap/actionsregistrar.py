043916b139ad24a699c548ec26b6b79a4dbc1793
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-11-20 00:34:56 +0000

    Fix: ActionRegistrar should use ResourceReference

--- st2actions/st2actions/bootstrap/actionsregistrar.py
+++ st2actions/st2actions/bootstrap/actionsregistrar.py
@@ -24,7 +24,8 @@ from st2common.content.loader import (ContentPackLoader, MetaLoader)
 from st2common.content.validators import RequirementsValidator
 from st2common.persistence.action import Action
 from st2common.models.db.action import ActionDB
-from st2common.util.action_db import get_runnertype_by_name
+from st2common.models.system.common import ResourceReference
+import st2common.util.action_db as action_utils
 
 LOG = logging.getLogger(__name__)
 
@@ -58,9 +59,9 @@ class ActionsRegistrar(object):
 
     def _register_action(self, pack, action):
         content = self._meta_loader.load(action)
-        try:
-            model = Action.get_by_name(str(content['name']))
-        except ValueError:
+        action_ref = ResourceReference(pack=pack, name=str(content['name']))
+        model = action_utils.get_action_by_ref(action_ref)
+        if not model:
             model = ActionDB()
         model.name = content['name']
         model.description = content['description']
@@ -85,7 +86,7 @@ class ActionsRegistrar(object):
 
     def _has_valid_runner_type(self, runner_type):
         try:
-            return True, get_runnertype_by_name(runner_type)
+            return True, action_utils.get_runnertype_by_name(runner_type)
         except:
             return False, None
 
