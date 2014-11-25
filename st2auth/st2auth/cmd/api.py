6b9a3e04dd1a0619d38ef4c5a3d7c5838bd95abf
Tomaz Muraus <tomaz@tomaz.me>
2014-11-25 22:05:11 +0800

    Fix st2auth entry point.
    We still want to allow user to run the st2auth as a standalone service for
    testing purposes.

---
+++ st2auth/st2auth/cmd/api.py
@@ -0,0 +1,77 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import eventlet
+import os
+import sys
+
+from oslo.config import cfg
+from eventlet import wsgi
+
+from st2common import log as logging
+from st2common.models.db import db_setup
+from st2common.models.db import db_teardown
+from st2auth import config
+from st2auth import app
+
+
+eventlet.monkey_patch(
+    os=True,
+    select=True,
+    socket=True,
+    thread=False if '--use-debugger' in sys.argv else True,
+    time=True)
+
+LOG = logging.getLogger(__name__)
+
+
+def _setup():
+    # 1. parse args to setup config.
+    config.parse_args()
+
+    # 2. setup logging.
+    logging.setup(cfg.CONF.auth.logging)
+
+    # 3. all other setup which requires config to be parsed and logging to
+    # be correctly setup.
+    username = cfg.CONF.database.username if hasattr(cfg.CONF.database, 'username') else None
+    password = cfg.CONF.database.password if hasattr(cfg.CONF.database, 'password') else None
+    db_setup(cfg.CONF.database.db_name, cfg.CONF.database.host, cfg.CONF.database.port,
+             username=username, password=password)
+
+
+def _run_server():
+    host = cfg.CONF.auth.host
+    port = cfg.CONF.auth.port
+
+    LOG.info('(PID=%s) ST2 Auth API is serving on http://%s:%s.', os.getpid(), host, port)
+
+    wsgi.server(eventlet.listen((host, port)), app.setup_app())
+    return 0
+
+
+def _teardown():
+    db_teardown()
+
+
+def main():
+    try:
+        _setup()
+        return _run_server()
+    except:
+        LOG.exception('(PID=%s) ST2 Auth API quit due to exception.', os.getpid())
+        return 1
+    finally:
+        _teardown()
