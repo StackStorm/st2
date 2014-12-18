0f149da50abbb327c263b2f48159b9b7953c4083
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-12-18 00:16:24 +0000

    Spin timer up as part of rules_engine process

--- st2reactor/st2reactor/cmd/rulesengine.py
+++ st2reactor/st2reactor/cmd/rulesengine.py
@@ -9,6 +9,7 @@ from st2common.models.db import db_teardown
 from st2common.constants.logging import DEFAULT_LOGGING_CONF_PATH
 from st2reactor.rules import config
 from st2reactor.rules import worker
+from st2reactor.timer.base import St2Timer
 
 LOG = logging.getLogger('st2reactor.bin.rulesengine')
 
@@ -37,8 +38,10 @@ def _teardown():
 
 
 def main():
+    timer = St2Timer(local_timezone=cfg.CONF.timer.local_timezone)
     try:
         _setup()
+        timer.start()
         return worker.work()
     except SystemExit as exit_code:
         sys.exit(exit_code)
@@ -46,4 +49,5 @@ def main():
         LOG.exception('(PID:%s) RulesEngine quit due to exception.', os.getpid())
         return 1
     finally:
+        timer.cleanup()
         _teardown()
