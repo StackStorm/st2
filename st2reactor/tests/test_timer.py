a6c530134e97f6e555fea2fff27efd5d399e0902
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2014-12-18 23:38:23 +0000

    Add end-to-end test case

--- st2reactor/tests/test_timer.py
+++ st2reactor/tests/test_timer.py
@@ -1,12 +1,29 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import copy
 
 import bson
-import unittest2
+import eventlet
+import mock
 
 from st2common.constants.pack import SYSTEM_PACK_NAME
 from st2common.models.db.reactor import TriggerDB
 from st2common.models.system.common import ResourceReference
 from st2reactor.timer.base import St2Timer
+from st2tests.base import EventletTestCase
 import st2tests.config as tests_config
 
 
@@ -23,7 +40,7 @@ class TestDispatcher(object):
         return self.payload is not None and self.payload.get('executed_at', None)
 
 
-class TimerTest(unittest2.TestCase):
+class TimerTest(EventletTestCase):
     test_trigger = None
 
     @classmethod
@@ -32,7 +49,7 @@ class TimerTest(unittest2.TestCase):
         tests_config.parse_args()
         parameters = {}
         parameters['unit'] = 'seconds'
-        parameters['delta'] = 30
+        parameters['delta'] = 1
         ref = ResourceReference.to_string_reference(SYSTEM_PACK_NAME, 'st2.IntervalTimer')
         TimerTest.test_trigger = TriggerDB(name='testtimer', pack='test', parameters=parameters,
                                            type=ref)
@@ -72,3 +89,18 @@ class TimerTest(unittest2.TestCase):
             pass
         timer.remove_trigger(TimerTest.test_trigger)
         self.assertTrue(len(timer._scheduler.get_jobs()) == 0)
+
+    @mock.patch.object(St2Timer, '_register_timer_trigger_types', mock.MagicMock())
+    def test_timer_end_to_end(self):
+        timer = St2Timer(local_timezone='America/Los_Angeles')
+        mock_dispatcher = TestDispatcher()
+        setattr(timer, '_trigger_dispatcher', mock_dispatcher)
+        timer.add_trigger(TimerTest.test_trigger)
+
+        def kickoff_timer(timer):
+            timer.start()
+
+        eventlet.spawn(kickoff_timer, timer)
+        eventlet.sleep(2)
+        self.assertTrue(mock_dispatcher.assert_payload())
+        timer.cleanup()
