d6fd7cc79b6d436140ceaaf00aee9a8181207e7c
Tomaz Muraus <tomaz@tomaz.me>
2014-12-22 23:26:36 +0100

    Call shutdown() method on the ProcessContainer when shutting down sensor manager using CTRL-C.
    This fixes a bug with stray processes.

--- st2reactor/st2reactor/container/process_container.py
+++ st2reactor/st2reactor/container/process_container.py
@@ -16,6 +16,7 @@
 import os
 import sys
 import time
+import copy
 import json
 import subprocess
 
@@ -92,7 +93,8 @@ class ProcessSensorContainer(object):
     def shutdown(self):
         LOG.info('Container shutting down. Invoking cleanup on sensors.')
 
-        for sensor_id in self._sensors:
+        sensors = copy.deepcopy(self._sensors)
+        for sensor_id in sensors:
             self._stop_sensor_process(sensor_id=sensor_id)
 
         LOG.info('All sensors are shut down.')
