9742f1197477071bbef96218e65e13db778af478
Tomaz Muraus <tomaz@tomaz.me>
2015-01-05 15:22:45 +0100

    Add enabled attribute to the SensorType API model.

--- st2common/st2common/models/api/reactor.py
+++ st2common/st2common/models/api/reactor.py
@@ -41,6 +41,9 @@ class SensorTypeAPI(BaseAPI):
             'description': {
                 'type': 'string'
             },
+            'enabled': {
+                'type': 'boolean',
+            },
             'artifact_uri': {
                 'type': 'string',
             },
