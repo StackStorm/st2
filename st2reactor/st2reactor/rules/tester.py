b1fc3f27e1fabd2b879e3c333a6f5ef02d953221
Tomaz Muraus <tomaz@tomaz.me>
2015-01-08 19:37:09 +0100

    Avoid duplicated code and use MetaLoader instead.

--- st2reactor/st2reactor/rules/tester.py
+++ st2reactor/st2reactor/rules/tester.py
@@ -13,13 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-from st2common.constants.meta import PARSER_FUNCS
 from st2common.models.db.reactor import RuleDB
 from st2common.models.db.reactor import TriggerDB
 from st2common.models.db.reactor import TriggerInstanceDB
 from st2common.models.system.common import ResourceReference
+from st2common.content.loader import MetaLoader
 from st2reactor.rules.matcher import RulesMatcher
 
 __all__ = [
@@ -38,6 +36,7 @@ class RuleTester(object):
         """
         self._rule_file_path = rule_file_path
         self._trigger_instance_file_path = trigger_instance_file_path
+        self._meta_loader = MetaLoader()
 
     def evaluate(self):
         """
@@ -63,7 +62,7 @@ class RuleTester(object):
         return len(matching_rules) >= 1
 
     def _get_rule_db_from_file(self, file_path):
-        data = self._get_obj_from_file(file_path=file_path)
+        data = self._meta_loader.load(file_path=file_path)
         rule_db = RuleDB()
         rule_db.trigger = data['trigger']['type']
         rule_db.criteria = data.get('criteria', None)
@@ -72,16 +71,6 @@ class RuleTester(object):
         return rule_db
 
     def _get_trigger_instance_db_from_file(self, file_path):
-        data = self._get_obj_from_file(file_path=file_path)
+        data = self._meta_loader.load(file_path=file_path)
         instance = TriggerInstanceDB(**data)
         return instance
-
-    def _get_obj_from_file(self, file_path):
-        _, file_ext = os.path.splitext(file_path)
-        parser_func = PARSER_FUNCS[file_ext]
-
-        with open(file_path, 'r') as fp:
-            content = fp.read()
-
-        data = parser_func(content)
-        return data
