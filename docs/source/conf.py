ae1c167f3057645f8fe443ea1dfc92bce9dd0222
Dmitri Zimine <dz@stackstorm.com>
2015-01-14 19:07:17 -0800

    Small doc fixes:
    * Fix broken links
    * remove unused references to non-existing pages
    * Update README to show how do references the right way and not to have
    more broken links
    * fix style on highlighting TOC header
    * add favicon
    * get rid of hardcoded short version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ copyright = u'2014, StackStorm Inc'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6'
+version = '.'.join(__version__.split('.')[:2])
 # The full version, including alpha/beta/rc tags.
 release = __version__
 
@@ -155,7 +155,7 @@ html_theme_path = ["_themes", ]
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = None
+# html_favicon = "favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
