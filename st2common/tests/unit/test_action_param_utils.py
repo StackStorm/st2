c8d32b484b93fef2d6be8cba6f606930cc9d591b
manasdk <manas@stackstorm.com>
2015-01-15 22:37:00 +0000

    Use fixtures in test_action_param_utils

--- st2common/tests/unit/test_action_param_utils.py
+++ st2common/tests/unit/test_action_param_utils.py
@@ -17,20 +17,24 @@
 import copy
 
 from unittest2 import TestCase
-from st2common.models.api.action import RunnerTypeAPI
-from st2common.models.db.action import ActionDB
 from st2common.models.utils import action_param_utils
+from st2tests.fixturesloader import FixturesLoader
+
+
+FIXTURES_PACK = 'generic'
+
+TEST_MODELS = {
+    'actions': ['action1.json'],
+    'runners': ['testrunner1.json']
+}
+
+FIXTURES = FixturesLoader().load_models(fixtures_pack=FIXTURES_PACK,
+                                        fixtures_dict=TEST_MODELS)
 
 
 class ActionParamsUtilsTest(TestCase):
-    action_db = None
-    runnertype_db = None
-
-    @classmethod
-    def setUpClass(cls):
-        super(ActionParamsUtilsTest, cls).setUpClass()
-        # TODO(manas): Use fixtures
-        ActionParamsUtilsTest._setup_test_models()
+    action_db = FIXTURES['actions']['action1.json']
+    runnertype_db = FIXTURES['runners']['testrunner1.json']
 
     def test_merge_action_runner_params_meta(self):
         required, optional, immutable = action_param_utils.get_params_view(
@@ -77,64 +81,3 @@ class ActionParamsUtilsTest(TestCase):
         self.assertEqual(merged_meta['default'], action_meta['default'])
         # Immutability is set in action.
         self.assertEqual(merged_meta['immutable'], action_meta['immutable'])
-
-    @classmethod
-    def _setup_test_models(cls):
-        ActionParamsUtilsTest._setup_runner_models()
-        ActionParamsUtilsTest._setup_action_models()
-
-    @classmethod
-    def _setup_runner_models(cls):
-        test_runner = {
-            'name': 'test-runner',
-            'description': 'A test runner.',
-            'enabled': True,
-            'runner_parameters': {
-                'runnerstr': {
-                    'description': 'Foo str param.',
-                    'type': 'string',
-                    'default': 'defaultfoo'
-                },
-                'runnerint': {
-                    'description': 'Foo int param.',
-                    'type': 'number'
-                },
-                'runnerfoo': {
-                    'description': 'Some foo param.',
-                    'default': 'FOO'
-                },
-                'runnerdummy': {
-                    'description': 'Dummy param.',
-                    'type': 'string',
-                    'default': 'runnerdummy'
-                },
-                'runnerimmutable': {
-                    'description': 'Immutable param.',
-                    'type': 'string',
-                    'default': 'runnerimmutable',
-                    'immutable': True
-                }
-            },
-            'runner_module': 'tests.test_runner'
-        }
-        runnertype_api = RunnerTypeAPI(**test_runner)
-        ActionParamsUtilsTest.runnertype_db = RunnerTypeAPI.to_model(runnertype_api)
-
-    @classmethod
-    def _setup_action_models(cls):
-        action_db = ActionDB()
-        action_db.name = 'action-1'
-        action_db.description = 'awesomeness'
-        action_db.enabled = True
-        action_db.pack = 'wolfpack'
-        action_db.entry_point = ''
-        action_db.runner_type = {'name': 'test-runner'}
-        action_db.parameters = {
-            'actionstr': {'type': 'string', 'required': True},
-            'actionint': {'type': 'number', 'default': 10},
-            'runnerdummy': {'type': 'string', 'default': 'actiondummy', 'immutable': True},
-            'runnerfoo': {'type': 'string', 'immutable': True},
-            'runnerimmutable': {'type': 'string', 'default': 'failed_override'},
-            'actionimmutable': {'type': 'string', 'default': 'actionimmutable', 'immutable': True}
-        }
-        ActionParamsUtilsTest.action_db = action_db
