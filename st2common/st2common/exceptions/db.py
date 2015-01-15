90963666f22bea81d433724d232deaa0f3e2fec1
manasdk <manas@stackstorm.com>
2015-01-15 02:53:01 +0000

    Add a special exception for capturing object conflicts.

--- st2common/st2common/exceptions/db.py
+++ st2common/st2common/exceptions/db.py
@@ -22,3 +22,12 @@ class StackStormDBObjectNotFoundError(StackStormBaseException):
 
 class StackStormDBObjectMalformedError(StackStormBaseException):
     pass
+
+
+class StackStormDBObjectConflictError(StackStormBaseException):
+    """
+    Exception that captures a DB object conflict error.
+    """
+    def __init__(self, message, conflict_id):
+        super(StackStormDBObjectConflictError, self).__init__(message)
+        self.conflict_id = conflict_id
