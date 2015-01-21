176877215e15b3b607c2e47dde3d31a234cd4066
Tomaz Muraus <tomaz@tomaz.me>
2015-01-21 13:46:56 +0100

    Add tests for get_packs_base_paths.

---
+++ st2common/tests/unit/test_content_utils.py
@@ -0,0 +1,41 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest2
+from oslo.config import cfg
+
+from st2common.content.utils import get_packs_base_paths
+from st2tests import config as tests_config
+
+
+class ContentUtilsTestCase(unittest2.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        tests_config.parse_args()
+
+    def test_get_pack_base_paths(self):
+        cfg.CONF.content.packs_base_paths = '/opt/path1'
+        result = get_packs_base_paths()
+        self.assertEqual(result, ['/opt/path1'])
+
+        # Multiple paths, no trailing colon
+        cfg.CONF.content.packs_base_paths = '/opt/path1:/opt/path2'
+        result = get_packs_base_paths()
+        self.assertEqual(result, ['/opt/path1', '/opt/path2'])
+
+        # Multiple paths, trailing colon
+        cfg.CONF.content.packs_base_paths = '/opt/path1:/opt/path2:'
+        result = get_packs_base_paths()
+        self.assertEqual(result, ['/opt/path1', '/opt/path2'])
