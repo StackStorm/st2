243f36451e284e86b59a81b4016548c3aaadaab6
Patrick Hoolboom <patrick@stackstorm.com>
2015-01-21 20:27:18 -0500

    Fixing rhel6 packaging

--- st2client/setup.py
+++ st2client/setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # Licensed to the StackStorm, Inc ('StackStorm') under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
