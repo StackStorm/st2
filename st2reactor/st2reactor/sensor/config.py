e8812263538dd512c6cdad26ca70de50cc7829e4
manasdk <manas@stackstorm.com>
2015-02-05 23:58:33 +0000

    ignore_errors liberally to avoid weird duplicate registration errors.

--- st2reactor/st2reactor/sensor/config.py
+++ st2reactor/st2reactor/sensor/config.py
@@ -21,41 +21,43 @@ from st2common.constants.system import VERSION_STRING
 CONF = cfg.CONF
 
 
-def _register_common_opts():
-    st2cfg.register_opts()
+def _register_common_opts(ignore_errors=False):
+    st2cfg.register_opts(ignore_errors=True)
 
 
-def _register_sensor_container_opts():
+def _register_sensor_container_opts(ignore_errors=False):
     logging_opts = [
         cfg.StrOpt('logging', default='conf/logging.sensorcontainer.conf',
                    help='location of the logging.conf file')
     ]
-    CONF.register_opts(logging_opts, group='sensorcontainer')
+    st2cfg.do_register_opts(logging_opts, group='sensorcontainer', ignore_errors=ignore_errors)
 
     sensor_test_opt = cfg.StrOpt('sensor-name', help='Only run sensor with the provided name.')
-    CONF.register_cli_opt(sensor_test_opt)
+    st2cfg.do_register_cli_opts(sensor_test_opt, ignore_errors=ignore_errors)
 
     st2_webhook_opts = [
         cfg.StrOpt('host', default='0.0.0.0', help='Host for the st2 webhook endpoint.'),
         cfg.IntOpt('port', default='6000', help='Port for the st2 webhook endpoint.'),
         cfg.StrOpt('url', default='/webhooks/st2/', help='URL of the st2 webhook endpoint.')
     ]
-    CONF.register_opts(st2_webhook_opts, group='st2_webhook_sensor')
+    st2cfg.do_register_opts(st2_webhook_opts, group='st2_webhook_sensor',
+                            ignore_errors=ignore_errors)
 
     generic_webhook_opts = [
         cfg.StrOpt('host', default='0.0.0.0', help='Host for the generic webhook endpoint.'),
         cfg.IntOpt('port', default='6001', help='Port for the generic webhook endpoint.'),
         cfg.StrOpt('url', default='/webhooks/generic/', help='URL of the st2 webhook endpoint.')
     ]
-    CONF.register_opts(generic_webhook_opts, group='generic_webhook_sensor')
+    st2cfg.do_register_opts(generic_webhook_opts, group='generic_webhook_sensor',
+                            ignore_errors=ignore_errors)
 
 
-def register_opts():
-    _register_common_opts()
-    _register_sensor_container_opts()
+def register_opts(ignore_errors=False):
+    _register_common_opts(ignore_errors=ignore_errors)
+    _register_sensor_container_opts(ignore_errors=ignore_errors)
 
 
-register_opts()
+register_opts(ignore_errors=True)
 
 
 def parse_args(args=None):
