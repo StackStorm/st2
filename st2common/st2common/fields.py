d9dc024b5b4ce39941f1e37abb2faeaa6d485e6c
Tomaz Muraus <tomaz@tomaz.me>
2015-02-16 20:18:51 +0100

    Add our implementation of ComplexDateTimeField which stores the value as number of microseconds since the epoch.
    Note: This fixes a bug with live action execution tests periodically failing.

---
+++ st2common/st2common/fields.py
@@ -0,0 +1,100 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import datetime
+
+from mongoengine import LongField
+
+__all__ = [
+    'ComplexDateTimeField'
+]
+
+SECOND_TO_MICROSECONDS = 1000000
+
+
+class ComplexDateTimeField(LongField):
+    """
+    Date time field which handles microseconds exactly and internally stores
+    the timestamp as number of microseconds since the unix epoch.
+
+    Note: We need to do that because mongoengine serializes this field as comma
+    delimited string which breaks sorting.
+    """
+
+    def _convert_from_datetime(self, val):
+        """
+        Convert a `datetime` object to number of microseconds since epoch representation
+        (which will be stored in MongoDB). This is the reverse function of
+        `_convert_from_db`.
+        """
+        result = self._datetime_to_microseconds_since_epoch(value=val)
+        return result
+
+    def _convert_from_db(self, value):
+        result = self._microseconds_since_epoch_to_datetime(data=value)
+        return result
+
+    def _microseconds_since_epoch_to_datetime(self, data):
+        """
+        Convert a number representation to a `datetime` object (the object you
+        will manipulate). This is the reverse function of
+        `_convert_from_datetime`.
+
+        :param data: Number of microseconds since the epoch.
+        :type data: ``int``
+        """
+        result = datetime.datetime.fromtimestamp(data//SECOND_TO_MICROSECONDS)
+        microseconds_reminder = (data % SECOND_TO_MICROSECONDS)
+        result.replace(microsecond=microseconds_reminder)
+        return result
+
+    def _datetime_to_microseconds_since_epoch(self, value):
+        seconds = time.mktime(value.timetuple())
+        microseconds_reminder = value.time().microsecond
+        result = (int(seconds * SECOND_TO_MICROSECONDS) + microseconds_reminder)
+        return result
+
+    def __get__(self, instance, owner):
+        data = super(ComplexDateTimeField, self).__get__(instance, owner)
+        if data is None:
+            return datetime.datetime.now()
+        if isinstance(data, datetime.datetime):
+            return data
+        return self._convert_from_db(data)
+
+    def __set__(self, instance, value):
+        value = self._convert_from_datetime(value) if value else value
+        return super(ComplexDateTimeField, self).__set__(instance, value)
+
+    def validate(self, value):
+        value = self.to_python(value)
+        if not isinstance(value, datetime.datetime):
+            self.error('Only datetime objects may used in a '
+                       'ComplexDateTimeField')
+
+    def to_python(self, value):
+        original_value = value
+        try:
+            return self._convert_from_db(value)
+        except:
+            return original_value
+
+    def to_mongo(self, value):
+        value = self.to_python(value)
+        return self._convert_from_datetime(value)
+
+    def prepare_query_value(self, op, value):
+        return self._convert_from_datetime(value)
