e8e7e247afe62c478d67c35b65489f052b1b714e
Tomaz Muraus <tomaz@tomaz.me>
2015-02-17 18:07:42 +0100

    Add new "execution re-run" cli command for re-running action executions.

--- st2client/st2client/commands/action.py
+++ st2client/st2client/commands/action.py
@@ -118,7 +118,10 @@ class ActionDeleteCommand(resource.ContentPackResourceDeleteCommand):
     pass
 
 
-class ActionRunCommand(resource.ResourceCommand):
+class ActionRunCommandMixin(object):
+    """
+    Mixin class which contains utility functions related to action execution.
+    """
     attribute_display_order = ['id', 'ref', 'context', 'parameters', 'status',
                                'start_timestamp', 'result']
     attribute_transform_functions = {
@@ -127,6 +130,95 @@ class ActionRunCommand(resource.ResourceCommand):
         'parameters': format_parameters
     }
 
+    def run_and_print(self, args, **kwargs):
+        if self._print_help(args, **kwargs):
+            return
+
+        execution = self.run(args, **kwargs)
+        self.print_output(execution, table.PropertyValueTable,
+                          attributes=['all'], json=args.json,
+                          attribute_display_order=self.attribute_display_order,
+                          attribute_transform_functions=self.attribute_transform_functions)
+
+        if args.async:
+            self.print_output('To get the results, execute: \n'
+                              '    $ st2 execution get %s' % execution.id,
+                              six.text_type)
+
+    def _get_execution_result(self, execution, action_exec_mgr, args, **kwargs):
+        pending_statuses = [LIVEACTION_STATUS_SCHEDULED, LIVEACTION_STATUS_RUNNING]
+
+        if not args.async:
+            while execution.status in pending_statuses:
+                time.sleep(1)
+                if not args.json:
+                    sys.stdout.write('.')
+                execution = action_exec_mgr.get_by_id(execution.id, **kwargs)
+
+            sys.stdout.write('\n')
+
+            if self._is_error_result(result=execution.result):
+                execution.result = self._format_error_result(execution.result)
+
+        return execution
+
+    def _is_error_result(self, result):
+        if not isinstance(result, dict):
+            return False
+
+        if 'message' not in result:
+            return False
+
+        if 'traceback' not in result:
+            return False
+
+        return True
+
+    def _format_error_result(self, result):
+        result = 'Message: %s\nTraceback: %s' % (result['message'],
+                result['traceback'])
+        return result
+
+    @add_auth_token_to_kwargs_from_cli
+    def _print_help(self, args, **kwargs):
+        # Print appropriate help message if the help option is given.
+        if getattr(args, 'help', False):
+            if args.ref_or_id:
+                try:
+                    action = self.get_resource(args.ref_or_id, **kwargs)
+                    runner_mgr = self.app.client.managers['RunnerType']
+                    runner = runner_mgr.get_by_name(action.runner_type, **kwargs)
+                    parameters, required, optional, _ = self._get_params_types(runner,
+                                                                               action)
+                    print('')
+                    print(textwrap.fill(action.description))
+                    print('')
+                    if required:
+                        required = self._sort_parameters(parameters=parameters,
+                                                         names=required)
+
+                        print('Required Parameters:')
+                        [self.print_param(name, parameters.get(name))
+                            for name in required]
+                    if optional:
+                        optional = self._sort_parameters(parameters=parameters,
+                                                         names=optional)
+
+                        print('Optional Parameters:')
+                        [self.print_param(name, parameters.get(name))
+                            for name in optional]
+                except resource.ResourceNotFoundError:
+                    print('Action "%s" is not found.' % args.ref_or_id)
+                except Exception as e:
+                    print('ERROR: Unable to print help for action "%s". %s' %
+                          (args.ref_or_id, e))
+            else:
+                self.parser.print_help()
+            return True
+        return False
+
+
+class ActionRunCommand(ActionRunCommandMixin, resource.ResourceCommand):
     def __init__(self, resource, *args, **kwargs):
 
         super(ActionRunCommand, self).__init__(
@@ -295,20 +387,9 @@ class ActionRunCommand(resource.ResourceCommand):
         action_exec_mgr = self.app.client.managers['LiveAction']
 
         execution = action_exec_mgr.create(execution, **kwargs)
-
-        if not args.async:
-            while execution.status == LIVEACTION_STATUS_SCHEDULED \
-                    or execution.status == LIVEACTION_STATUS_RUNNING:
-                time.sleep(1)
-                if not args.json:
-                    sys.stdout.write('.')
-                execution = action_exec_mgr.get_by_id(execution.id, **kwargs)
-
-            sys.stdout.write('\n')
-
-            if self._is_error_result(result=execution.result):
-                execution.result = self._format_error_result(execution.result)
-
+        execution = self._get_execution_result(execution=execution,
+                                               action_exec_mgr=action_exec_mgr,
+                                               args=args, **kwargs)
         return execution
 
     @staticmethod
@@ -358,59 +439,6 @@ class ActionRunCommand(resource.ResourceCommand):
 
         return parameters, required, optional, immutable
 
-    @add_auth_token_to_kwargs_from_cli
-    def print_help(self, args, **kwargs):
-        # Print appropriate help message if the help option is given.
-        if args.help:
-            if args.ref_or_id:
-                try:
-                    action = self.get_resource(args.ref_or_id, **kwargs)
-                    runner_mgr = self.app.client.managers['RunnerType']
-                    runner = runner_mgr.get_by_name(action.runner_type, **kwargs)
-                    parameters, required, optional, _ = self._get_params_types(runner,
-                                                                               action)
-                    print('')
-                    print(textwrap.fill(action.description))
-                    print('')
-                    if required:
-                        required = self._sort_parameters(parameters=parameters,
-                                                         names=required)
-
-                        print('Required Parameters:')
-                        [self.print_param(name, parameters.get(name))
-                            for name in required]
-                    if optional:
-                        optional = self._sort_parameters(parameters=parameters,
-                                                         names=optional)
-
-                        print('Optional Parameters:')
-                        [self.print_param(name, parameters.get(name))
-                            for name in optional]
-                except resource.ResourceNotFoundError:
-                    print('Action "%s" is not found.' % args.ref_or_id)
-                except Exception as e:
-                    print('ERROR: Unable to print help for action "%s". %s' %
-                          (args.ref_or_id, e))
-            else:
-                self.parser.print_help()
-            return True
-        return False
-
-    def run_and_print(self, args, **kwargs):
-        if self.print_help(args, **kwargs):
-            return
-
-        # Execute the action.
-        instance = self.run(args, **kwargs)
-        self.print_output(instance, table.PropertyValueTable,
-                          attributes=['all'], json=args.json,
-                          attribute_display_order=self.attribute_display_order,
-                          attribute_transform_functions=self.attribute_transform_functions)
-        if args.async:
-            self.print_output('To get the results, execute: \n'
-                              '    $ st2 execution get %s' % instance.id,
-                              six.text_type)
-
     def _sort_parameters(self, parameters, names):
         """
         Sort a provided list of action parameters.
@@ -441,23 +469,6 @@ class ActionRunCommand(resource.ResourceCommand):
         sort_value = parameter.get('position', name)
         return sort_value
 
-    def _is_error_result(self, result):
-        if not isinstance(result, dict):
-            return False
-
-        if 'message' not in result:
-            return False
-
-        if 'traceback' not in result:
-            return False
-
-        return True
-
-    def _format_error_result(self, result):
-        result = 'Message: %s\nTraceback: %s' % (result['message'],
-                result['traceback'])
-        return result
-
     def _get_inherited_env_vars(self):
         env_vars = os.environ.copy()
 
@@ -478,6 +489,10 @@ class ActionExecutionBranch(resource.ResourceBranch):
             parent_parser=parent_parser, read_only=True,
             commands={'list': ActionExecutionListCommand,
                       'get': ActionExecutionGetCommand})
+
+        # Register extended commands
+        self.commands['re-run'] = ActionExecutionReRunCommand(self.resource, self.app,
+                                                              self.subparsers)
 
 
 class ActionExecutionListCommand(resource.ResourceCommand):
@@ -582,3 +597,34 @@ class ActionExecutionGetCommand(resource.ResourceCommand):
         except resource.ResourceNotFoundError:
             self.print_not_found(args.id)
             raise OperationFailureException('Execution %s not found.' % args.id)
+
+
+class ActionExecutionReRunCommand(ActionRunCommandMixin, resource.ResourceCommand):
+    def __init__(self, resource, *args, **kwargs):
+
+        super(ActionExecutionReRunCommand, self).__init__(
+            resource, kwargs.pop('name', 're-run'),
+            'A command to re-run a particular action.',
+            *args, **kwargs)
+
+        self.parser.add_argument('id', nargs='?',
+                                 metavar='id',
+                                 help='ID of action execution to re-run ')
+        self.parser.add_argument('-a', '--async',
+                                 action='store_true', dest='async',
+                                 help='Do not wait for action to finish.')
+
+    @add_auth_token_to_kwargs_from_cli
+    def run(self, args, **kwargs):
+        existing_execution = self.manager.get_by_id(args.id, **kwargs)
+
+        if not existing_execution:
+            raise resource.ResourceNotFoundError('Action execution with id "%s" cannot be found.' %
+                                                 (args.id))
+
+        action_exec_mgr = self.app.client.managers['LiveAction']
+        execution = action_exec_mgr.create(existing_execution, **kwargs)
+        execution = self._get_execution_result(execution=execution,
+                                               action_exec_mgr=action_exec_mgr,
+                                               args=args, **kwargs)
+        return execution
