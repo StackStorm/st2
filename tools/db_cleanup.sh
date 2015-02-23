60e78b410c54de1a496f59783ce29f4ab6842223
James Fryman <james@fryman.io>
2015-02-23 14:49:02 -0800

    Update db_cleanup.sh

--- tools/db_cleanup.sh
+++ tools/db_cleanup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # Simple app that runs DB cleanup commands, and repopulates from disk
+ROOT_PASSWORD=${MYSQL_PASSWORD:StackStorm}
+
 
 echo "Cleaning MongoDB Database..."
 mongo st2 --eval "db.dropDatabase();"
 
 echo "Cleaning Mistral Database..."
-mysql -uroot -pStackStorm -e "DROP DATABASE IF EXISTS mistral"
-mysql -uroot -pStackStorm -e "CREATE DATABASE mistral"
-mysql -uroot -pStackStorm -e "GRANT ALL PRIVILEGES ON mistral.* TO 'mistral'@'localhost' IDENTIFIED BY 'StackStorm'"
-mysql -uroot -pStackStorm -e "FLUSH PRIVILEGES"
+mysql -uroot -p$ROOT_PASSWORD -e "DROP DATABASE IF EXISTS mistral"
+mysql -uroot -p$ROOT_PASSWORD -e "CREATE DATABASE mistral"
+mysql -uroot -p$ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON mistral.* TO 'mistral'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD'"
+mysql -uroot -p$ROOT_PASSWORD -e "FLUSH PRIVILEGES"
 
 cd /opt/openstack/mistral
 python ./tools/sync_db.py --config-file /etc/mistral/mistral.conf
