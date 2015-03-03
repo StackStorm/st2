5b14f7a6e093b205efd5222b421800208df4efcf
Patrick Hoolboom <patrick@stackstorm.com>
2015-03-02 23:00:52 -0800

    Actually change the links this time

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -230,8 +230,8 @@ html_context = {
         ('latest', 'http://docs.stackstorm.com/latest'),
         (version, 'http://docs.stackstorm.com/%s' % version),
         # TODO(dzimine): get "prev stable version" from somewhere (?)
-        ('0.8', 'http://docs.stackstorm.com/0.7'),
-        ('0.7', 'http://docs.stackstorm.com/0.6.0')
+        ('0.8', 'http://docs.stackstorm.com/0.8'),
+        ('0.7', 'http://docs.stackstorm.com/0.7')
     ],
     'current_version': version
 }
