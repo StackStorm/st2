492ccd1dba46a97ba0a2c2fb178acbdd413b8161
Patrick Hoolboom <patrick@stackstorm.com>
2015-03-30 01:11:48 -0700

    First commit of bootstrap script for testing

---
+++ tools/setup_preinst_packs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PACK=$1
+if [[ "$PACK" == "examples" ]]; then
+    PATH="/usr/share/doc/st2/"
+elif [[ "$PACK" == "tests" ]]; then
+    PATH="/usr/share/stackstorm/"
+fi
+
+echo -e "Adding packs_base_paths for ${PATH}/${PACK}..."
+grep packs_base_path /etc/st2/st2.conf || echo -e "\n[content]\npacks_base_paths = ${PATH}/${PACK}" >> /etc/st2/st2.conf'
+
+if [[ $? ne 0 ]]; then
+    echo "Failed to add pack path!"
+    exit 2
+else
+    echo -e "Restarting St2 for new path to take affect..."
+    st2ctl restart
+    echo -e "Reloading St2 content..."
+    st2ctl reload
