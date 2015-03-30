b9438978b58b86093c71fd933451db68ebf51cf2
Patrick Hoolboom <patrick@stackstorm.com>
2015-03-30 12:24:25 -0700

    Removed trailing slashes from PATH, removed extra ', Fixed if statement for exit code, added virtualenv creation

--- tools/st2-setup-samples.sh
+++ tools/st2-setup-samples.sh
@@ -2,15 +2,18 @@
 
 PACK=$1
 if [[ "$PACK" == "examples" ]]; then
-    PATH="/usr/share/doc/st2/"
+    PATH="/usr/share/doc/st2"
 elif [[ "$PACK" == "tests" ]]; then
-    PATH="/usr/share/stackstorm/"
+    PATH="/usr/share/stackstorm"
+else
+    echo "Invalid pack: ${PACK}"
+    exit 2
 fi
 
 echo -e "Adding packs_base_paths for ${PATH}/${PACK}..."
-grep packs_base_path /etc/st2/st2.conf || echo -e "\n[content]\npacks_base_paths = ${PATH}/${PACK}" >> /etc/st2/st2.conf'
+grep packs_base_path /etc/st2/st2.conf || echo -e "\n[content]\npacks_base_paths = ${PATH}/${PACK}" >> /etc/st2/st2.conf
 
-if [[ $? ne 0 ]]; then
+if [[ $? -ne 0 ]]; then
     echo "Failed to add pack path!"
     exit 2
 else
@@ -18,3 +21,7 @@ else
     st2ctl restart
     echo -e "Reloading St2 content..."
     st2ctl reload
+fi
+
+virtualenv --system-site-packages /opt/stackstorm/virtualenvs/${PACK}
+pip install -r /opt/stackstorm/virtualenvs/${PACK}/requirements.txt -E /opt/stackstorm/virtualenvs/${PACK}
