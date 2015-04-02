8e999104d202d9c057c923bdf4deff6f94df62e7
manasdk <manasdk@gmail.com>
2015-04-01 20:54:38 -0700

    AliasExecution tests and corresponding fixtures.

--- st2api/tests/unit/controllers/exp/test_alias_execution.py
+++ st2api/tests/unit/controllers/exp/test_alias_execution.py
@@ -15,8 +15,55 @@
 
 import mock
 
+from st2common.constants.action import LIVEACTION_STATUS_SUCCEEDED
+from st2common.services import action as action_service
+from st2tests.fixturesloader import FixturesLoader
 from tests import FunctionalTest
+
+FIXTURES_PACK = 'aliases'
+
+TEST_MODELS = {
+    'actionaliases': ['alias1.yaml', 'alias2.yaml'],
+    'actions': ['action1.yaml'],
+    'runners': ['runner1.yaml']
+}
+
+TEST_LOAD_MODELS = {
+    'actionaliases': ['alias3.yaml']
+}
+
+
+class DummyActionExecution(object):
+    def __init__(self, id_=None, status=LIVEACTION_STATUS_SUCCEEDED, result=''):
+        self.id = id_
+        self.status = status
+        self.result = result
 
 
 class TestAliasExecution(FunctionalTest):
-    pass
+
+    models = None
+    alias1 = None
+    alias2 = None
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestAliasExecution, cls).setUpClass()
+        cls.models = FixturesLoader().save_fixtures_to_db(fixtures_pack=FIXTURES_PACK,
+                                                          fixtures_dict=TEST_MODELS)
+        cls.alias1 = cls.models['actionaliases']['alias1.yaml']
+        cls.alias2 = cls.models['actionaliases']['alias2.yaml']
+
+    @mock.patch.object(action_service, 'schedule',
+                       return_value=(None, DummyActionExecution(id_=1)))
+    def testBasicExecution(self, schedule):
+        alias_execution = 'alias1 Lorem ipsum value1 dolor sit "value2 value3" amet.'
+        post_resp = self._do_post(alias_execution)
+        self.assertEqual(post_resp.status_int, 200)
+        expected_parameters = {'param1': 'value1', 'param2': 'value2 value3'}
+        self.assertEquals(schedule.call_args[0][0].parameters, expected_parameters)
+
+    def _do_post(self, execution, expect_errors=False):
+        execution = {'command': execution}
+        return self.app.post_json('/exp/aliasexecution', execution,
+                                  expect_errors=expect_errors)
