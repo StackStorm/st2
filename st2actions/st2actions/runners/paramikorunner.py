4d792c2be068fe6175f28a5c19edd9f35cc5e58b
Tomaz Muraus <tomaz@tomaz.me>
2015-05-06 19:39:27 +0100

    Ignore pylint checks for currently unused and wip module.

--- st2actions/st2actions/runners/paramikorunner.py
+++ st2actions/st2actions/runners/paramikorunner.py
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: skip-file
+# Note: This module is not used.
+
 import uuid
 
 from st2actions.runners import ActionRunner
