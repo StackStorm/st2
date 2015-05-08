d9748cf25facae673e80fc14fca9ecd0a08f4a99
James Fryman <james@fryman.io>
2015-05-07 20:45:23 -0500

    normalize mistral startup command

--- tools/st2_deploy.sh
+++ tools/st2_deploy.sh
@@ -298,7 +298,7 @@ start on runlevel [2345]
 stop on runlevel [016]
 respawn
 
-exec /opt/openstack/mistral/.venv/bin/python /opt/openstack/mistral/mistral/cmd/launch.py --config-file /etc/mistral/mistral.conf --log-config-append /etc/mistral/wf_trace_logging.conf
+exec /opt/openstack/mistral/.venv/bin/python /opt/openstack/mistral/mistral/cmd/launch.py --config-file /etc/mistral/mistral.conf --log-file /var/log/mistral.log --log-config-append /etc/mistral/wf_trace_logging.conf
 mistral_upstart
 }
 
