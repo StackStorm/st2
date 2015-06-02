f2c085ec8e2803fc968cb9666d20f76f410228e3
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-06-01 17:50:06 -0700

    Allow filtering based on occurence_time in triggerinstances cli

--- st2client/st2client/commands/triggerinstance.py
+++ st2client/st2client/commands/triggerinstance.py
@@ -15,7 +15,9 @@
 
 from st2client.commands import resource
 from st2client.commands.noop import NoopCommand
+from st2client.formatters import table
 from st2client.models import TriggerInstance
+from st2client.utils.date import format_isodate
 
 
 class TriggerInstanceBranch(resource.ResourceBranch):
@@ -32,8 +34,67 @@ class TriggerInstanceBranch(resource.ResourceBranch):
             })
 
 
-class TriggerInstanceListCommand(resource.ResourceListCommand):
+class TriggerInstanceListCommand(resource.ResourceCommand):
     display_attributes = ['id', 'trigger', 'occurrence_time', 'payload']
+
+    attribute_transform_functions = {
+        'occurrence_time': format_isodate
+    }
+
+    def __init__(self, resource, *args, **kwargs):
+        super(TriggerInstanceListCommand, self).__init__(
+            resource, 'list', 'Get the list of the 50 most recent %s.' %
+            resource.get_plural_display_name().lower(),
+            *args, **kwargs)
+
+        self.group = self.parser.add_argument_group()
+        self.parser.add_argument('-n', '--last', type=int, dest='last',
+                                 default=50,
+                                 help=('List N most recent %s; '
+                                       'list all if 0.' %
+                                       resource.get_plural_display_name().lower()))
+
+        # Filter options
+        self.group.add_argument('--trigger', help='Trigger reference to filter the list.')
+
+        self.parser.add_argument('-tg', '--timestamp-gt', type=str, dest='timestamp_gt',
+                                 default=None,
+                                 help=('Only return trigger instances with occurrence_time '
+                                       'greater than the one provided. '
+                                       'Use time in the format 2000-01-01T12:00:00.000Z'))
+        self.parser.add_argument('-tl', '--timestamp-lt', type=str, dest='timestamp_lt',
+                                 default=None,
+                                 help=('Only return trigger instances with timestamp '
+                                       'lower than the one provided. '
+                                       'Use time in the format 2000-01-01T12:00:00.000Z'))
+        # Display options
+        self.parser.add_argument('-a', '--attr', nargs='+',
+                                 default=self.display_attributes,
+                                 help=('List of attributes to include in the '
+                                       'output. "all" will return all '
+                                       'attributes.'))
+        self.parser.add_argument('-w', '--width', nargs='+', type=int,
+                                 default=None,
+                                 help=('Set the width of columns in output.'))
+
+    @resource.add_auth_token_to_kwargs_from_cli
+    def run(self, args, **kwargs):
+        # Filtering options
+        if args.trigger:
+            kwargs['trigger'] = args.trigger
+        if args.timestamp_gt:
+            kwargs['timestamp_gt'] = args.timestamp_gt
+        if args.timestamp_lt:
+            kwargs['timestamp_lt'] = args.timestamp_lt
+
+        return self.manager.query(limit=args.last, **kwargs)
+
+    def run_and_print(self, args, **kwargs):
+        instances = self.run(args, **kwargs)
+        self.print_output(reversed(instances), table.MultiColumnTable,
+                          attributes=args.attr, widths=args.width,
+                          json=args.json,
+                          attribute_transform_functions=self.attribute_transform_functions)
 
 
 class TriggerInstanceGetCommand(resource.ResourceGetCommand):
