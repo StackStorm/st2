fffbd94658c030734626efc70606a18af8805782
Winson Chan <m4d.coder@gmail.com>
2015-06-04 23:17:02 +0000

    Use eventlet to run the scheduler in the notifier

--- st2actions/st2actions/scheduler.py
+++ st2actions/st2actions/scheduler.py
@@ -15,7 +15,7 @@
 
 import datetime
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import apscheduler.util as aps_utils
 from kombu import Connection
@@ -135,7 +135,7 @@ def recover_delayed_executions():
 
 
 def get_rescheduler():
-    timer = BackgroundScheduler()
+    timer = BlockingScheduler()
 
     time_spec = {
         'seconds': cfg.CONF.scheduler.rescheduling_interval,
