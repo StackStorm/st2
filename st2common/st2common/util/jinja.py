75dbe4e4be3ffdcf9077a4e7dcd0e9703723aefe
manasdk <manasdk@gmail.com>
2015-06-09 16:47:55 -0700

    Some new filters for our jinja templates
    * Templates that work with regex and also some useful support around semver
    * Tests which should also show how to use the filters in a jinja string

--- st2common/st2common/util/jinja.py
+++ st2common/st2common/util/jinja.py
@@ -14,12 +14,117 @@
 # limitations under the License.
 
 import json
-
 import jinja2
 import six
+import re
+
+import semver
 
 
-def render_values(mapping=None, context=None):
+class CustomFilters(object):
+    '''
+    Collection of CustomFilters for jinja2
+    '''
+
+    ###############
+    # regex filters
+    @staticmethod
+    def _get_regex_flags(ignorecase=False):
+        return re.I if ignorecase else 0
+
+    @staticmethod
+    def _regex_match(value, pattern='', ignorecase=False):
+        if not isinstance(value, six.string_types):
+            value = str(value)
+        flags = CustomFilters._get_regex_flags(ignorecase)
+        return bool(re.match(pattern, value, flags))
+
+    @staticmethod
+    def _regex_replace(value='', pattern='', replacement='', ignorecase=False):
+        if not isinstance(value, six.string_types):
+            value = str(value)
+        flags = CustomFilters._get_regex_flags(ignorecase)
+        regex = re.compile(pattern, flags)
+        return regex.sub(replacement, value)
+
+    @staticmethod
+    def _regex_search(value, pattern='', ignorecase=False):
+        if not isinstance(value, six.string_types):
+            value = str(value)
+        flags = CustomFilters._get_regex_flags(ignorecase)
+        return bool(re.search(pattern, value, flags))
+
+    #################
+    # version filters
+    @staticmethod
+    def _version_compare(value, pattern):
+        return semver.compare(value, pattern)
+
+    @staticmethod
+    def _version_more_than(value, pattern):
+        return semver.compare(value, pattern) == 1
+
+    @staticmethod
+    def _version_less_than(value, pattern):
+        return semver.compare(value, pattern) == -1
+
+    @staticmethod
+    def _version_equal(value, pattern):
+        return semver.compare(value, pattern) == 0
+
+    @staticmethod
+    def _version_match(value, pattern):
+        return semver.match(value, pattern)
+
+    @staticmethod
+    def _version_bump_major(value):
+        return semver.bump_major(value)
+
+    @staticmethod
+    def _version_bump_minor(value):
+        return semver.bump_minor(value)
+
+    @staticmethod
+    def _version_bump_patch(value):
+        return semver.bump_patch(value)
+
+    @staticmethod
+    def _version_strip_patch(value):
+        return "{major}.{minor}".format(**semver.parse(value))
+
+    @staticmethod
+    def get_filters():
+        return {
+            'regex_match': CustomFilters._regex_match,
+            'regex_replace': CustomFilters._regex_replace,
+            'regex_search': CustomFilters._regex_search,
+            'version_compare': CustomFilters._version_compare,
+            'version_more_than': CustomFilters._version_more_than,
+            'version_less_than': CustomFilters._version_less_than,
+            'version_equal': CustomFilters._version_equal,
+            'version_match': CustomFilters._version_match,
+            'version_bump_major': CustomFilters._version_bump_major,
+            'version_bump_minor': CustomFilters._version_bump_minor,
+            'version_bump_patch': CustomFilters._version_bump_patch,
+            'version_strip_patch': CustomFilters._version_strip_patch
+        }
+
+
+def get_jinja_environment(allow_undefined=False):
+    '''
+    jinja2.Environment object that is setup with right behaviors and custom filters.
+
+    :param strict_undefined: If should allow undefined variables in templates
+    :type strict_undefined: ``bool``
+
+    '''
+    undefined = jinja2.Undefined if allow_undefined else jinja2.StrictUndefined
+    env = jinja2.Environment(undefined=undefined)
+    env.filters.update(CustomFilters.get_filters())
+    return env
+
+
+def render_values(mapping=None, context=None, allow_undefined=False):
     """
     Render an incoming mapping using context provided in context using Jinja2. Returns a dict
     containing rendered mapping.
@@ -36,7 +141,7 @@ def render_values(mapping=None, context=None):
     if not context or not mapping:
         return mapping
 
-    env = jinja2.Environment(undefined=jinja2.StrictUndefined)
+    env = get_jinja_environment(allow_undefined=allow_undefined)
     rendered_mapping = {}
     for k, v in six.iteritems(mapping):
         # jinja2 works with string so transform list and dict to strings.
