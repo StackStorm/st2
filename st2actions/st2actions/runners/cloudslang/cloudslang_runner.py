b03154127bada6a2024ef93bef7318378ccf41d1
Tomaz Muraus <tomaz@tomaz.me>
2015-06-11 21:13:32 +0800

    Use with - make sure the file is closed and change some variable names to be more explicit.

--- st2actions/st2actions/runners/cloudslang/cloudslang_runner.py
+++ st2actions/st2actions/runners/cloudslang/cloudslang_runner.py
@@ -14,12 +14,13 @@
 # limitations under the License.
 
 import uuid
-
 import os
 import tempfile
 import yaml
+
 from oslo.config import cfg
 from eventlet.green import subprocess
+
 from st2common.util.green.shell import run_command
 from st2common.util.shell import kill_process
 from st2common.util.shell import quote_unix
@@ -61,43 +62,55 @@ class CloudSlangRunner(ActionRunner):
                                                    LOCAL_RUNNER_DEFAULT_ACTION_TIMEOUT)
 
     def run(self, action_parameters):
-        inputs_file = None
+        inputs_file_path = None
+
         try:
             LOG.debug('    action_parameters = %s', action_parameters)
 
             has_inputs = self._inputs is not None
-            inputs_file = self.prepare_inputs_file(has_inputs, inputs_file)
+            inputs_file_path = self.prepare_inputs_file(has_inputs=has_inputs)
 
-            command = self.prepare_command(has_inputs, inputs_file)
+            command = self.prepare_command(has_inputs=has_inputs,
+                                           inputs_file_path=inputs_file_path)
 
             result, status = self.run_cli_command(command)
             return status, jsonify.json_loads(result, CloudSlangRunner.KEYS_TO_TRANSFORM), None
         finally:
-            if inputs_file and os.path.exists(inputs_file.name):
-                os.remove(inputs_file.name)
+            if inputs_file_path and os.path.isfile(inputs_file_path):
+                os.remove(inputs_file_path)
 
-    def prepare_inputs_file(self, has_inputs, inputs_file):
-        if has_inputs:
-            inputs_file = tempfile.NamedTemporaryFile(delete=False)
-            LOG.info(self._inputs)
-            yaml_inputs = yaml.safe_dump(self._inputs, default_flow_style=False)
-            inputs_file.write(yaml_inputs)
-            inputs_file.seek(0)
+    def prepare_inputs_file(self, has_inputs):
+        """
+        :type has_inputs: ``bool``
 
-            for line in inputs_file:
-                LOG.info(line.rstrip())
-        return inputs_file
+        :return: Path to the temporary file holding inputs YAML definition.
+        """
+        if not has_inputs:
+            return None
 
-    def prepare_command(self, has_inputs, inputs_file):
+        LOG.debug('Inputs: %s ', self._inputs)
+
+        inputs_file = tempfile.NamedTemporaryFile(delete=False)
+        inputs_file_path = inputs_file.name
+        yaml_inputs = yaml.safe_dump(self._inputs, default_flow_style=False)
+
+        with open(inputs_file_path, 'w') as fp:
+            fp.write(yaml_inputs)
+
+        LOG.debug('YAML serialized inputs: %s', yaml_inputs)
+
+        return inputs_file_path
+
+    def prepare_command(self, has_inputs, inputs_file_path):
         LOG.info(self._cloudslang_home)
-        cloudslang_binary = os.path.join(self._cloudslang_home, "bin/cslang")
+        cloudslang_binary = os.path.join(self._cloudslang_home, 'bin/cslang')
         LOG.info(cloudslang_binary)
         command_args = ['--f', self._path,
-                        '--if', inputs_file.name if has_inputs else "",
+                        '--if', inputs_file_path if has_inputs else '',
                         '--cp', self._cloudslang_home]
         command = cloudslang_binary + " run " + " ".join([quote_unix(arg) for arg in command_args])
         LOG.info('Executing action via CloudSlangRunner: %s', self.runner_id)
-        LOG.debug('command is: %s', command)
+        LOG.debug('Command is: %s', command)
         return command
 
     def run_cli_command(self, command):
@@ -105,10 +118,12 @@ class CloudSlangRunner(ActionRunner):
             cmd=command, stdin=None,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             shell=True, timeout=self._timeout, kill_func=kill_process)
+
         error = None
         if timed_out:
             error = 'Action failed to complete in %s seconds' % self._timeout
             exit_code = -9
+
         succeeded = (exit_code == 0)
         result = {
             'failed': not succeeded,
@@ -119,6 +134,7 @@ class CloudSlangRunner(ActionRunner):
         }
         if error:
             result['error'] = error
+
         status = LIVEACTION_STATUS_SUCCEEDED if succeeded else LIVEACTION_STATUS_FAILED
         self._log_action_completion(logger=LOG, result=result, status=status, exit_code=exit_code)
         return result, status
