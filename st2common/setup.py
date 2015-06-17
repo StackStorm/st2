93a1baa05b8c4327ff6476f5a1fc4f1efb24258c
Denis Baryshev <dennybaa@gmail.com>
2015-06-18 01:36:48 +0300

    setup.py new requirements idea

--- st2common/setup.py
+++ st2common/setup.py
@@ -14,24 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+import re
+import os.path
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+def fetch_requirements():
+    links = []
+    reqs = []
+    for req in parse_requirements('requirements.txt', session=False):
+        if req.link:
+            links.append(str(req.link))
+        reqs.append(str(req.req))
+    return (reqs, links)
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+version_file = os.path.join(current_dir, '../st2client/st2client/__init__.py')
+with open(version_file, 'r') as f:
+    vmatch = re.search(r'__version__ = [\'\"](.*)[\'\"]$', f.read(), flags=re.MULTILINE)
+
+
+install_reqs, dep_links = fetch_requirements()
+ST2_COMPONENT = os.path.basename(current_dir)
+ST2_VERSION = vmatch.group(1)
+
 
 setup(
-    name='st2common',
-    version='0.4.0',
-    description='',
+    name=ST2_COMPONENT,
+    version=ST2_VERSION,
+    description='{} component'.format(ST2_COMPONENT),
     author='StackStorm',
     author_email='info@stackstorm.com',
-    install_requires=[
-        "pecan",
-    ],
-    test_suite='st2common',
+    install_requires=install_reqs,
+    dependency_links=dep_links,
+    test_suite=ST2_COMPONENT,
     zip_safe=False,
     include_package_data=True,
-    packages=find_packages(exclude=['ez_setup'])
+    packages=find_packages(exclude=['setuptools'])
 )
