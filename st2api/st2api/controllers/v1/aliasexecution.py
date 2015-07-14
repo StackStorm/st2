92549cce9d748441f0434dafe8a9188203b2aada
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-07-14 16:42:58 -0700

    Move ActionAlias and ActionAliasExecution controllers to /v1/

---
+++ st2api/st2api/controllers/v1/aliasexecution.py
@@ -0,0 +1,126 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jsonschema
+import pecan
+import six
+
+from pecan import rest
+from st2common import log as logging
+from st2common.models.api.base import jsexpose
+from st2common.models.api.action import AliasExecutionAPI
+from st2common.models.api.auth import get_system_username
+from st2common.models.db.liveaction import LiveActionDB
+from st2common.models.db.notification import NotificationSchema, NotificationSubSchema
+from st2common.models.utils import action_alias_utils, action_param_utils
+from st2common.persistence.actionalias import ActionAlias
+from st2common.services import action as action_service
+from st2common.util import reference
+
+
+http_client = six.moves.http_client
+
+LOG = logging.getLogger(__name__)
+
+CAST_OVERRIDES = {
+    'array': (lambda cs_x: [v.strip() for v in cs_x.split(',')])
+}
+
+
+class ActionAliasExecutionController(rest.RestController):
+
+    @jsexpose(body_cls=AliasExecutionAPI, status_code=http_client.OK)
+    def post(self, payload):
+        action_alias_name = payload.name if payload else None
+
+        if not action_alias_name:
+            pecan.abort(http_client.BAD_REQUEST, 'Alias execution "name" is required')
+
+        format = payload.format or ''
+        command = payload.command or ''
+
+        try:
+            action_alias_db = ActionAlias.get_by_name(action_alias_name)
+        except ValueError:
+            action_alias_db = None
+
+        if not action_alias_db:
+            msg = 'Unable to identify action alias with name "%s".' % (action_alias_name)
+            pecan.abort(http_client.NOT_FOUND, msg)
+            return
+
+        if not action_alias_db.enabled:
+            msg = 'Action alias with name "%s" is disabled.' % (action_alias_name)
+            pecan.abort(http_client.BAD_REQUEST, msg)
+            return
+
+        execution_parameters = self._extract_parameters(action_alias_db=action_alias_db,
+                                                        format=format,
+                                                        param_stream=command)
+        notify = self._get_notify_field(payload)
+        execution = self._schedule_execution(action_alias_db=action_alias_db,
+                                             params=execution_parameters,
+                                             notify=notify)
+
+        return str(execution.id)
+
+    def _tokenize_alias_execution(self, alias_execution):
+        tokens = alias_execution.strip().split(' ', 1)
+        return (tokens[0], tokens[1] if len(tokens) > 1 else None)
+
+    def _extract_parameters(self, action_alias_db, format, param_stream):
+        if action_alias_db.formats and format in action_alias_db.formats:
+            alias_format = format
+        else:
+            alias_format = None
+
+        parser = action_alias_utils.ActionAliasFormatParser(alias_format=alias_format,
+                                                            param_stream=param_stream)
+        return parser.get_extracted_param_value()
+
+    def _get_notify_field(self, payload):
+        on_complete = NotificationSubSchema()
+        on_complete.channels = [payload.notification_channel]
+        on_complete.data = {
+            'user': payload.user,
+            'source_channel': payload.source_channel
+        }
+        notify = NotificationSchema()
+        notify.on_complete = on_complete
+        return notify
+
+    def _schedule_execution(self, action_alias_db, params, notify):
+        try:
+            # prior to shipping off the params cast them to the right type.
+            params = action_param_utils.cast_params(action_ref=action_alias_db.action_ref,
+                                                    params=params,
+                                                    cast_overrides=CAST_OVERRIDES)
+            context = {
+                'action_alias_ref': reference.get_ref_from_model(action_alias_db),
+                'user': get_system_username()
+            }
+            liveaction = LiveActionDB(action=action_alias_db.action_ref, context=context,
+                                      parameters=params, notify=notify)
+            _, action_execution_db = action_service.request(liveaction)
+            return action_execution_db
+        except ValueError as e:
+            LOG.exception('Unable to execute action.')
+            pecan.abort(http_client.BAD_REQUEST, str(e))
+        except jsonschema.ValidationError as e:
+            LOG.exception('Unable to execute action. Parameter validation failed.')
+            pecan.abort(http_client.BAD_REQUEST, str(e))
+        except Exception as e:
+            LOG.exception('Unable to execute action. Unexpected error encountered.')
+            pecan.abort(http_client.INTERNAL_SERVER_ERROR, str(e))
