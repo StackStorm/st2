5fc3eac89310420cdac2fa84a2564857f2bf4ce7
armab <blog@arma.biz>
2015-07-17 16:38:27 +0300

    Remove code style checks from Travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,11 +6,7 @@ if [ -z ${TASK} ]; then
   exit 2
 fi
 
-if [ ${TASK} == 'checks' ]; then
-  make .flake8
-  make .pylint
-  make .docs
-elif [ ${TASK} == 'unit' ]; then
+if [ ${TASK} == 'unit' ]; then
   # compile .py files, useful as compatibility syntax check
   make compile
   make unit-tests-coverage-xml
