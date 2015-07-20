f6f30134402e1699f41cf7c7f4ddcf19bfb78db6
Tomaz Muraus <tomaz@tomaz.me>
2015-07-20 14:57:38 +0800

    Add module with permission checking decorators.

--- st2common/st2common/rbac/decorators.py
+++ st2common/st2common/rbac/decorators.py
@@ -12,3 +12,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+This module contains permission checking decorators for wrapping pecan request handler methods.
+"""
+
+from functools import wraps
+
+import pecan
+
+from st2common.rbac import utils
+
+__all__ = [
+    'request_user_is_admin',
+    'request_user_has_permission'
+]
+
+
+def request_user_is_admin():
+    def decorate(func):
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            utils.assert_request_user_is_admin(request=pecan.request)
+            return func(*args, **kwargs)
+        return func_wrapper
+    return decorate
+
+
+def request_user_has_permission(resource_db, permission_type):
+    def decorate(func):
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            utils.assert_request_user_has_permission(request=pecan.request,
+                                                     resource_db=resource_db,
+                                                     permission_type=permission_type)
+            return func(*args, **kwargs)
+        return func_wrapper
+    return decorate
