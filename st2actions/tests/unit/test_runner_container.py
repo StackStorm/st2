441d89cee28116344e163cef0be8a2aae3072c59
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-07-30 14:02:30 -0700

    Test case for non utf-8

--- st2actions/tests/unit/test_runner_container.py
+++ st2actions/tests/unit/test_runner_container.py
@@ -15,6 +15,7 @@
 
 import mock
 
+from bson.errors import InvalidStringData
 from oslo_config import cfg
 from st2common.constants import action as action_constants
 from st2actions.runners import get_runner
@@ -39,8 +40,9 @@ from st2tests.fixturesloader import FixturesLoader
 from st2actions.container.base import get_runner_container
 
 TEST_FIXTURES = {
-    'runners': ['testrunner1.yaml', 'testfailingrunner1.yaml', 'testasyncrunner1.yaml'],
-    'actions': ['action1.yaml', 'async_action1.yaml', 'action-invalid-runner.yaml']
+    'runners': ['run-local.yaml', 'testrunner1.yaml', 'testfailingrunner1.yaml',
+                'testasyncrunner1.yaml'],
+    'actions': ['local.yaml', 'action1.yaml', 'async_action1.yaml', 'action-invalid-runner.yaml']
 }
 
 FIXTURES_PACK = 'generic'
@@ -61,6 +63,7 @@ class RunnerContainerTest(DbTestCase):
             fixtures_pack=FIXTURES_PACK, fixtures_dict=TEST_FIXTURES)
         RunnerContainerTest.runnertype_db = models['runners']['testrunner1.yaml']
         RunnerContainerTest.action_db = models['actions']['action1.yaml']
+        RunnerContainerTest.local_action_db = models['actions']['local.yaml']
         RunnerContainerTest.async_action_db = models['actions']['async_action1.yaml']
         RunnerContainerTest.failingaction_db = models['actions']['action-invalid-runner.yaml']
 
@@ -83,7 +86,7 @@ class RunnerContainerTest(DbTestCase):
         params = {
             'actionstr': 'bar'
         }
-        liveaction_db = self._get_action_exec_db_model(RunnerContainerTest.action_db, params)
+        liveaction_db = self._get_liveaction_model(RunnerContainerTest.action_db, params)
         liveaction_db = LiveAction.add_or_update(liveaction_db)
         executions.create_execution_object(liveaction_db)
         # Assert that execution ran successfully.
@@ -102,6 +105,24 @@ class RunnerContainerTest(DbTestCase):
         }
 
         self.assertDictEqual(liveaction_db.context, context)
+
+    def test_dispatch_non_utf8_result(self):
+        runner_container = get_runner_container()
+        params = {
+            'cmd': 'python -c \'print \"\\x82\"\''
+        }
+        liveaction_db = self._get_liveaction_model(RunnerContainerTest.local_action_db, params)
+        liveaction_db = LiveAction.add_or_update(liveaction_db)
+        executions.create_execution_object(liveaction_db)
+        print(liveaction_db)
+
+        try:
+            runner_container.dispatch(liveaction_db)
+            self.fail('Mongo won\'t handle non UTF-8 strings. Should have failed.')
+        except InvalidStringData:
+            liveaction_db = LiveAction.get_by_id(liveaction_db.id)
+            result = liveaction_db.result
+            print(result)
 
     def test_dispatch_runner_failure(self):
         runner_container = get_runner_container()
@@ -123,7 +144,7 @@ class RunnerContainerTest(DbTestCase):
             'actionstr': 'foo',
             'actionint': 20
         }
-        liveaction_db = self._get_action_exec_db_model(RunnerContainerTest.action_db, params)
+        liveaction_db = self._get_liveaction_model(RunnerContainerTest.action_db, params)
         liveaction_db = LiveAction.add_or_update(liveaction_db)
         executions.create_execution_object(liveaction_db)
         # Assert that execution ran successfully.
@@ -140,7 +161,7 @@ class RunnerContainerTest(DbTestCase):
             'actionint': 20,
             'async_test': True
         }
-        liveaction_db = self._get_action_exec_db_model(RunnerContainerTest.async_action_db, params)
+        liveaction_db = self._get_liveaction_model(RunnerContainerTest.async_action_db, params)
         liveaction_db = LiveAction.add_or_update(liveaction_db)
         executions.create_execution_object(liveaction_db)
         # Assert that execution ran without exceptions.
@@ -155,7 +176,7 @@ class RunnerContainerTest(DbTestCase):
         self.assertTrue(found.query_context is not None)
         self.assertTrue(found.query_module is not None)
 
-    def _get_action_exec_db_model(self, action_db, params):
+    def _get_liveaction_model(self, action_db, params):
         status = action_constants.LIVEACTION_STATUS_REQUESTED
         start_timestamp = date_utils.get_datetime_utc_now()
         action_ref = ResourceReference(name=action_db.name, pack=action_db.pack).ref
