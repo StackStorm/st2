e9a14968451344a0bae1c2449780742551c8b357
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2015-08-03 18:15:16 -0700

    Add support for testing dir copy

--- st2common/bin/paramiko_ssh_evenlets_tester.py
+++ st2common/bin/paramiko_ssh_evenlets_tester.py
@@ -17,15 +17,23 @@ eventlet.monkey_patch(
     time=True)
 
 
-def main(user, pkey, hosts_str, cmd, path):
+def main(user, pkey, hosts_str, cmd, file_path, dir_path):
     hosts = hosts_str.split(",")
     client = ParallelSSHClient(user, pkey, hosts)
     pp = pprint.PrettyPrinter(indent=4)
 
-    if path:
-        if not os.path.exists(path):
+    if file_path:
+        if not os.path.exists(file_path):
             raise Exception('File not found.')
-        results = client.put(path, '/home/lakshmi/test')
+        results = client.put(file_path, '/home/lakshmi/test_file', mode="0660")
+        pp.pprint('Copy results: \n%s' % results)
+        results = client.run('ls -rlth')
+        pp.pprint('ls results: \n%s' % results)
+
+    if dir_path:
+        if not os.path.exists(dir_path):
+            raise Exception('File not found.')
+        results = client.put(dir_path, '/home/lakshmi/', mode="0660")
         pp.pprint('Copy results: \n%s' % results)
         results = client.run('ls -rlth')
         pp.pprint('ls results: \n%s' % results)
@@ -45,9 +53,11 @@ if __name__ == '__main__':
                         help='SSH user name.')
     parser.add_argument('--cmd', required=False,
                         help='Command to run on host.')
-    parser.add_argument('--path', required=False,
-                        help='Path to copy to remote host.')
+    parser.add_argument('--file', required=False,
+                        help='Path of file to copy to remote host.')
+    parser.add_argument('--dir', required=False,
+                        help='Path of dir to copy to remote host.')
     args = parser.parse_args()
 
     main(user=args.user, pkey=args.private_key, hosts_str=args.hosts, cmd=args.cmd,
-         path=args.path)
+         file_path=args.file, dir_path=args.dir)
