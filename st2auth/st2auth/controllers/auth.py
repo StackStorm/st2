27a0f3f00b2af487db9f1de61ec5948683bb3937
Tomaz Muraus <tomaz@tomaz.me>
2015-08-13 19:33:30 +0200

    Make sure auth API endpoints returns JSON on errors.

--- st2auth/st2auth/controllers/auth.py
+++ st2auth/st2auth/controllers/auth.py
@@ -23,6 +23,7 @@ from oslo_config import cfg
 from st2common.exceptions.auth import TTLTooLargeException
 from st2common.models.api.base import jsexpose
 from st2common.models.api.auth import TokenAPI
+from st2common.util.jsonify import json_encode
 from st2common.services.access import create_token
 from st2common import log as logging
 from st2auth.backends import get_backend_instance
@@ -114,7 +115,12 @@ class TokenController(rest.RestController):
 
     def _abort_request(self, status_code=http_client.UNAUTHORIZED,
                        message='Invalid or missing credentials'):
-        pecan.abort(status_code, message)
+        body = json_encode({
+            'faultstring': message
+        })
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+        pecan.abort(status_code=status_code, detail=body, headers=headers)
 
     def _process_successful_response(self, token):
         api_url = cfg.CONF.auth.api_url
