a9f3120051991afd2209e67eb57ff33e67f903cf
Tomaz Muraus <tomaz@tomaz.me>
2015-08-18 18:36:59 +0200

    Improve logging in wait_for_ssh action, remove unecessary second sleep.

--- contrib/linux/actions/wait_for_ssh.py
+++ contrib/linux/actions/wait_for_ssh.py
@@ -13,13 +13,15 @@ class BaseAction(Action):
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        for x in range(retries):
+        for index in range(retries):
+            attempt = index + 1
+
             try:
+                self.logger.debug('SSH connection attempt: %s' % (attempt))
                 client.connect(hostname=hostname, username=username, pkey=key)
                 return True
-            except Exception, e:
-                self.logger.info(e)
+            except Exception as e:
+                self.logger.info('Attempt %s failed (%s), sleeping...' % (attempt, str(e)))
                 time.sleep(ssh_timeout)
-            time.sleep(20)
 
-        raise Exception("Exceeded max retries")
+        raise Exception('Exceeded max retries (%s)' % (retries))
