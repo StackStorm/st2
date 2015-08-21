c5f25e1016916076a4e21001450bc85e55f3b3cf
Edward Medvedev <ed@doesnotcompute.ru>
2015-08-21 19:03:26 +0600

    Typo

--- st2api/st2api/controllers/v1/packviews.py
+++ st2api/st2api/controllers/v1/packviews.py
@@ -99,7 +99,7 @@ class FileController(BaseFileController):
         pack_db = self._get_by_name_or_id(name_or_id=name_or_id)
 
         if not pack_db:
-            raise StackStormDBObjectNotFoundError('Pack with name_or_id "%s" doesn't exist' % (name_or_id))
+            raise StackStormDBObjectNotFoundError('Pack with name_or_id "%s" does not exist' % (name_or_id))
 
         if not file_path_components:
             raise ValueError('Missing file path')
