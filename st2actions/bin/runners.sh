260ae83f6eeb03aaef7b9733c7ed373f7c7468a3
Denis Baryshev <dennybaa@gmail.com>
2015-08-25 18:35:05 +0300

    added runners.sh wrapper to start multiple services

---
+++ st2actions/bin/runners.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f /etc/debian/version ]; then
+  >&2 echo "Runners wrapper doesn't support debian platforms right now!"
+  exit 1
+else
+  # redhatish and simply other platforms, they must support systemd
+  sv=systemd
+  svbin=/usr/bin/systemctl
+  if [ -z $svbin ]; then
+    >&2 echo "Non systemd platforms are not supported"
+    exit 1
+  fi
+fi
+
+WORKERSNUM=${WORKERSNUM:-`nproc`}
+action="$1"; shift;
+for i in `seq $WORKERSNUM`; do
+  [ "$sv" = "systemd" ] && $svbin $action $@
+done
