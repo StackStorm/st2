fd30ca3475de4c845286655b0d9a4944873a9e6e
Denis Baryshev <dennybaa@gmail.com>
2015-09-02 15:23:23 +0300

    runners.sh support for sysv, systemd, upstart platforms

--- st2actions/bin/runners.sh
+++ st2actions/bin/runners.sh
@@ -1,20 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -f /etc/debian/version ]; then
-  >&2 echo "Runners wrapper doesn't support debian platforms right now!"
-  exit 1
-else
-  # redhatish and simply other platforms, they must support systemd
+SYSTEMDCTL=/usr/bin/systemctl
+UPSTARTCTL=/sbin/initctl
+WORKER=st2actionrunner-worker
+
+# 1. Choose init type
+if [ -x $SYSTEMDCTL ]; then
   sv=systemd
-  svbin=/usr/bin/systemctl
-  if [ -x $svbin ]; then
-    >&2 echo "Non systemd platforms are not supported"
+  svbin=$SYSTEMDCTL
+elif [ -x $UPSTARTCTL ]; then
+  sv=upstart
+  svbin=$UPSTARTCTL
+else
+  # Old debians, redhats and centos, amazon etc
+  sv=sysv
+  svbin=/etc/init.d/$WORKER
+  if [ ! -x $svbin ]; then
+    >&2 echo "Init file not found: $svbin"
+    >&2 echo "Unknown platform, we support ONLY debian, systemd and sysv!"
     exit 1
   fi
 fi
 
+# 2. Spwan workers
 WORKERSNUM=${WORKERSNUM:-`nproc`}
 action="$1"; shift;
+rs=0
 for i in `seq $WORKERSNUM`; do
-  [ "$sv" = "systemd" ] && $svbin $action st2actionrunner@$i
+  if [ $sv = systemd ]; then
+    $svbin $action st2actionrunner@$i
+  elif [ $sv = upstart ]; then
+    $svbin $action st2actionrunner WORKERID=$i
+  elif [ $sv = sysv ]; then
+    WORKERID=$i $svbin $action
+  fi
+  cmdrs=$?
+  [ $cmdrs -gt 0 ] && rs=$cmdrs
 done
+
+exit $rs
