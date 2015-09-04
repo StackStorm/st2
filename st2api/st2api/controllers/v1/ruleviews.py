ed3f147e12ce38d499204874d3ae8b3977c1482e
manasdk <manas@stackstorm.com>
2015-09-04 16:01:54 -0700

    Improvements in DB querying i.e. some optimization
    * The Query method using the obscure mongoengine appraoch is esoteric enough
      that I decided to leave it in the rulesviews module. It needs a lot more
      thinking to generalize and promote it ot a bulk_query methods.

--- st2api/st2api/controllers/v1/ruleviews.py
+++ st2api/st2api/controllers/v1/ruleviews.py
@@ -15,10 +15,13 @@
 
 import six
 
+from mongoengine.queryset import Q
+
 from st2common import log as logging
 from st2api.controllers import resource
 from st2common.models.api.rule import RuleViewAPI
 from st2common.models.api.base import jsexpose
+from st2common.models.system.common import ResourceReference
 from st2common.persistence.action import Action
 from st2common.persistence.rule import Rule
 from st2common.persistence.trigger import TriggerType, Trigger
@@ -65,15 +68,15 @@ class RuleViewController(resource.ContentPackResourceController):
         action_by_refs, trigger_by_refs, trigger_type_by_refs = self._get_referenced_models(rules)
 
         for rule in rules:
-            action_db = action_by_refs[rule.action['ref']]
+            action_db = action_by_refs.get(rule.action['ref'], None)
             rule.action['description'] = action_db.description if action_db else ''
 
-            trigger_db = trigger_by_refs[rule.trigger['ref']]
+            trigger_db = trigger_by_refs.get(rule.trigger['ref'], None)
             rule.trigger['description'] = trigger_db.description if trigger_db else ''
 
             # If description is not found in trigger get description from triggertype
             if not rule.trigger['description']:
-                trigger_type_db = trigger_type_by_refs[rule.trigger['type']]
+                trigger_type_db = trigger_type_by_refs.get(rule.trigger['type'], None)
                 rule.trigger['description'] = trigger_type_db.description if trigger_type_db else ''
 
         return rules
@@ -96,13 +99,45 @@ class RuleViewController(resource.ContentPackResourceController):
         trigger_by_refs = {}
         trigger_type_by_refs = {}
 
-        for ref in action_refs:
-            action_by_refs[ref] = Action.get_by_ref(ref)
+        # The functions that will return args that can used to query.
+        ref_query_args = lambda resource_ref: {'ref': resource_ref.ref}
+        name_pack_query_args = lambda resource_ref: {'name': resource_ref.name,
+                                                     'pack': resource_ref.pack}
+        action_dbs = self._get_entities(model_persistence=Action,
+                                        refs=action_refs,
+                                        query_args=ref_query_args)
+        for action_db in action_dbs:
+            action_by_refs[action_db.ref] = action_db
 
-        for ref in trigger_refs:
-            trigger_by_refs[ref] = Trigger.get_by_ref(ref)
+        trigger_dbs = self._get_entities(model_persistence=Trigger,
+                                         refs=trigger_refs,
+                                         query_args=name_pack_query_args)
+        for trigger_db in trigger_dbs:
+            trigger_by_refs[trigger_db.get_reference().ref] = trigger_db
 
-        for ref in trigger_type_refs:
-            trigger_type_by_refs[ref] = TriggerType.get_by_ref(ref)
+        trigger_type_dbs = self._get_entities(model_persistence=TriggerType,
+                                              refs=trigger_type_refs,
+                                              query_args=name_pack_query_args)
+        for trigger_type_db in trigger_type_dbs:
+            trigger_type_by_refs[trigger_type_db.get_reference().ref] = trigger_type_db
 
         return (action_by_refs, trigger_by_refs, trigger_type_by_refs)
+
+    def _get_entities(self, model_persistence, refs, query_args):
+        """
+        Returns all the entities for the supplied refs. model_persistence is the persistence
+        object that will be used to get to the correct query method and the query_args function
+        to return the ref specific query argument.
+
+        This is such a weirdly specific method that it is likely better only in this context.
+        """
+        q = None
+        for ref in refs:
+            resource_ref = ResourceReference.from_string_reference(ref=ref)
+            if not q:
+                q = Q(**query_args(resource_ref))
+            else:
+                q |= Q(**query_args(resource_ref))
+        if q:
+            return model_persistence._get_impl().model.objects(q)
+        return []
