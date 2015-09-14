26f1f46bdcbda986ee6a72a6fbf3f466e4843832
Tomaz Muraus <tomaz@tomaz.me>
2015-09-14 19:22:23 +0200

    Fix packs.info so it correctly searches for pack in all the search dirs.

--- contrib/packs/actions/pack_mgmt/info.py
+++ contrib/packs/actions/pack_mgmt/info.py
@@ -14,21 +14,42 @@
 # limitations under the License.
 
 import os
-import sys
 import json
 
 from st2actions.runners.pythonrunner import Action
+from st2common.content.utils import get_packs_base_paths
 
 GITINFO_FILE = '.gitinfo'
 
 
 class PackInfo(Action):
     def run(self, pack, pack_dir="/opt/stackstorm/packs"):
-        gitinfo = os.path.join(pack_dir, pack, GITINFO_FILE)
-        try:
-            with open(gitinfo) as data_file:
-                details = json.load(data_file)
-                return details
-        except:
-            error = 'Pack %s doesn\'t exist or it doesn\'t contain .gitinfo file' % (pack)
+        packs_base_paths = get_packs_base_paths()
+
+        pack_git_info_path = None
+        for packs_base_path in packs_base_paths:
+            git_info_path = os.path.join(packs_base_path, pack, GITINFO_FILE)
+
+            if os.path.isfile(git_info_path):
+                pack_git_info_path = git_info_path
+                break
+
+        error = ('Pack %s doesn\'t exist or it doesn\'t contain a valid .gitinfo file' % (pack))
+
+        if not pack_git_info_path:
             raise Exception(error)
+
+        try:
+            details = self._parse_git_info_file(git_info_path)
+        except Exception as e:
+            error = ('Pack %s doesn\'t contain a valid .gitinfo file: %s' % (pack, str(e)))
+            raise Exception(error)
+
+        return details
+
+    def _parse_git_info_file(self, file_path):
+        with open(file_path) as data_file:
+            details = json.load(data_file)
+            return details
+
+        return details
