3d8e01a4962e92dce0461cd053daefa566e39965
manasdk <manas@stackstorm.com>
2015-09-18 14:04:32 -0700

    Delay config evaluation by skipping decorator
    * Decorators lead to consumption of config before config is setup.
      This delays config read and avoids any import gymnastics or awkward
      object proxies.

--- st2common/st2common/persistence/db_init.py
+++ st2common/st2common/persistence/db_init.py
@@ -16,7 +16,7 @@
 import mongoengine
 
 from oslo_config import cfg
-from retrying import retry
+import retrying
 
 from st2common import log as logging
 from st2common.models.db import db_setup
@@ -26,10 +26,6 @@ __all__ = [
 ]
 
 LOG = logging.getLogger(__name__)
-
-DB_CONNRETRY_MAX_DELAY = cfg.CONF.database.connection_retry_max_delay_m * 60 * 1000
-DB_CONNRETRY_EXP_BACK_OFF_MAX = cfg.CONF.database.connection_retry_backoff_max_s * 1000
-DB_CONNRETRY_EXP_BACK_OFF_MUL = cfg.CONF.database.connection_retry_backoff_mul * 1000
 
 
 def _retry_if_connection_error(error):
@@ -45,14 +41,20 @@ def _retry_if_connection_error(error):
     return is_connection_error
 
 
-@retry(retry_on_exception=_retry_if_connection_error,
-       wait_exponential_multiplier=DB_CONNRETRY_EXP_BACK_OFF_MUL,
-       wait_exponential_max=DB_CONNRETRY_EXP_BACK_OFF_MAX,
-       stop_max_delay=DB_CONNRETRY_MAX_DELAY)
 def db_setup_with_retry(db_name, db_host, db_port, username=None, password=None,
                         ensure_indexes=True):
     """
     This method is a retry version of db_setup.
     """
-    return db_setup(db_name, db_host, db_port, username=username, password=password,
-                    ensure_indexes=ensure_indexes)
+    # Using as an annotation would be nice but annotations are evaluated at import
+    # time and simple ways to use the annotation means the config gets read before
+    # it is setup. Likely there is a way to use some proxies to delay the actual
+    # reading of config values however this is lesser code.
+    retrying_obj = retrying.Retrying(
+        retry_on_exception=_retry_if_connection_error,
+        wait_exponential_multiplier=cfg.CONF.database.connection_retry_backoff_mul * 1000,
+        wait_exponential_max=cfg.CONF.database.connection_retry_backoff_max_s * 1000,
+        stop_max_delay=cfg.CONF.database.connection_retry_max_delay_m * 60 * 1000
+    )
+    return retrying_obj.call(db_setup, db_name, db_host, db_port, username=username,
+                             password=password, ensure_indexes=ensure_indexes)
