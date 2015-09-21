deec8f4a488863ab51bcb2c8e8a97b6d4f2e2900
Tomaz Muraus <tomaz@tomaz.me>
2015-09-21 13:22:34 +0200

    Add tests for tag creation when registering trigger definitions from disk.

--- st2reactor/tests/unit/test_sensor_and_rule_registration.py
+++ st2reactor/tests/unit/test_sensor_and_rule_registration.py
@@ -63,6 +63,9 @@ class SensorRegistrationTestCase(DbTestCase):
         self.assertEqual(trigger_type_dbs[0].pack, 'pack_with_sensor')
         self.assertEqual(trigger_type_dbs[1].name, 'trigger_type_2')
         self.assertEqual(trigger_type_dbs[1].pack, 'pack_with_sensor')
+        self.assertEqual(len(trigger_type_dbs[1].tags), 2)
+        self.assertEqual(trigger_type_dbs[1].tags[0].name, 'tag1name')
+        self.assertEqual(trigger_type_dbs[1].tags[0].value, 'tag1 value')
 
         # Verify second call to registration doesn't create a duplicate objects
         registrar.register_sensors_from_packs(base_dirs=[PACKS_DIR])
@@ -109,6 +112,7 @@ class SensorRegistrationTestCase(DbTestCase):
 
         self.assertEqual(trigger_type_dbs[0].name, 'trigger_type_1')
         self.assertEqual(trigger_type_dbs[0].pack, 'pack_with_sensor')
+        self.assertEqual(len(trigger_type_dbs[0].tags), 0)
         self.assertEqual(trigger_type_dbs[1].name, 'trigger_type_2')
         self.assertEqual(trigger_type_dbs[1].pack, 'pack_with_sensor')
         self.assertEqual(trigger_type_dbs[1].description, 'test 2')
