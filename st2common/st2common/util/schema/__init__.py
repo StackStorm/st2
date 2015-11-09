caa33d67f93c3812e3affd8e8032a16fb0cc514e
Tomaz Muraus <tomaz@tomaz.me>
2015-11-09 20:39:41 +0100

    Add missing license header, use os.path.join.

--- st2common/st2common/util/schema/__init__.py
+++ st2common/st2common/util/schema/__init__.py
@@ -1,3 +1,18 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import copy
 
@@ -21,10 +36,10 @@ __all__ = [
 # and draft 3 version of required.
 PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 SCHEMAS = {
-    'draft4': jsonify.load_file('%s/draft4.json' % PATH),
-    'custom': jsonify.load_file('%s/custom.json' % PATH),
+    'draft4': jsonify.load_file(os.path.join(PATH, 'draft4.json')),
+    'custom': jsonify.load_file(os.path.join(PATH, 'custom.json')),
     # Custom schema for action params which doesn't allow parameter "type" attribute to be array
-    'action_params': jsonify.load_file('%s/action_params.json' % PATH)
+    'action_params': jsonify.load_file(os.path.join(PATH, 'action_params.json'))
 }
 
 SCHEMA_ANY_TYPE = {
