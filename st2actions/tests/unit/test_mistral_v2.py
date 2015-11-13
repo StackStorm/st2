bcdbdafe14ba7fab50ae7365766212c6968ef74e
Tomaz Muraus <tomaz@tomaz.me>
2015-11-13 15:04:43 +0100

    Use the same variable name across the code base, update affected tests.

--- st2actions/tests/unit/test_mistral_v2.py
+++ st2actions/tests/unit/test_mistral_v2.py
@@ -195,6 +195,7 @@ class MistralRunnerTest(DbTestCase):
         env = {
             'st2_execution_id': str(execution.id),
             'st2_liveaction_id': str(liveaction.id),
+            'st2_action_api_url': 'http://0.0.0.0:9101/v1',
             '__actions': {
                 'st2.action': {
                     'st2_context': {
@@ -244,6 +245,7 @@ class MistralRunnerTest(DbTestCase):
         env = {
             'st2_execution_id': str(execution.id),
             'st2_liveaction_id': str(liveaction.id),
+            'st2_action_api_url': 'https://0.0.0.0:9101/v1',
             '__actions': {
                 'st2.action': {
                     'st2_context': {
@@ -294,6 +296,7 @@ class MistralRunnerTest(DbTestCase):
         env = {
             'st2_execution_id': str(execution.id),
             'st2_liveaction_id': str(liveaction.id),
+            'st2_action_api_url': 'http://0.0.0.0:9101/v1',
             '__actions': {
                 'st2.action': {
                     'st2_context': {
