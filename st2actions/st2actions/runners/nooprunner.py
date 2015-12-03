6758db0616666952942c068cb7ad8fe9124aac1e
James Fryman <james@fryman.io>
2015-12-03 13:43:32 -0600

    including fixes based on code review (h/t @manasdk)

--- st2actions/st2actions/runners/nooprunner.py
+++ st2actions/st2actions/runners/nooprunner.py
@@ -33,8 +33,8 @@ class NoopRunner(ActionRunner):
     def run(self, action_parameters):
 
         LOG.info('Executing action via NoopRunner: %s', self.runner_id)
-        LOG.info('[Action info] name: %s, Id: %s' %
-                 (self.action_name, str(self.liveaction_id)))
+        LOG.info('[Action info] name: %s, Id: %s' ,
+                 self.action_name, str(self.execution_id))
 
         result = {
             'failed': False,
