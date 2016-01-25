912a455350e848c0bfa12c4a64d5543665191ee4
valarmathip <pvalarmathi@terafastnet.com>
2016-01-25 18:15:32 +0530

    Added two new tests to check the submit_debug_info.py enhancement work

--- st2debug/tests/integration/test_submit_debug_info.py
+++ st2debug/tests/integration/test_submit_debug_info.py
@@ -29,7 +29,7 @@ import st2debug.cmd.submit_debug_info
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_DIR = os.path.join(BASE_DIR, 'fixtures')
 GPG_INSTALLED = find_executable('gpg') is not None
-
+SUBMIT_DEBUG_YAML_FILE = os.path.join(FIXTURES_DIR, 'submit-debug-info.yaml')
 
 @unittest2.skipIf(not GPG_INSTALLED, 'gpg binary not available')
 class SubmitDebugInfoTestCase(CleanFilesTestCase):
@@ -121,6 +121,75 @@ class SubmitDebugInfoTestCase(CleanFilesTestCase):
         self.assertTrue(os.path.isdir(pack_dir))
         self.assertTrue(not os.path.exists(config_path))
 
+    def test_create_archive_include_all_with_shell_commands(self):
+        # Load the submit debug info yaml file
+        st2debug.cmd.submit_debug_info.load_config_yaml_file(SUBMIT_DEBUG_YAML_FILE)
+        archive_path = create_archive(include_logs=True, include_configs=True,
+                                      include_content=True,
+                                      include_system_info=True,
+                                      include_shell_commands=True,
+                                      config_yaml=SUBMIT_DEBUG_YAML_FILE)
+
+        # Verify archive has been created
+        self.assertTrue(os.path.isfile(archive_path))
+        self.to_delete_files.append(archive_path)
+
+        extract_path = tempfile.mkdtemp()
+        self.to_delete_directories.append(extract_path)
+        self._extract_archive(archive_path=archive_path, extract_path=extract_path)
+
+        # Verify all the required directories have been created
+        required_directories = ['logs', 'configs', 'content', 'commands']
+
+        for directory_name in required_directories:
+            full_path = os.path.join(extract_path, directory_name)
+            self.assertTrue(os.path.isdir(full_path))
+
+        # Verify system info file has ben created
+        full_path = os.path.join(extract_path, 'system_info.yaml')
+        self.assertTrue(os.path.isfile(full_path))
+
+        # Verify logs have been copied
+        logs_path = os.path.join(extract_path, 'logs')
+        log_files = os.listdir(logs_path)
+        self.assertTrue(len(log_files), 2)
+
+        # Verify commands output have been copied
+        commands_path = os.path.join(extract_path, 'commands')
+        commands_output = os.listdir(commands_path)
+        self.assertTrue(len(log_files), 1)
+
+        # Verify configs have been copied
+        st2_config_path = os.path.join(extract_path, 'configs', 'st2.conf')
+        mistral_config_path = os.path.join(extract_path, 'configs', 'mistral.conf')
+
+        self.assertTrue(os.path.isfile(st2_config_path))
+        self.assertTrue(os.path.isfile(mistral_config_path))
+
+        # Verify packs have been copied
+        content_path = os.path.join(extract_path, 'content/dir-1')
+        pack_directories = os.listdir(content_path)
+        self.assertEqual(len(pack_directories), 1)
+
+        # Verify sensitive data has been masked in the configs
+        with open(st2_config_path, 'r') as fp:
+            st2_config_content = fp.read()
+        with open(mistral_config_path, 'r') as fp:
+            mistral_config_content = fp.read()
+
+        self.assertTrue('ponies' not in st2_config_content)
+        self.assertTrue('url = **removed**' in st2_config_content)
+
+        self.assertTrue('StackStorm' not in mistral_config_content)
+        self.assertTrue('connection = **removed**' in mistral_config_content)
+
+        # Very config.yaml has been removed from the content pack directories
+        pack_dir = os.path.join(content_path, 'twilio')
+        config_path = os.path.join(pack_dir, 'config.yaml')
+
+        self.assertTrue(os.path.isdir(pack_dir))
+        self.assertTrue(not os.path.exists(config_path))
+
     def test_create_archive_exclusion(self):
         # Verify only system info file is included
         archive_path = create_archive(include_logs=False, include_configs=False,
@@ -150,6 +219,27 @@ class SubmitDebugInfoTestCase(CleanFilesTestCase):
         plaintext_archive_path = create_archive(include_logs=True, include_configs=True,
                                                 include_content=True,
                                                 include_system_info=True)
+        plaintext_archive_size = os.stat(plaintext_archive_path).st_size
+
+        encrypted_archive_path = encrypt_archive(archive_file_path=plaintext_archive_path)
+        encrypt_archive_size = os.stat(encrypted_archive_path).st_size
+
+        self.assertTrue(os.path.isfile(encrypted_archive_path))
+        self.assertTrue(encrypt_archive_size > plaintext_archive_size)
+
+        self.assertRaises(Exception, archive_path=encrypted_archive_path,
+                          extract_path='/tmp')
+
+    def test_encrypt_archive_with_custom_gpg_key(self):
+        # Load the submit debug info yaml file
+        st2debug.cmd.submit_debug_info.load_config_yaml_file(SUBMIT_DEBUG_YAML_FILE)
+
+        plaintext_archive_path = create_archive(include_logs=True, include_configs=True,
+                                                include_content=True,
+                                                include_system_info=True,
+                                                include_shell_commands=True,
+                                                config_yaml=SUBMIT_DEBUG_YAML_FILE)
+
         plaintext_archive_size = os.stat(plaintext_archive_path).st_size
 
         encrypted_archive_path = encrypt_archive(archive_file_path=plaintext_archive_path)
