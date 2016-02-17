7ba9b524c55e278209ac90e7e4fe342f9171e525
Tomaz Muraus <tomaz@tomaz.me>
2016-02-17 15:11:22 +0100

    Introduce new sleep_delay parameter and actually use ssh_timeout parametr for SSH connection connect timeout.

--- contrib/linux/actions/wait_for_ssh.py
+++ contrib/linux/actions/wait_for_ssh.py
@@ -9,12 +9,14 @@ from st2actions.runners.ssh.paramiko_ssh import ParamikoSSHClient
 
 
 class BaseAction(Action):
-    def run(self, hostname, port, username, password=None, keyfile=None, ssh_timeout=30,
-            retries=10):
-        # Note: If neither password nor key file is provided, we try to use
-        # system user key file
+    def run(self, hostname, port, username, password=None, keyfile=None, ssh_timeout=5,
+            sleep_delay=20, retries=10):
+        # Note: If neither password nor key file is provided, we try to use system user 
+        # key file
         if not password and not keyfile:
             keyfile = cfg.CONF.system_user.ssh_key_file
+            self.logger.info('Neither "password" nor "keyfile" parameter provided, '
+                             'defaulting to using "%s" key file' % (keyfile))
 
         client = ParamikoSSHClient(hostname=hostname, port=port, username=username,
                                    password=password, key_files=keyfile,
@@ -28,7 +30,8 @@ class BaseAction(Action):
                 client.connect()
                 return True
             except Exception as e:
-                self.logger.info('Attempt %s failed (%s), sleeping...' % (attempt, str(e)))
-                time.sleep(ssh_timeout)
+                self.logger.info('Attempt %s failed (%s), sleeping for %s seconds...' %
+                                  (attempt, str(e), sleep_delay))
+                time.sleep(sleep_delay)
 
         raise Exception('Exceeded max retries (%s)' % (retries))
