2c99a26c9b617ae6b9871d8daa34044f281aa322
Tomaz Muraus <tomaz@tomaz.me>
2016-02-26 19:50:55 +0100

    Move stream host and port config option to stream specific config file.

--- st2stream/st2stream/config.py
+++ st2stream/st2stream/config.py
@@ -46,9 +46,11 @@ def get_logging_config_path():
 
 
 def _register_app_opts():
-    # Note "host", "port", "allow_origin", "mask_secrets" options are registered as part of
-    # st2common config since they are also used outside st2stream
+    # Note "allow_origin", "mask_secrets" options are registered as part of st2common config since
+    # they are also used outside st2stream
     api_opts = [
+        cfg.StrOpt('host', default='0.0.0.0', help='StackStorm stream API server host'),
+        cfg.IntOpt('port', default=9102, help='StackStorm API stream, server port'),
         cfg.IntOpt('heartbeat', default=25,
                    help='Send empty message every N seconds to keep connection open'),
         cfg.BoolOpt('debug', default=False,
