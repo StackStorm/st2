4089875b60c0336b6d92073cfb06cde35e2c4af8
Tomaz Muraus <tomaz@tomaz.me>
2016-03-15 12:25:59 -0700

    Refactor the code and use constants.

--- st2common/st2common/validators/api/reactor.py
+++ st2common/st2common/validators/api/reactor.py
@@ -18,9 +18,8 @@ import six
 from apscheduler.triggers.cron import CronTrigger
 
 from st2common.exceptions.apivalidation import ValueValidationException
-from st2common.models.system.common import ResourceReference
-from st2common.constants.pack import SYSTEM_PACK_NAME
 from st2common.constants.triggers import SYSTEM_TRIGGER_TYPES
+from st2common.constants.triggers import CRON_TIMER_TRIGGER_REF
 from st2common.util import schema as util_schema
 import st2common.operators as criteria_operators
 
@@ -75,8 +74,9 @@ def validate_trigger_parameters(trigger_type_ref, parameters):
                                    cls=util_schema.CustomValidator, use_default=True,
                                    allow_default_none=True)
 
-    # Additional validation fron CronTimer
-    if trigger_type_ref == ResourceReference.to_string_reference(SYSTEM_PACK_NAME, 'st2.CronTimer'):
+    # Additional validation for CronTimer trigger
+    # TODO: If we need to add more checks like this we should consider abstracting this out.
+    if trigger_type_ref == CRON_TIMER_TRIGGER_REF:
         # Validate that the user provided parameters are valid. This is required since JSON schema
         # allows arbitrary strings, but not any arbitrary string is a valid CronTrigger argument
         # Note: Constructor throws ValueError on invalid parameters
