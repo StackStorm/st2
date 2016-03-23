1a42da41ea22cae2bd1573cf3633e76a92d713b2
Tomaz Muraus <tomaz@tomaz.me>
2016-03-23 12:44:45 -0700

    Throw if remote-shell-cmd action defines "entry_point" attribute.
    This attribute makes no sense in the context of remote command action
    and usually indicates a typo (user most likely wanted to use
    remote-shell-script runner).

--- st2actions/st2actions/runners/remote_command_runner.py
+++ st2actions/st2actions/runners/remote_command_runner.py
@@ -51,6 +51,13 @@ class RemoteCommandRunner(BaseFabricRunner):
         return (status, result, None)
 
     def _get_remote_action(self, action_paramaters):
+        # remote script actions with entry_point don't make sense, user probably wanted to use
+        # "remote-shell-script" action
+        if self.entry_point:
+            msg = ('Action "%s" specified "entry_point" attribute. Perhaps wanted to use '
+                   '"remote-shell-script" runner?' % (self.action_name))
+            raise Exception(msg)
+
         command = self.runner_parameters.get(RUNNER_COMMAND, None)
         env_vars = self._get_env_vars()
         return FabricRemoteAction(self.action_name,
@@ -84,6 +91,13 @@ class ParamikoRemoteCommandRunner(BaseParallelSSHRunner):
         return self._parallel_ssh_client.run(command, timeout=remote_action.get_timeout())
 
     def _get_remote_action(self, action_paramaters):
+        # remote script actions with entry_point don't make sense, user probably wanted to use
+        # "remote-shell-script" action
+        if self.entry_point:
+            msg = ('Action "%s" specified "entry_point" attribute. Perhaps wanted to use '
+                   '"remote-shell-script" runner?' % (self.action_name))
+            raise Exception(msg)
+
         command = self.runner_parameters.get(RUNNER_COMMAND, None)
         env_vars = self._get_env_vars()
         return ParamikoRemoteCommandAction(self.action_name,
