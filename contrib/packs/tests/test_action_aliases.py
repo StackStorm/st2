bd78a9aa8e6e4b7b7b934d3e95274bd10cd746fb
Tomaz Muraus <tomaz@tomaz.me>
2016-04-07 13:46:27 +0200

    Update and add tests for deploy pack alias.

--- contrib/packs/tests/test_action_aliases.py
+++ contrib/packs/tests/test_action_aliases.py
@@ -6,16 +6,65 @@ class DeployActionAliasTestCase(BaseActionAliasTestCase):
 
     def test_deploy_alias(self):
         # Includes packs
-        format_string_1 = self.action_alias_db.formats[0]['representation'][0]
+        format_string = self.action_alias_db.formats[0]['representation'][0]
 
-        # String with branch
-        command = 'pack deploy StackStorm/st2contrib branch master'
-        command = 'pack deploy StackStorm/st2contrib branch ma_branch'
+        # Command with branch
+        command = 'pack deploy StackStorm/st2contrib packs libcloud,aws branch master'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib',
+            'packs': 'libcloud,aws',
+            'branch': 'master'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
 
-        format_string_2 = self.action_alias_db.formats[0]['representation'][1]
+        command = 'pack deploy StackStorm/st2contrib packs libcloud branch ma_branch'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib',
+            'packs': 'libcloud',
+            'branch': 'ma_branch'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
+
+        # Command without branch
+        format_string = self.action_alias_db.formats[0]['representation'][1]
+        command = 'pack deploy StackStorm/st2contrib packs libcloud,aws'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib',
+            'packs': 'libcloud,aws'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
+
+        command = 'pack deploy StackStorm/st2contrib packs libcloud'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib',
+            'packs': 'libcloud'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
 
         # Doesnt include packs
-        format_string_1 = self.action_alias_db.formats[1]['representation'][0]
-        format_string_2 = self.action_alias_db.formats[1]['representation'][1]
-        print format_string_1
-        pass
+        format_string = self.action_alias_db.formats[1]['representation'][0]
+        command = 'pack deploy StackStorm/st2contrib branch trunk'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib',
+            'branch': 'trunk'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
+
+        format_string = self.action_alias_db.formats[1]['representation'][1]
+        command = 'pack deploy StackStorm/st2contrib'
+        expected_parameters = {
+            'repo_name': 'StackStorm/st2contrib'
+        }
+        self.assertExtractedParametersMatch(format_string=format_string,
+                                            command=command,
+                                            values=expected_parameters)
