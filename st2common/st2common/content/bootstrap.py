f15db18d51d451e2619681afe2d754649b2b671f
Tomaz Muraus <tomaz@tomaz.me>
2016-04-13 14:38:28 +0200

    Pass missing base_dir argument to the method call.

--- st2common/st2common/content/bootstrap.py
+++ st2common/st2common/content/bootstrap.py
@@ -31,6 +31,7 @@ import st2common.bootstrap.policiesregistrar as policies_registrar
 import st2common.bootstrap.runnersregistrar as runners_registrar
 import st2common.bootstrap.rulesregistrar as rules_registrar
 import st2common.bootstrap.ruletypesregistrar as rule_types_registrar
+import st2common.content.utils as content_utils
 from st2common.util.virtualenvs import setup_pack_virtualenv
 
 __all__ = [
@@ -70,16 +71,21 @@ def setup_virtualenvs():
     pack_dir = cfg.CONF.register.pack
     fail_on_failure = cfg.CONF.register.fail_on_failure
 
-    # Note: We need to register packs here as well
     registrar = ResourceRegistrar()
 
     if pack_dir:
         pack_name = os.path.basename(pack_dir)
         pack_names = [pack_name]
+
+        # 1. Register pack
         registrar.register_pack(pack_name=pack_name, pack_dir=pack_dir)
     else:
+        # 1. Register pack
+        base_dirs = content_utils.get_packs_base_paths()
+        registrar.register_packs(base_dirs=base_dirs)
+
+        # 2. Retrieve available packs (aka packs which have been registered)
         pack_names = registrar.get_registered_packs()
-        registrar.register_packs()
 
     setup_count = 0
     for pack_name in pack_names:
