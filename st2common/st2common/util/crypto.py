75be11b450667e40ffe4d4505fc227c1f848c41b
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-04-19 16:35:15 -0700

    Utility methods

---
+++ st2common/st2common/util/crypto.py
@@ -0,0 +1,22 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def symmetric_encrypt(encrypt_key, message):
+    return encrypt_key.Encrypt(message)
+
+
+def symmetric_decrypt(decrypt_key, crypto):
+    return decrypt_key.Decrypt(crypto)
