c5ad5b3dcce1e3900504fb369b0346fe33adb285
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-04-22 16:34:11 -0700

    First set of tests

--- st2common/st2common/util/crypto.py
+++ st2common/st2common/util/crypto.py
@@ -17,8 +17,38 @@ import binascii
 
 
 def symmetric_encrypt(encrypt_key, message):
+    """
+    Encrypt the given message using the encrypt_key. Returns a UTF-8 str
+    ready to be stored in database. Note that we convert the hex notation
+    to a ASCII notation to produce a UTF-8 friendly string.
+
+    Also, this method will not return the same output on multiple invocations
+    of same method. The reason is that the Encrypt method uses a different
+    'Initialization Vector' per run and the IV is part of the output.
+
+    :param encrypt_key: Symmetric AES key to use for encryption.
+    :type encrypt_key: :class:`keyczar.keys.AesKey`
+
+    :param message: Message to be encrypted.
+    :type message: ``str``
+
+    :rtype: ``str``
+    """
     return binascii.hexlify(encrypt_key.Encrypt(message)).upper()
 
 
 def symmetric_decrypt(decrypt_key, crypto):
+    """
+    Decrypt the given crypto text into plain text. Returns the original
+    string input. Note that we first convert the string to hex notation
+    and then decrypt. This is reverse of the encrypt operation.
+
+    :param decrypt_key: Symmetric AES key to use for decryption.
+    :type decrypt_key: :class:`keyczar.keys.AesKey`
+
+    :param crypto: Crypto text to be decrypted.
+    :type crypto: ``str``
+
+    :rtype: ``str``
+    """
     return decrypt_key.Decrypt(binascii.unhexlify(crypto))
