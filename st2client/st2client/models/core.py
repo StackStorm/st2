c4ac445da8c11770276e6cf4682bf4d54c60eba7
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-04-25 11:43:47 -0700

    Fix st2client tests

--- st2client/st2client/models/core.py
+++ st2client/st2client/models/core.py
@@ -240,7 +240,8 @@ class ResourceManager(object):
         if 'limit' in kwargs and kwargs.get('limit') <= 0:
             kwargs.pop('limit')
         token = kwargs.get('token', None)
-        params = {}
+        params = kwargs.get('params', {})
+        print(params)
         for k, v in six.iteritems(kwargs):
             if k != 'token':
                 params[k] = v
