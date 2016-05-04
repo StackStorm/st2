457623373c8c63b3c9fb010761ee389a6e3ac911
Tomaz Muraus <tomaz@tomaz.me>
2016-05-04 18:37:45 +0800

    Also add test cases for key + passpharse.

--- st2actions/tests/unit/test_paramiko_ssh_runner.py
+++ st2actions/tests/unit/test_paramiko_ssh_runner.py
@@ -24,6 +24,7 @@ from st2actions.runners.ssh.paramiko_ssh_runner import RUNNER_HOSTS
 from st2actions.runners.ssh.paramiko_ssh_runner import RUNNER_USERNAME
 from st2actions.runners.ssh.paramiko_ssh_runner import RUNNER_PASSWORD
 from st2actions.runners.ssh.paramiko_ssh_runner import RUNNER_PRIVATE_KEY
+from st2actions.runners.ssh.paramiko_ssh_runner import RUNNER_PASSPHRASE
 
 import st2tests.config as tests_config
 from st2tests.fixturesloader import get_resources_base_path
@@ -39,6 +40,11 @@ class ParamikoSSHRunnerTestCase(unittest2.TestCase):
     @mock.patch('st2actions.runners.ssh.paramiko_ssh_runner.ParallelSSHClient')
     def test_pre_run(self, mock_client):
         # Test case which verifies that ParamikoSSHClient is instantiated with the correct arguments
+        private_key_path = os.path.join(get_resources_base_path(),
+                                       'ssh', 'dummy_rsa')
+
+        with open(private_key_path, 'r') as fp:
+            private_key = fp.read()
 
         # Username and password provided
         runner = Runner('id')
@@ -64,12 +70,6 @@ class ParamikoSSHRunnerTestCase(unittest2.TestCase):
         mock_client.assert_called_with(**expected_kwargs)
 
         # Private key provided as raw key material
-        path = os.path.join(get_resources_base_path(),
-                            'ssh', 'dummy_rsa')
-
-        with open(path, 'r') as fp:
-            private_key = fp.read()
-
         runner = Runner('id')
         runner.context = {}
         runner_parameters = {
@@ -92,13 +92,38 @@ class ParamikoSSHRunnerTestCase(unittest2.TestCase):
         }
         mock_client.assert_called_with(**expected_kwargs)
 
+        # Private key provided as raw key material + passphrase
+        runner = Runner('id')
+        runner.context = {}
+        runner_parameters = {
+            RUNNER_HOSTS: 'localhost21',
+            RUNNER_USERNAME: 'someuser21',
+            RUNNER_PRIVATE_KEY: private_key,
+            RUNNER_PASSPHRASE: 'passphrase21'
+        }
+        runner.runner_parameters = runner_parameters
+        runner.pre_run()
+
+        expected_kwargs = {
+            'hosts': ['localhost21'],
+            'user': 'someuser21',
+            'pkey_material': private_key,
+            'passphrase': 'passphrase21',
+            'port': 22,
+            'concurrency': 1,
+            'bastion_host': None,
+            'raise_on_any_error': False,
+            'connect': True
+        }
+        mock_client.assert_called_with(**expected_kwargs)
+
         # Private key provided as path to the private key file
         runner = Runner('id')
         runner.context = {}
         runner_parameters = {
             RUNNER_HOSTS: 'localhost',
             RUNNER_USERNAME: 'someuser3',
-            RUNNER_PRIVATE_KEY: path
+            RUNNER_PRIVATE_KEY: private_key_path
         }
         runner.runner_parameters = runner_parameters
         runner.pre_run()
@@ -106,7 +131,32 @@ class ParamikoSSHRunnerTestCase(unittest2.TestCase):
         expected_kwargs = {
             'hosts': ['localhost'],
             'user': 'someuser3',
-            'pkey_file': path,
+            'pkey_file': private_key_path,
+            'port': 22,
+            'concurrency': 1,
+            'bastion_host': None,
+            'raise_on_any_error': False,
+            'connect': True
+        }
+        mock_client.assert_called_with(**expected_kwargs)
+
+        # Private key provided as path to the private key file + passpharse
+        runner = Runner('id')
+        runner.context = {}
+        runner_parameters = {
+            RUNNER_HOSTS: 'localhost31',
+            RUNNER_USERNAME: 'someuser31',
+            RUNNER_PRIVATE_KEY: private_key_path,
+            RUNNER_PASSPHRASE: 'passphrase31'
+        }
+        runner.runner_parameters = runner_parameters
+        runner.pre_run()
+
+        expected_kwargs = {
+            'hosts': ['localhost31'],
+            'user': 'someuser31',
+            'pkey_file': private_key_path,
+            'passphrase': 'passphrase31',
             'port': 22,
             'concurrency': 1,
             'bastion_host': None,
