67d7ed9a1ead1cb458080df9551fc368f6d5526f
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-05-17 20:21:53 -0700

    Remove v2 APIs

--- st2api/st2api/controllers/root.py
+++ st2api/st2api/controllers/root.py
@@ -20,7 +20,6 @@ from st2common import log as logging
 from st2common.controllers import BaseRootController
 import st2api.controllers.exp.root as exp_root
 import st2api.controllers.v1.root as v1_root
-import st2api.controllers.v2.root as v2_root
 
 __all__ = [
     'RootController'
@@ -34,12 +33,10 @@ class RootController(BaseRootController):
 
     def __init__(self):
         v1_controller = v1_root.RootController()
-        v2_controller = v2_root.RootController()
         exp_controller = exp_root.RootController()
 
         self.controllers = {
             'v1': v1_controller,
-            'v2': v2_controller,
             'exp': exp_controller
         }
 
