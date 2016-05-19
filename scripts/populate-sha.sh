4936246bdda69e2f47ebd826a3752371b8d0bdef
Anirudh Rekhi <arekhi@brocade.com>
2016-05-18 21:30:11 -0700

    Modified the config file creation

--- scripts/populate-sha.sh
+++ scripts/populate-sha.sh
@@ -3,14 +3,14 @@
 git_sha=$(git rev-parse --short HEAD)
 version=$(cat ../st2common/st2common/__init__.py | grep __version__ | sed -r '1!d;s/.*=\s+//'| tr -d "'")
 
-echo $version\_SHA_$git_sha
-
-exec 3>&1 >package_meta.conf
-
-echo "[server]"
-echo "# Server version and SHA for the build"
-echo "version = $version"
-echo "git_sha = $git_sha"
-
-exec >&3
-
+config=../package.meta
+echo "Writing Version and SHA info. to $config"
+if [ -e "$config" ]; then
+	rm $config
+fi
+touch $config
+cat <<package_meta >$config
+[server]
+version = $version
+git_sha = $git_sha
+package_meta
