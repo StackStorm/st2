2dd2a8320414d83f3b1705690d68d4c5a2cd60f8
Anirudh Rekhi <arekhi@brocade.com>
2016-05-19 11:56:31 -0700

    Updated as per the comment above

--- scripts/populate-sha.sh
+++ scripts/populate-sha.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-git_sha=$(git rev-parse --short HEAD)
-version=$(cat ../st2common/st2common/__init__.py | grep __version__ | sed -r '1!d;s/.*=\s+//'| tr -d "'")
-
 config=../package.meta
 echo "Writing Version and SHA info. to $config"
 if [ -e "$config" ]; then
@@ -11,6 +8,6 @@ fi
 touch $config
 cat <<package_meta >$config
 [server]
-version = $version
-git_sha = $git_sha
+version = $(python -c 'execfile("../st2common/st2common/__init__.py"); print __version__')
+git_sha = $(git rev-parse --short HEAD)
 package_meta
