5d21d14d99a0a15608e574876fd5872f181839c2
Anirudh Rekhi <arekhi@brocade.com>
2016-05-19 19:17:20 -0700

    Getting Circle_build_url to package.meta

--- scripts/populate-sha.sh
+++ scripts/populate-sha.sh
@@ -8,7 +8,7 @@ echo LS: `ls -al ../`
 
 
 config=../package.meta
-echo "Writing Version and SHA info. to $config"
+echo "Writing version, SHA and circle_build_url  to $config  ..."
 if [ -e "$config" ]; then
 	rm $config
 fi
@@ -17,11 +17,13 @@ cat <<package_meta >$config
 [server]
 version = $(python -c 'execfile("../st2common/st2common/__init__.py"); print __version__')
 git_sha = $(git rev-parse --short HEAD)
+circle_build_url=$(echo $ST2_CIRCLE_URL)
+
 package_meta
 
 echo Contents of package.meta:
 cat ../package.meta
 popd
 
-echo Export variables to find Circle CI URL
+echo env variables to find Circle CI URL
 export
