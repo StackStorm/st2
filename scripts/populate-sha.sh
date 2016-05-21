279331af175efab69be75c4b6799e9f6a75266ce
Anirudh Rekhi <arekhi@brocade.com>
2016-05-20 18:18:49 -0700

    Cleaning up the populate-sha.sh

--- scripts/populate-sha.sh
+++ scripts/populate-sha.sh
@@ -2,11 +2,6 @@
 
 pushd $GITDIR/scripts
 
-echo GIT:$GITDIR
-echo PWD:`pwd`
-echo LS: `ls -al ../`
-
-
 config=../package.meta
 echo "Writing version, SHA and circle_build_url  to $config  ..."
 if [ -e "$config" ]; then
@@ -17,8 +12,7 @@ cat <<package_meta >$config
 [server]
 version = $(python -c 'execfile("../st2common/st2common/__init__.py"); print __version__')
 git_sha = $(git rev-parse --short HEAD)
-circle_build_url=$(echo $ST2_CIRCLE_URL)
-circle_build_url=$(echo $CIRCLE_BUILD_URL)
+circle_build_url = $(echo $ST2_CIRCLE_URL)
 
 package_meta
 
