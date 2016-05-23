53accc29a5fe80c3d4dff158fcf6ef677dec4cd2
Tomaz Muraus <tomaz@tomaz.me>
2016-05-23 13:57:26 +0200

    Add some WIP tests for config loader.

---
+++ st2common/tests/unit/test_config_loader.py
@@ -0,0 +1,41 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mock import Mock
+
+from st2tests.base import CleanDbTestCase
+from st2common.util.config_loader import ContentPackConfigLoader
+#from st2common.services import config as config_service
+
+__all__ = [
+    'ContentPackConfigLoaderTestCase'
+]
+
+
+class ContentPackConfigLoaderTestCase(CleanDbTestCase):
+    def test_get_config_all_values_are_loaded_from_local_config(self):
+        # Test a scenario where all the values are loaded from pack local config and pack global
+        # config (pack name.yaml) doesn't exist
+        # Test a scenario where no values are overridden in the datastore
+        loader = ContentPackConfigLoader(pack_name='dummy_pack_4')
+        config = loader.get_config()
+
+        expected_config = {
+            'api_key': '',
+            'api_secret': '',
+            'regions': ['us-west-1', 'us-east-1'],
+            'private_key_path': None
+        }
+        self.assertEqual(config, expected_config)
