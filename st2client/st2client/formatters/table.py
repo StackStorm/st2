6a56bc54d05b6f04ae046eb40c66978f09140926
Tomaz Muraus <tomaz@tomaz.me>
2016-06-14 10:15:11 +0200

    Make table formatter more robust - it shouldn't throw if there are no entries.
    In such case we can't inferr available attributes so we simply ignore
    "attributes=all" scenario.

--- st2client/st2client/formatters/table.py
+++ st2client/st2client/formatters/table.py
@@ -100,8 +100,13 @@ class MultiColumnTable(formatters.Formatter):
 
         if not attributes or 'all' in attributes:
             entries = list(entries) if entries else []
-            attributes = sorted([attr for attr in entries[0].__dict__
-                                 if not attr.startswith('_')])
+
+            if len(entries) >= 1:
+                attributes = entries[0].__dict__.keys()
+                attributes = sorted([attr for attr in attributes if not attr.startswith('_')])
+            else:
+                # There are no entries so we can't infer available attributes
+                attributes = []
 
         # Determine table format.
         if len(attributes) == len(widths):
