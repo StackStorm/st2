8400c8873d9e95a68e321d4775940e431e715467
Tomaz Muraus <tomaz@tomaz.me>
2016-07-13 17:55:52 +0200

    Also add a test case for scheduler and pre_run policies.

--- st2actions/tests/unit/policies/test_base.py
+++ st2actions/tests/unit/policies/test_base.py
@@ -15,10 +15,12 @@
 
 import mock
 
-import st2actions
 from st2tests import config as test_config
 test_config.parse_args()
+
+import st2actions
 from st2actions.notifier import notifier
+from st2actions import scheduler
 from st2common.bootstrap.policiesregistrar import register_policy_types
 from st2common.models.db.action import LiveActionDB
 from st2common.persistence.policy import Policy
@@ -29,12 +31,13 @@ from st2tests.fixturesloader import FixturesLoader
 
 
 __all__ = [
-    'BasePoliciesTestCase'
+    'SchedulerPoliciesTest'
+    'NotifierPoliciesTest'
 ]
 
 
 PACK = 'generic'
-TEST_FIXTURES = {
+TEST_FIXTURES_1 = {
     'runners': [
         'testrunner1.yaml'
     ],
@@ -42,32 +45,99 @@ TEST_FIXTURES = {
         'action1.yaml'
     ],
     'policies': [
-        'policy_4.yaml'
+        'policy_4.yaml',
+    ]
+}
+TEST_FIXTURES_2 = {
+    'runners': [
+        'testrunner1.yaml'
+    ],
+    'actions': [
+        'action1.yaml'
+    ],
+    'policies': [
+        'policy_1.yaml',
     ]
 }
 
 
-class BasePoliciesTestCase(CleanDbTestCase):
+class SchedulerPoliciesTest(CleanDbTestCase):
     @classmethod
     def setUpClass(cls):
         DbTestCase.setUpClass()
-        super(BasePoliciesTestCase, cls).setUpClass()
+        super(SchedulerPoliciesTest, cls).setUpClass()
 
     def setUp(self):
-        super(BasePoliciesTestCase, self).setUp()
+        super(SchedulerPoliciesTest, self).setUp()
 
         # Register common policy types
         register_policy_types(st2actions)
 
         loader = FixturesLoader()
         models = loader.save_fixtures_to_db(fixtures_pack=PACK,
-                                            fixtures_dict=TEST_FIXTURES)
+                                            fixtures_dict=TEST_FIXTURES_2)
 
-        # Instantiate policy applicator we will use in the tests
+        # Policy with "post_run" application
+        self.policy_db = models['policies']['policy_1.yaml']
+
+    @mock.patch('st2actions.scheduler.policies')
+    def test_disabled_policy_not_applied_on_pre_run(self, mock_policies):
+        scheduler_worker = scheduler.get_scheduler()
+
+        ##########
+        # First test a scenario where policy is enabled
+        ##########
+        self.assertTrue(self.policy_db.enabled)
+
+        # Post run hasn't been called yet, call count should be 0
+        self.assertEqual(mock_policies.get_driver.call_count, 0)
+
+        liveaction = LiveActionDB(action='wolfpack.action-1', parameters={'actionstr': 'foo'})
+        live_action_db, execution_db = action_service.request(liveaction)
+        scheduler_worker._apply_pre_run_policies(liveaction_db=live_action_db)
+
+        # Ony policy has been applied so call count should be 1
+        self.assertEqual(mock_policies.get_driver.call_count, 1)
+
+        ##########
+        # Now a scenaro with disabled policy
+        ##########
+        mock_policies.get_driver.call_count = 0
+        self.policy_db.enabled = False
+        self.policy_db = Policy.add_or_update(self.policy_db)
+        self.assertFalse(self.policy_db.enabled)
+
+        self.assertEqual(mock_policies.get_driver.call_count, 0)
+
+        liveaction = LiveActionDB(action='wolfpack.action-1', parameters={'actionstr': 'foo'})
+        live_action_db, execution_db = action_service.request(liveaction)
+        scheduler_worker._apply_pre_run_policies(liveaction_db=live_action_db)
+
+        # Policy is disabled so call_count should stay the same as before as no policies have been
+        # applied
+        self.assertEqual(mock_policies.get_driver.call_count, 0)
+
+class NotifierPoliciesTest(CleanDbTestCase):
+    @classmethod
+    def setUpClass(cls):
+        DbTestCase.setUpClass()
+        super(NotifierPoliciesTest, cls).setUpClass()
+
+    def setUp(self):
+        super(NotifierPoliciesTest, self).setUp()
+
+        # Register common policy types
+        register_policy_types(st2actions)
+
+        loader = FixturesLoader()
+        models = loader.save_fixtures_to_db(fixtures_pack=PACK,
+                                            fixtures_dict=TEST_FIXTURES_1)
+
+        # Policy with "post_run" application
         self.policy_db = models['policies']['policy_4.yaml']
 
     @mock.patch('st2actions.notifier.notifier.policies')
-    def test_disabled_policies_are_not_applied(self, mock_policies):
+    def test_disabled_policy_not_applied_on_post_run(self, mock_policies):
         notifier_worker = notifier.get_notifier()
 
         ##########
@@ -80,7 +150,7 @@ class BasePoliciesTestCase(CleanDbTestCase):
 
         liveaction = LiveActionDB(action='wolfpack.action-1', parameters={'actionstr': 'foo'})
         live_action_db, execution_db = action_service.request(liveaction)
-        notifier_worker._apply_post_run_policies(liveaction=live_action_db)
+        notifier_worker._apply_post_run_policies(liveaction_db=live_action_db)
 
         # Ony policy has been applied so call count should be 1
         self.assertEqual(mock_policies.get_driver.call_count, 1)
@@ -98,7 +168,7 @@ class BasePoliciesTestCase(CleanDbTestCase):
 
         liveaction = LiveActionDB(action='wolfpack.action-1', parameters={'actionstr': 'foo'})
         live_action_db, execution_db = action_service.request(liveaction)
-        notifier_worker._apply_post_run_policies(liveaction=live_action_db)
+        notifier_worker._apply_post_run_policies(liveaction_db=live_action_db)
 
         # Policy is disabled so call_count should stay the same as before as no policies have been
         # applied
