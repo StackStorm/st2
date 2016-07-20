467341a5d42d471cb9d1dba3964f4df984c05055
Tomaz Muraus <tomaz@tomaz.me>
2016-07-20 19:25:27 +0200

    Update BufferedDispatcher class to log a notice if pool has been occupied with no free workers for more than threshold amount of seconds.

--- st2common/st2common/util/greenpooldispatch.py
+++ st2common/st2common/util/greenpooldispatch.py
@@ -13,12 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 import eventlet
 import Queue
+
+from st2common import log as logging
 
 __all__ = [
     'BufferedDispatcher'
 ]
+
+# If the thread pool has been occupied with no empty threads for more than this number of seconds
+# a message will be logged
+POOL_BUSY_THRESHOLD_SECONDS = 60
+
+POOL_BUSY_LOG_MESSAGE = """
+BufferedDispatcher pool "%s" has been busy with no free threads for more than %s seconds. If there \
+are server resources available, consider increasing the dispatcher pool size in the config.
+""".strip()
+
+LOG = logging.getLogger(__name__)
 
 
 class BufferedDispatcher(object):
@@ -33,6 +48,13 @@ class BufferedDispatcher(object):
         self._name = name
 
         self._work_buffer = Queue.Queue()
+
+        # Internal attributes we use to track how long the pool is busy without any free workers
+        self._pool_last_free_ts = time.time()
+
+    @property
+    def name(self):
+        return self._name or id(self)
 
     def dispatch(self, handler, *args):
         self._work_buffer.put((handler, args), block=True, timeout=1)
@@ -51,14 +73,22 @@ class BufferedDispatcher(object):
 
     def _flush_now(self):
         if self._dispatcher_pool.free() <= 0:
+            now = time.time()
+
+            if (now - self._pool_last_free_ts) >= POOL_BUSY_THRESHOLD_SECONDS:
+                LOG.info(POOL_BUSY_LOG_MESSAGE % (self.name, POOL_BUSY_THRESHOLD_SECONDS))
+
             return
+
+        # Update the time of when there were free threads available
+        self._pool_last_free_ts = time.time()
+
         while not self._work_buffer.empty() and self._dispatcher_pool.free() > 0:
             (handler, args) = self._work_buffer.get_nowait()
             self._dispatcher_pool.spawn(handler, *args)
 
     def __repr__(self):
-        name = self._name or id(self)
-        values = (name, self._pool_limit, self._monitor_thread_empty_q_sleep_time,
+        values = (self.name, self._pool_limit, self._monitor_thread_empty_q_sleep_time,
                   self._monitor_thread_no_workers_sleep_time)
         return ('<BufferedDispatcher name=%s,dispatch_pool_size=%s,'
                 'monitor_thread_empty_q_sleep_time=%s,monitor_thread_no_workers_sleep_time=%s>' %
