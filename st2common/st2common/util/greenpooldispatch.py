fa64df98b5ee154233e1f37d2c826bc3b3640c8f
Tomaz Muraus <tomaz@tomaz.me>
2016-07-20 18:35:30 +0200

    Add __repr__ and optional name attribute to BufferedDispatcher class.
    This makes it easier to debug and differentiate between different dispatcher
    instances used in the code.

--- st2common/st2common/util/greenpooldispatch.py
+++ st2common/st2common/util/greenpooldispatch.py
@@ -16,16 +16,22 @@
 import eventlet
 import Queue
 
+__all__ = [
+    'BufferedDispatcher'
+]
+
 
 class BufferedDispatcher(object):
 
     def __init__(self, dispatch_pool_size=50, monitor_thread_empty_q_sleep_time=5,
-                 monitor_thread_no_workers_sleep_time=1):
+                 monitor_thread_no_workers_sleep_time=1, name=None):
         self._pool_limit = dispatch_pool_size
         self._dispatcher_pool = eventlet.GreenPool(dispatch_pool_size)
         self._dispatch_monitor_thread = eventlet.greenthread.spawn(self._flush)
         self._monitor_thread_empty_q_sleep_time = monitor_thread_empty_q_sleep_time
         self._monitor_thread_no_workers_sleep_time = monitor_thread_no_workers_sleep_time
+        self._name = name
+
         self._work_buffer = Queue.Queue()
 
     def dispatch(self, handler, *args):
@@ -49,3 +55,11 @@ class BufferedDispatcher(object):
         while not self._work_buffer.empty() and self._dispatcher_pool.free() > 0:
             (handler, args) = self._work_buffer.get_nowait()
             self._dispatcher_pool.spawn(handler, *args)
+
+    def __repr__(self):
+        name = self._name or id(self)
+        values = (name, self._pool_limit, self._monitor_thread_empty_q_sleep_time,
+                  self._monitor_thread_no_workers_sleep_time)
+        return ('<BufferedDispatcher name=%s,dispatch_pool_size=%s,'
+                'monitor_thread_empty_q_sleep_time=%s,monitor_thread_no_workers_sleep_time=%s>' %
+                values)
