68051838b7c763bdc5ca590f572394c3b243e29a
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-07-27 17:43:44 -0700

    Switch to fixtures based testing for format_execution_result

--- contrib/chatops/tests/test_format_result.py
+++ contrib/chatops/tests/test_format_result.py
@@ -1,8 +1,8 @@
+import json
 import mock
 
 from st2tests.base import BaseActionTestCase
 
-from fixtures import REMOTE_SHELL_CMD_EXECUTION_MODEL
 from format_execution_result import FormatResultAction
 
 __all__ = [
@@ -13,8 +13,24 @@ __all__ = [
 class FormatResultActionTestCase(BaseActionTestCase):
     action_cls = FormatResultAction
 
-    @mock.patch.object(FormatResultAction, '_get_execution', mock.MagicMock(
-        return_value=REMOTE_SHELL_CMD_EXECUTION_MODEL))
     def test_rendering_works_remote_shell_cmd(self):
+        remote_shell_cmd_execution_model = json.loads(
+            self.get_fixture_content('remote_cmd_execution.json')
+        )
+
         action = self.get_action_instance()
+        action._get_execution = mock.MagicMock(
+            return_value=remote_shell_cmd_execution_model
+        )
         self.assertTrue(action.run(execution_id='57967f9355fc8c19a96d9e4f'))
+
+    def test_rendering_local_shell_cmd(self):
+        local_shell_cmd_execution_model = json.loads(
+            self.get_fixture_content('local_cmd_execution.json')
+        )
+
+        action = self.get_action_instance()
+        action._get_execution = mock.MagicMock(
+            return_value=local_shell_cmd_execution_model
+        )
+        self.assertTrue(action.run(execution_id='5799522f55fc8c2d33ac03e0'))
