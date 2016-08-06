8e99332aef274fb2269501f109918db91f4ebc58
Tomaz Muraus <tomaz@tomaz.me>
2016-08-06 10:06:48 +0200

    Don't use ram disk with MongoDB 2.4 and 2.6.
    Those versions don't use small files and require a lot more disk spaces (3 GB)
    because of space preallocation.

--- scripts/travis/install-and-run-mongodb.sh
+++ scripts/travis/install-and-run-mongodb.sh
@@ -2,7 +2,15 @@
 
 # Script which installs versions of MongoDB specified using an environment variable
 
+# Note: MongoDB 2.4 and 2.6 don't work with ramdisk since they don't work with
+# small files and require at least 3 GB of space
+if [ ${MONGODB} = '2.4.9' ] || [ ${MONGODB} = '2.6.12' ]; then
+    DATA_DIR=/tmp/mongodbdata
+else
+    DATA_DIR=/mnt/ramdisk/mongodb
+fi
+
 wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB}.tgz -O /tmp/mongodb.tgz
 tar -xvf /tmp/mongodb.tgz
-mkdir /mnt/ramdisk/mongodb
-${PWD}/mongodb-linux-x86_64-${MONGODB}/bin/mongod --dbpath /mnt/ramdisk/mongodb --bind_ip 127.0.0.1 &> /dev/null &
+mkdir -p ${DATA_DIR}
+${PWD}/mongodb-linux-x86_64-${MONGODB}/bin/mongod --dbpath ${DATA_DIR} --bind_ip 127.0.0.1 &> /dev/null &
