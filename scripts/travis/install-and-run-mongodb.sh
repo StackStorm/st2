2538a280b7acc0bf7d3ac8e1676675d6dae6cd33
Tomaz Muraus <tomaz@tomaz.me>
2016-08-06 09:32:21 +0200

    Move functionality into scripts, only install mongodb for unit and integration tests.

---
+++ scripts/travis/install-and-run-mongodb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Script which installs versions of MongoDB specified using an environment variable
+
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB}.tgz -O /tmp/mongodb.tgz
+tar -xvf /tmp/mongodb.tgz
+mkdir /tmp/ramdisk/mongodb
+${PWD}/mongodb-linux-x86_64-${MONGODB}/bin/mongod --dbpath /mnt/ramdisk/mongodb --bind_ip 127.0.0.1 &> /dev/null &
