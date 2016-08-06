e1ed4db45340724656e92fde48e4ac50a71a1ef9
Tomaz Muraus <tomaz@tomaz.me>
2016-08-06 09:40:45 +0200

    Fix the path.

--- scripts/travis/install-and-run-mongodb.sh
+++ scripts/travis/install-and-run-mongodb.sh
@@ -4,5 +4,5 @@
 
 wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB}.tgz -O /tmp/mongodb.tgz
 tar -xvf /tmp/mongodb.tgz
-mkdir /tmp/ramdisk/mongodb
+mkdir /mnt/ramdisk/mongodb
 ${PWD}/mongodb-linux-x86_64-${MONGODB}/bin/mongod --dbpath /mnt/ramdisk/mongodb --bind_ip 127.0.0.1 &> /dev/null &
