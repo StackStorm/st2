3c085ddb78dda7e82488de64f6ab814b16f55093
Anthony Shaw <anthonyshaw@apache.org>
2016-09-30 14:14:31 +1000

    add a service user and remove unused import

--- st2auth/tests/unit/test_handlers.py
+++ st2auth/tests/unit/test_handlers.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest2
 import mock
 from oslo_config import cfg
 import webob.exc
