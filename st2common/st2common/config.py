3e912e2c4a237587eb0709cbfa6f668baac4a6f3
Edward Medvedev <edward.medvedev@gmail.com>
2016-10-07 18:26:16 +0700

    Change default index path

--- st2common/st2common/config.py
+++ st2common/st2common/config.py
@@ -80,7 +80,7 @@ def register_opts(ignore_errors=False):
                    help='Path to the directory which contains system packs.'),
         cfg.StrOpt('packs_base_paths', default=None,
                    help='Paths which will be searched for integration packs.'),
-        cfg.ListOpt('index_url', default=['https://exchange.stackstorm.org/index/v1/index.json'],
+        cfg.ListOpt('index_url', default=['https://index.stackstorm.org/v1/index.json'],
                     help=('A URL pointing to the pack index. StackStorm Exchange is used by '
                           'default. Use a comma-separated list for multiple indexes if you '
                           'want to get other packs discovered with "st2 pack search".')),
