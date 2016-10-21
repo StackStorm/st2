b039fee58fd2a1a0e9b2329041f95678eecb2d00
Tomaz Muraus <tomaz@tomaz.me>
2016-10-21 10:53:23 +0200

    Update affected API endpoint rbac tests.

--- st2api/tests/unit/controllers/v1/test_rbac_for_supported_endpoints.py
+++ st2api/tests/unit/controllers/v1/test_rbac_for_supported_endpoints.py
@@ -142,6 +142,27 @@ class APIControllersRBACTestCase(APIControllerWithRBACTestCase):
                 'path': '/v1/packs/dummy_pack_1',
                 'method': 'GET'
             },
+            # Pack management
+            {
+                'path': '/v1/packs/install',
+                'method': 'POST',
+                'payload': {'packs': 'libcloud'}
+            },
+            {
+                'path': '/v1/packs/uninstall',
+                'method': 'POST',
+                'payload': {'packs': 'libcloud'}
+            },
+            {
+                'path': '/v1/packs/register',
+                'method': 'POST',
+                'payload': {'types': ['actions']}
+            },
+            {
+                'path': '/v1/packs/search',
+                'method': 'POST',
+                'payload': {'query': 'cloud'}
+            },
             # Pack views
             {
                 'path': '/v1/packs/views/files/dummy_pack_1',
