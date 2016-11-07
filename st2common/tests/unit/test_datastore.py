0716f65ee9ed409888256f7bbc92bd636febd948
Tomaz Muraus <tomaz@tomaz.me>
2016-11-07 10:12:25 +0100

    Fix lint.

--- st2common/tests/unit/test_datastore.py
+++ st2common/tests/unit/test_datastore.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import os
-import unittest2
 from datetime import timedelta
 from st2common.util.date import get_datetime_utc_now
 
