754a6f34e62e3a541a4c3b6802c39a38563e6604
Dmitri Zimine <dz@stackstorm.com>
2016-12-01 14:03:10 -0800

    Ed's comment

--- contrib/packs/actions/pack_mgmt/download.py
+++ contrib/packs/actions/pack_mgmt/download.py
@@ -278,7 +278,7 @@ class DownloadGitRepoAction(Action):
         """Allow passing short GitHub style URLs"""
         if not repo_url:
             raise Exception('No valid repo_url provided or could be inferred.')
-        if "file:" in repo_url:
+        if repo_url.startswith("file://"):
             return repo_url
         else:
             if len(repo_url.split('/')) == 2 and "git@" not in repo_url:
