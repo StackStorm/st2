4bae6468c5264391ce438e618e329f0b871978e4
Tomaz Muraus <tomaz@tomaz.me>
2017-02-07 13:01:42 +0100

    Use wget quiet mode.

--- .circle/install-and-run-mongodb.sh
+++ .circle/install-and-run-mongodb.sh
@@ -16,7 +16,7 @@ MONGODB_DIR=/tmp/mongodb
 mkdir -p ${DATA_DIR}
 mkdir -p ${MONGODB_DIR}
 
-wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz -O /tmp/mongodb.tgz
+wget -q http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz -O /tmp/mongodb.tgz
 tar -xvf /tmp/mongodb.tgz -C ${MONGODB_DIR} --strip=1
 echo "Starting MongoDB v${MONGODB_VERSION}"
 ${MONGODB_DIR}/bin/mongod --nojournal --journalCommitInterval 500 \
