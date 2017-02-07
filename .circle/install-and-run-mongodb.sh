c31d9a83cba747ca6100b93ce5b96bd987946033
Tomaz Muraus <tomaz@tomaz.me>
2017-02-07 12:08:48 +0100

    Use better and more explicit variable name.

--- .circle/install-and-run-mongodb.sh
+++ .circle/install-and-run-mongodb.sh
@@ -2,8 +2,8 @@
 
 # Script which installs versions of MongoDB specified using an environment variable
 
-if [ ! "${MONGODB}" ]; then
-    echo "MONGODB environment variable not set"
+if [ ! "${MONGODB_VERSION}" ]; then
+    echo "MONGODB_VERSION environment variable not set"
     exit 2
 fi
 
@@ -16,9 +16,9 @@ MONGODB_DIR=/tmp/mongodb
 mkdir -p ${DATA_DIR}
 mkdir -p ${MONGODB_DIR}
 
-wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB}.tgz -O /tmp/mongodb.tgz
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz -O /tmp/mongodb.tgz
 tar -xvf /tmp/mongodb.tgz -C ${MONGODB_DIR} --strip=1
-echo "Starting MongoDB v${MONGODB}"
+echo "Starting MongoDB v${MONGODB_VERSION}"
 ${MONGODB_DIR}/bin/mongod --nojournal --journalCommitInterval 500 \
     --syncdelay 0 --dbpath ${DATA_DIR} --bind_ip 127.0.0.1 &> /tmp/mongodb.log &
 EXIT_CODE=$?
