83ae3443d25b8f31f00286fda95d1af1ed08f0cb
Tomaz Muraus <tomaz@tomaz.me>
2017-02-07 08:24:43 +0100

    Also run unit and integration tests with MongoDB 3.4.

---
+++ .circle/install-mongodb.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Script which installs versions of MongoDB specified using an environment variable
+
+if [ ! "${MONGODB}" ]; then
+    echo "MONGODB environment variable not set"
+    exit 2
+fi
+
+# Note: MongoDB 2.4 and 2.6 don't work with ramdisk since they don't work with
+# small files and require at least 3 GB of space
+# TODO: Use ramdisk
+DATA_DIR=/tmp/mongodbdata
+
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB}.tgz -O /tmp/mongodb.tgz
+tar -xvf /tmp/mongodb.tgz
+mkdir -p ${DATA_DIR}
+echo "Starting MongoDB v${MONGODB}"
+${PWD}/mongodb-linux-x86_64-${MONGODB}/bin/mongod --nojournal --journalCommitInterval 500 \
+    --syncdelay 0 --dbpath ${DATA_DIR} --bind_ip 127.0.0.1 &> /tmp/mongodb.log &
+EXIT_CODE=$?
+sleep 5
+
+if [ ${EXIT_CODE} -ne 0 ]; then
+    echo "Failed to start MongoDB"
+    tail -30 /tmp/mongodb.log
+    exit 1
+fi
+
+tail -30 /tmp/mongob.log
