ad40201675aceb140e60bfc5e5901a2ec9cbe78f
Tomaz Muraus <tomaz@tomaz.me>
2017-02-09 13:03:10 +0100

    Add more logging.

--- .circle/install-and-run-mongodb.sh
+++ .circle/install-and-run-mongodb.sh
@@ -12,8 +12,12 @@ MONGODB_DIR=/tmp/mongodb
 mkdir -p ${DATA_DIR}
 mkdir -p ${MONGODB_DIR}
 
-wget -q http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz -O /tmp/mongodb.tgz
+DOWNLOAD_URL="http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${MONGODB_VERSION}.tgz"
+
+echo "Downloading MongoDB ${MONGODB_VERSION} from ${DOWNLOAD_URL}"
+wget -q ${DOWNLOAD_URL} -O /tmp/mongodb.tgz
 tar -xvf /tmp/mongodb.tgz -C ${MONGODB_DIR} --strip=1
+
 echo "Starting MongoDB v${MONGODB_VERSION}"
 ${MONGODB_DIR}/bin/mongod --nojournal --journalCommitInterval 500 \
     --syncdelay 0 --dbpath ${DATA_DIR} --bind_ip 127.0.0.1 &> /tmp/mongodb.log &
