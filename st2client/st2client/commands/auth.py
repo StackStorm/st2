4a9be54ff35c16b03b0399af92af6425cf747ad9
Tomaz Muraus <tomaz@tomaz.me>
2017-02-10 14:28:44 +0100

    Use ConfigParser from six.moves everywhere.

--- st2client/st2client/commands/auth.py
+++ st2client/st2client/commands/auth.py
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from six.moves.configparser import ConfigParser
 import getpass
 import json
 import logging
+
+from six.moves.configparser import ConfigParser
 
 from st2client.base import BaseCLIApp
 from st2client import config_parser
