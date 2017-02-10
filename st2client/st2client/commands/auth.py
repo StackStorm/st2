e28c01d29763cfd2f15d67c582e1dc6389262a25
Tomaz Muraus <tomaz@tomaz.me>
2017-02-10 13:51:06 +0100

    Add missing parenthesis.

--- st2client/st2client/commands/auth.py
+++ st2client/st2client/commands/auth.py
@@ -97,7 +97,7 @@ class LoginCommand(resource.ResourceCommand):
         self.parser.add_argument('-w', '--write-password', action='store_true', default=False,
                                  dest='write_password',
                                  help='Write the password in plain text to the config file '
-                                      '(default is to omit it')
+                                      '(default is to omit it)')
 
     def run(self, args, **kwargs):
 
