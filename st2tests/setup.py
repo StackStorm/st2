458b8932c66af31ac021acd9886793201a3131c3
Tomaz Muraus <tomaz@tomaz.me>
2017-03-02 09:29:22 +0100

    Add setup.py, manifest.in and other Python dist related files which with allow user to publish st2tests as package and include it in st2 packages.

--- st2tests/setup.py
+++ st2tests/setup.py
@@ -14,25 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os.path
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
+from dist_utils import fetch_requirements
+from dist_utils import apply_vagrant_workaround
+from st2tests import __version__
+
+ST2_COMPONENT = 'st2tests'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS_FILE = os.path.join(BASE_DIR, 'requirements.txt')
+
+install_reqs, dep_links = fetch_requirements(REQUIREMENTS_FILE)
+
+apply_vagrant_workaround()
 setup(
-    name='st2tests',
-    version='0.4.0',
-    description='',
+    name=ST2_COMPONENT,
+    version=__version__,
+    description='{}'.format(ST2_COMPONENT),
     author='StackStorm',
     author_email='info@stackstorm.com',
-    install_requires=[
-        "pecan",
-    ],
-    test_suite='st2tests',
+    install_requires=install_reqs,
+    dependency_links=dep_links,
+    test_suite=ST2_COMPONENT,
     zip_safe=False,
     include_package_data=True,
-    packages=find_packages(exclude=['ez_setup'])
+    packages=find_packages(exclude=['setuptools', 'tests'])
 )
