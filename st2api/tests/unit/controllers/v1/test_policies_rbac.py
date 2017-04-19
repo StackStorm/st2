1230266243e9fca8c7a1275589e6c51b6e8ab966
Tomaz Muraus <tomaz@tomaz.me>
2017-04-19 13:15:33 +0200

    Add RBAC API endpoint tests for policies get all and get one API endpoint.

--- st2api/tests/unit/controllers/v1/test_policies_rbac.py
+++ st2api/tests/unit/controllers/v1/test_policies_rbac.py
@@ -33,7 +33,8 @@ from tests.base import APIControllerWithRBACTestCase
 http_client = six.moves.http_client
 
 __all__ = [
-    'PolicyTypeControllerRBACTestCase'
+    'PolicyTypeControllerRBACTestCase',
+    'PolicyControllerRBACTestCase'
 ]
 
 FIXTURES_PACK = 'generic'
@@ -44,7 +45,8 @@ TEST_FIXTURES = {
     ],
     'policies': [
         'policy_1.yaml',
-        'policy_2.yaml'
+        'policy_2.yaml',
+        'policy_8.yaml'
     ]
 }
 
@@ -174,5 +176,172 @@ class PolicyTypeControllerRBACTestCase(APIControllerWithRBACTestCase):
         expected_msg = ('User "policy_type_view" doesn\'t have required permission '
                         '"policy_type_view" on resource "%s"' % (policy_type_uid))
         resp = self.app.get('/v1/policytypes/%s' % (policy_type_id), expect_errors=True)
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+
+class PolicyControllerRBACTestCase(APIControllerWithRBACTestCase):
+    fixtures_loader = FixturesLoader()
+
+    def setUp(self):
+        super(PolicyControllerRBACTestCase, self).setUp()
+        self.models = self.fixtures_loader.save_fixtures_to_db(fixtures_pack=FIXTURES_PACK,
+                                                               fixtures_dict=TEST_FIXTURES)
+
+        file_name = 'policy_1.yaml'
+        PolicyTypeControllerRBACTestCase.POLICY_1 = self.fixtures_loader.load_fixtures(
+            fixtures_pack=FIXTURES_PACK,
+            fixtures_dict={'policies': [file_name]})['policies'][file_name]
+
+        file_name = 'policy_2.yaml'
+        PolicyTypeControllerRBACTestCase.POLICY_TYPE_2 = self.fixtures_loader.load_fixtures(
+            fixtures_pack=FIXTURES_PACK,
+            fixtures_dict={'policies': [file_name]})['policies'][file_name]
+
+        # Insert mock users, roles and assignments
+
+        # Users
+        user_1_db = UserDB(name='policy_list')
+        user_1_db = User.add_or_update(user_1_db)
+        self.users['policy_list'] = user_1_db
+
+        user_2_db = UserDB(name='policy_view_direct_policy1')
+        user_2_db = User.add_or_update(user_2_db)
+        self.users['policy_view_direct_policy1'] = user_2_db
+
+        user_3_db = UserDB(name='policy_view_policy8_parent_pack')
+        user_3_db = User.add_or_update(user_3_db)
+        self.users['policy_view_policy8_parent_pack'] = user_3_db
+
+        # Roles
+        # policy_list
+        grant_db = PermissionGrantDB(resource_uid=None,
+                                     resource_type=ResourceType.POLICY,
+                                     permission_types=[PermissionType.POLICY_LIST])
+        grant_db = PermissionGrant.add_or_update(grant_db)
+        permission_grants = [str(grant_db.id)]
+        role_1_db = RoleDB(name='policy_list', permission_grants=permission_grants)
+        role_1_db = Role.add_or_update(role_1_db)
+        self.roles['policy_list'] = role_1_db
+
+        # policy_view directly on policy1
+        policy_type_uid = self.models['policies']['policy_1.yaml'].get_uid()
+        grant_db = PermissionGrantDB(resource_uid=policy_type_uid,
+                                     resource_type=ResourceType.POLICY,
+                                     permission_types=[PermissionType.POLICY_VIEW])
+        grant_db = PermissionGrant.add_or_update(grant_db)
+        permission_grants = [str(grant_db.id)]
+        role_1_db = RoleDB(name='policy_view_direct_policy1', permission_grants=permission_grants)
+        role_1_db = Role.add_or_update(role_1_db)
+        self.roles['policy_view_direct_policy1'] = role_1_db
+
+        # policy_view on a parent pack of policy 8
+        policy_pack_uid = self.models['policies']['policy_8.yaml'].get_pack_uid()
+        grant_db = PermissionGrantDB(resource_uid=policy_pack_uid,
+                                     resource_type=ResourceType.PACK,
+                                     permission_types=[PermissionType.POLICY_VIEW])
+        grant_db = PermissionGrant.add_or_update(grant_db)
+        permission_grants = [str(grant_db.id)]
+        role_1_db = RoleDB(name='policy_view_policy8_parent_pack',
+                           permission_grants=permission_grants)
+        role_1_db = Role.add_or_update(role_1_db)
+        self.roles['policy_view_policy8_parent_pack'] = role_1_db
+
+        # Role assignments
+        role_assignment_db = UserRoleAssignmentDB(
+            user=self.users['policy_list'].name,
+            role=self.roles['policy_list'].name)
+        UserRoleAssignment.add_or_update(role_assignment_db)
+
+        role_assignment_db = UserRoleAssignmentDB(
+            user=self.users['policy_view_direct_policy1'].name,
+            role=self.roles['policy_view_direct_policy1'].name)
+        UserRoleAssignment.add_or_update(role_assignment_db)
+
+        role_assignment_db = UserRoleAssignmentDB(
+            user=self.users['policy_view_policy8_parent_pack'].name,
+            role=self.roles['policy_view_policy8_parent_pack'].name)
+        UserRoleAssignment.add_or_update(role_assignment_db)
+
+    def test_get_all_no_permissions(self):
+        user_db = self.users['no_permissions']
+        self.use_user(user_db)
+
+        resp = self.app.get('/v1/policies', expect_errors=True)
+        expected_msg = ('User "no_permissions" doesn\'t have required permission '
+                        '"policy_list"')
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+    def test_get_one_no_permissions(self):
+        user_db = self.users['no_permissions']
+        self.use_user(user_db)
+
+        policy_id = self.models['policies']['policy_1.yaml'].id
+        policy_uid = self.models['policies']['policy_1.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id), expect_errors=True)
+        expected_msg = ('User "no_permissions" doesn\'t have required permission "policy_view"'
+                        ' on resource "%s"' % (policy_uid))
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+    def test_get_all_permission_success_get_one_no_permission_failure(self):
+        user_db = self.users['policy_list']
+        self.use_user(user_db)
+
+        # policy_list permission, but no policy_view permission
+        resp = self.app.get('/v1/policies')
+        self.assertEqual(resp.status_code, httplib.OK)
+        self.assertEqual(len(resp.json), 3)
+
+        policy_id = self.models['policies']['policy_1.yaml'].id
+        policy_uid = self.models['policies']['policy_1.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id), expect_errors=True)
+        expected_msg = ('User "policy_list" doesn\'t have required permission "policy_view"'
+                        ' on resource "%s"' % (policy_uid))
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+    def test_get_one_permission_success_get_all_no_permission_failure(self):
+        user_db = self.users['policy_view_direct_policy1']
+        self.use_user(user_db)
+
+        # policy_view permission, but no policy_type_list permission
+        resp = self.app.get('/v1/policies', expect_errors=True)
+        expected_msg = ('User "policy_view_direct_policy1" doesn\'t have required permission '
+                        '"policy_list"')
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+        # policy_view in policy_1, but not on policy_2
+        policy_id = self.models['policies']['policy_1.yaml'].id
+        policy_uid = self.models['policies']['policy_1.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id))
+        self.assertEqual(resp.status_code, httplib.OK)
+        self.assertEqual(resp.json['uid'], policy_uid)
+
+        policy_id = self.models['policies']['policy_2.yaml'].id
+        policy_uid = self.models['policies']['policy_2.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id), expect_errors=True)
+        expected_msg = ('User "policy_view_direct_policy1" doesn\'t have required permission'
+                        ' "policy_view" on resource "%s"' % (policy_uid))
+        self.assertEqual(resp.status_code, httplib.FORBIDDEN)
+        self.assertEqual(resp.json['faultstring'], expected_msg)
+
+        # policy_view on parent pack of policy8, but not on policy1
+        user_db = self.users['policy_view_policy8_parent_pack']
+        self.use_user(user_db)
+
+        policy_id = self.models['policies']['policy_8.yaml'].id
+        policy_uid = self.models['policies']['policy_8.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id))
+        self.assertEqual(resp.status_code, httplib.OK)
+        self.assertEqual(resp.json['uid'], policy_uid)
+
+        policy_id = self.models['policies']['policy_1.yaml'].id
+        policy_uid = self.models['policies']['policy_1.yaml'].get_uid()
+        resp = self.app.get('/v1/policies/%s' % (policy_id), expect_errors=True)
+        expected_msg = ('User "policy_view_policy8_parent_pack" doesn\'t have required permission'
+                        ' "policy_view" on resource "%s"' % (policy_uid))
         self.assertEqual(resp.status_code, httplib.FORBIDDEN)
         self.assertEqual(resp.json['faultstring'], expected_msg)
