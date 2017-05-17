6830ceb0ccbfea61f9228d6ed6e018ed9454b533
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2017-05-17 16:57:55 -0700

    Set jitter interval for mistral query to 0 in tests

--- st2tests/st2tests/config.py
+++ st2tests/st2tests/config.py
@@ -79,6 +79,7 @@ def _override_common_opts():
     CONF.set_override(name='mask_secrets', override=True, group='log')
     CONF.set_override(name='url', override='zake://', group='coordination')
     CONF.set_override(name='lock_timeout', override=1, group='coordination')
+    CONF.set_override(name='jitter_interval', override=0, group='mistral')
 
 
 def _override_api_opts():
