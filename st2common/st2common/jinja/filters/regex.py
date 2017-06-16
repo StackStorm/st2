223b0a33f910feeaaaf6bb3bb732a2b478848e98
Matt Oswalt <oswaltm@brocade.com>
2017-06-16 12:17:35 -0700

    Updated tests and refactored filter implementation to use findall
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- st2common/st2common/jinja/filters/regex.py
+++ st2common/st2common/jinja/filters/regex.py
@@ -49,14 +49,8 @@ def regex_search(value, pattern='', ignorecase=False):
     return bool(re.search(pattern, value, flags))
 
 
-def regex_substring(value, pattern='', ignorecase=False, group_index=0):
+def regex_substring(value, pattern='', result_index=0, ignorecase=False):
     if not isinstance(value, six.string_types):
         value = str(value)
     flags = _get_regex_flags(ignorecase)
-    try:
-        result = re.search(pattern, value, flags).groups()[group_index]
-    except (AttributeError, IndexError):
-        # AttributeError - no match, so re.search(pattern, value, flags) returned None
-        # IndexError - no grouping characters in regex pattern
-        result = ""
-    return result
+    return re.findall(pattern, value, flags)[result_index]
