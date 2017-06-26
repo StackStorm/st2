f7f2824f97adba39c321d91b761b0ca976fa5331
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2017-06-26 07:32:46 -0700

    Execution formatter unescaping should be done before safe_dump

--- st2client/st2client/formatters/execution.py
+++ st2client/st2client/formatters/execution.py
@@ -41,7 +41,7 @@ class ExecutionResult(formatters.Formatter):
             entry = vars(entry)
             output = ''
             for attr in attrs:
-                value = jsutil.get_value(entry, attr)
+                value = strutil.unescape(jsutil.get_value(entry, attr))
                 if (isinstance(value, basestring) and len(value) > 0 and
                         value[0] in ['{', '['] and value[len(value) - 1] in ['}', ']']):
                     new_value = ast.literal_eval(value)
@@ -60,4 +60,4 @@ class ExecutionResult(formatters.Formatter):
                     value = ('\n' if isinstance(value, dict) else '') + formatted_value
                 output += ('\n' if output else '') + '%s: %s' % \
                     (DisplayColors.colorize(attr, DisplayColors.BLUE), value)
-        return strutil.unescape(output)
+        return output
