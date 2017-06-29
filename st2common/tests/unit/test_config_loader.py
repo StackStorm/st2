c533307b737e25e1f631afaf1c643e28bd9d5464
Tomaz Muraus <tomaz@tomaz.me>
2017-06-29 14:50:59 +0200

    Add a test case for config schema default values when a default value is falsey.

--- st2common/tests/unit/test_config_loader.py
+++ st2common/tests/unit/test_config_loader.py
@@ -130,6 +130,17 @@ class ContentPackConfigLoaderTestCase(CleanDbTestCase):
         config = loader.get_config()
         self.assertEqual(config['region'], 'default-region-value')
 
+    def test_default_values_are_used_when_default_values_are_falsy(self):
+        pack_name = 'dummy_pack_17'
+
+        loader = ContentPackConfigLoader(pack_name=pack_name)
+        config = loader.get_config()
+
+        # 1. Default values are used
+        self.assertEqual(config['key_with_default_falsy_value_1'], False)
+        self.assertEqual(config['key_with_default_falsy_value_2'], None)
+        self.assertEqual(config['key_with_default_falsy_value_3'], {})
+
     def test_get_config_nested_schema_default_values_from_config_schema_are_used(self):
         # Special case for more complex config schemas with attributes ntesting.
         # Validate that the default values are also used for one level nested object properties.
