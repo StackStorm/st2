86c987151377e25680ce03de2e13458dc8a0f0d0
Matt Oswalt <oswaltm@brocade.com>
2017-07-26 22:19:23 -0700

    Added version_match filter
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- st2tests/integration/mistral/test_filters.py
+++ st2tests/integration/mistral/test_filters.py
@@ -281,3 +281,29 @@ class VersionEqualFiltersTest(base.TestWorkflowExecution):
             self._assert_success(execution, num_tasks=1)
             self.assertEqual(execution.result['result_jinja'], expected_result)
             self.assertEqual(execution.result['result_yaql'], expected_result)
+
+
+class VersionMatchFiltersTest(base.TestWorkflowExecution):
+
+    def test_version_match(self):
+
+        versions = {
+            '>=0.9.3': True,
+            '>0.11.3': False,
+            '==0.9.3': False,
+            '<=0.10.1': True,
+            '<0.10.2': True
+        }
+
+        for compare_version, expected_result in versions.items():
+            execution = self._execute_workflow(
+                'examples.mistral-customfilters-version_match',
+                parameters={
+                    "version_a": '0.10.1',
+                    "version_b": compare_version
+                }
+            )
+            execution = self._wait_for_completion(execution)
+            self._assert_success(execution, num_tasks=1)
+            self.assertEqual(execution.result['result_jinja'], expected_result)
+            self.assertEqual(execution.result['result_yaql'], expected_result)
