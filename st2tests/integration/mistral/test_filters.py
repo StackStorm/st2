d79c0317246c2558c3a9715c79c365ee62b90057
Matt Oswalt <oswaltm@brocade.com>
2017-07-26 19:21:04 -0700

    Alphabetized tests
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- st2tests/integration/mistral/test_filters.py
+++ st2tests/integration/mistral/test_filters.py
@@ -23,6 +23,24 @@ REGEX_SEARCH_STR_2 = "567 Elsewhere Dr is your address. My address is 123 Somewh
 REGEX_SEARCH_STR_3 = "No address to be found here! Well, maybe 127.0.0.1"
 
 
+class JsonEscapeFiltersTest(base.TestWorkflowExecution):
+
+    def test_to_complex(self):
+        breaking_str = 'This text """ breaks JSON'
+        inputs = {'input_str': breaking_str}
+        execution = self._execute_workflow(
+            'examples.mistral-customfilters-json_escape', parameters=inputs
+        )
+        execution = self._wait_for_completion(execution)
+        self._assert_success(execution, num_tasks=1)
+        jinja_dict = json.loads(execution.result['result_jinja'])[0]
+        yaql_dict = json.loads(execution.result['result_yaql'])[0]
+        self.assertTrue(isinstance(jinja_dict, dict))
+        self.assertEqual(jinja_dict["title"], breaking_str)
+        self.assertTrue(isinstance(yaql_dict, dict))
+        self.assertEqual(yaql_dict["title"], breaking_str)
+
+
 class RegexMatchFiltersTest(base.TestWorkflowExecution):
 
     def test_regex_match(self):
@@ -121,6 +139,22 @@ class UseNoneFiltersTest(base.TestWorkflowExecution):
         self.assertEqual(execution.result['str_result_yaql'], 'foo')
 
 
+class ToComplexFiltersTest(base.TestWorkflowExecution):
+
+    def test_to_complex(self):
+        execution = self._execute_workflow(
+            'examples.mistral-customfilters-to_complex'
+        )
+        execution = self._wait_for_completion(execution)
+        self._assert_success(execution, num_tasks=2)
+        jinja_dict = json.loads(execution.result['result_jinja'])
+        yaql_dict = json.loads(execution.result['result_yaql'])
+        self.assertTrue(isinstance(jinja_dict, dict))
+        self.assertEqual(jinja_dict["a"], "b")
+        self.assertTrue(isinstance(yaql_dict, dict))
+        self.assertEqual(yaql_dict["a"], "b")
+
+
 class ToJsonStringFiltersTest(base.TestWorkflowExecution):
 
     def test_to_json_string(self):
@@ -151,37 +185,3 @@ class ToYamlStringFiltersTest(base.TestWorkflowExecution):
         self.assertEqual(jinja_dict["a"], "b")
         self.assertTrue(isinstance(yaql_dict, dict))
         self.assertEqual(yaql_dict["a"], "b")
-
-
-class ToComplexFiltersTest(base.TestWorkflowExecution):
-
-    def test_to_complex(self):
-        execution = self._execute_workflow(
-            'examples.mistral-customfilters-to_complex'
-        )
-        execution = self._wait_for_completion(execution)
-        self._assert_success(execution, num_tasks=2)
-        jinja_dict = json.loads(execution.result['result_jinja'])
-        yaql_dict = json.loads(execution.result['result_yaql'])
-        self.assertTrue(isinstance(jinja_dict, dict))
-        self.assertEqual(jinja_dict["a"], "b")
-        self.assertTrue(isinstance(yaql_dict, dict))
-        self.assertEqual(yaql_dict["a"], "b")
-
-
-class JsonEscapeFiltersTest(base.TestWorkflowExecution):
-
-    def test_to_complex(self):
-        breaking_str = 'This text """ breaks JSON'
-        inputs = {'input_str': breaking_str}
-        execution = self._execute_workflow(
-            'examples.mistral-customfilters-json_escape', parameters=inputs
-        )
-        execution = self._wait_for_completion(execution)
-        self._assert_success(execution, num_tasks=1)
-        jinja_dict = json.loads(execution.result['result_jinja'])[0]
-        yaql_dict = json.loads(execution.result['result_yaql'])[0]
-        self.assertTrue(isinstance(jinja_dict, dict))
-        self.assertEqual(jinja_dict["title"], breaking_str)
-        self.assertTrue(isinstance(yaql_dict, dict))
-        self.assertEqual(yaql_dict["title"], breaking_str)
