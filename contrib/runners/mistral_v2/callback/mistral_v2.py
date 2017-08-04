741aad1480323990b70ece772dee15400f3c123a
Matt Oswalt <matt@keepingitclassless.net>
2017-08-04 13:11:34 -0700

    Added pending status to mistral_v2 callback
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- contrib/runners/mistral_v2/callback/mistral_v2.py
+++ contrib/runners/mistral_v2/callback/mistral_v2.py
@@ -40,7 +40,8 @@ STATUS_MAP = {
     action_constants.LIVEACTION_STATUS_TIMED_OUT: 'ERROR',
     action_constants.LIVEACTION_STATUS_ABANDONED: 'ERROR',
     action_constants.LIVEACTION_STATUS_CANCELING: 'RUNNING',
-    action_constants.LIVEACTION_STATUS_CANCELED: 'CANCELLED'
+    action_constants.LIVEACTION_STATUS_CANCELED: 'CANCELLED',
+    action_constants.LIVEACTION_STATUS_PENDING: 'PENDING'
 }
 
 
