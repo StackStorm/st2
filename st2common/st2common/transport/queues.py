dee9f961ce9799747626fe0f0b7655ebc2f2ff50
Tomaz Muraus <tomaz@tomaz.me>
2017-08-09 13:15:03 +0200

    Fix queue name.

--- st2common/st2common/transport/queues.py
+++ st2common/st2common/transport/queues.py
@@ -87,5 +87,5 @@ STREAM_LIVEACTION_WORK_QUEUE = Queue(None, liveaction.LIVEACTION_XCHG,
 # later, if needed
 STREAM_EXECUTION_STDOUT_QUEUE = execution.get_stdout_queue(name=None,
                                                            routing_key=publishers.CREATE_RK)
-STREAM_EXECUTION_STDERR_QUEUE = execution.get_stdout_queue(name=None,
+STREAM_EXECUTION_STDERR_QUEUE = execution.get_stderr_queue(name=None,
                                                            routing_key=publishers.CREATE_RK)
