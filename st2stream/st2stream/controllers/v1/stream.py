c9a5625be7c5af9d0eed0ebe19d7e5f81c873965
Tomaz Muraus <tomaz@tomaz.me>
2017-08-16 14:43:32 +0200

    Fix typo.

--- st2stream/st2stream/controllers/v1/stream.py
+++ st2stream/st2stream/controllers/v1/stream.py
@@ -41,7 +41,7 @@ def format(gen):
 
 
 class StreamController(object):
-    def get_all(self, events=None, action_refs=None, execution_ids=None, requster_user=None):
+    def get_all(self, events=None, action_refs=None, execution_ids=None, requester_user=None):
         events = events.split(',') if events else None
         action_refs = action_refs.split(',') if action_refs else None
         execution_ids = execution_ids.split(',') if execution_ids else None
