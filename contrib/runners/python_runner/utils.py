4c4285b498e5f8aa3464eb851a196db4c2386a43
Tomaz Muraus <tomaz@tomaz.me>
2017-08-17 10:35:28 +0200

    Try making it a package to see if this works.

--- contrib/runners/python_runner/utils.py
+++ contrib/runners/python_runner/utils.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 from oslo_config import cfg
 
 from st2common.constants.action import ACTION_OUTPUT_RESULT_DELIMITER
