8c8018f592e9b5c5854eb1fec58e7db6a8b38733
Nick Maludy <nmaludy@gmail.com>
2017-09-28 20:02:39 -0400

    Fixed jsonpath import

--- st2common/st2common/jinja/filters/jsonpath_query.py
+++ st2common/st2common/jinja/filters/jsonpath_query.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import jsonpath-rw
+import jsonpath_rw
 
 __all__ = [
     'jsonpath_query',
