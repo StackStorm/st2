d0e21061e0f846a98d6f3bfbe3699cbd5722301a
Anthony Shaw <anthonyshaw@apache.org>
2018-01-29 11:35:23 +1100

    add optional arg to find_caller. Python 3.x logger.findCaller takes 1 arg

--- st2common/st2common/log.py
+++ st2common/st2common/log.py
@@ -63,7 +63,7 @@ LOGGER_KEYS = [
 _srcfile = get_normalized_file_path(__file__)
 
 
-def find_caller():
+def find_caller(stack_info=None):
     """
     Find the stack frame of the caller so that we can note the source file name, line number and
     function name.
