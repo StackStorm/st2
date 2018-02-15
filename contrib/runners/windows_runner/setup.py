1ed18310bf2a4d01e2995f6a2ead72ed79c700ca
Tomaz Muraus <tomaz@tomaz.me>
2018-02-15 14:56:17 +0100

    Fix directory structure.

--- contrib/runners/windows_runner/setup.py
+++ contrib/runners/windows_runner/setup.py
@@ -44,7 +44,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     packages=find_packages(exclude=['setuptools', 'tests']),
-    package_data={'windows_script_runner': ['runner.yaml']},
+    package_data={'windows_runner': ['runner.yaml']},
     scripts=[],
     entry_points={
         'st2common.runners.runner': [
