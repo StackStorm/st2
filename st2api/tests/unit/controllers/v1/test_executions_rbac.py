dd4533caebd8053b7d34e3a77f026ccd9cead8e3
Tomaz Muraus <tomaz@tomaz.me>
2018-03-29 12:29:14 +0200

    Add / improve RBAC permission isolation executions test case for various scenarios:
    * user is admin
    * user is system user
    * non system user

--- st2api/tests/unit/controllers/v1/test_executions_rbac.py
+++ st2api/tests/unit/controllers/v1/test_executions_rbac.py
@@ -13,19 +13,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-
 import mock
 from oslo_config import cfg
 
 from six.moves import http_client
-import st2common.validators.api.action as action_validator
+
+from st2common.validators.api import action as action_validator
+from st2common.rbac.types import PermissionType
+from st2common.rbac.types import ResourceType
 from st2common.models.db.auth import UserDB
 from st2common.persistence.auth import User
 from st2common.models.db.rbac import RoleDB
 from st2common.models.db.rbac import UserRoleAssignmentDB
+from st2common.models.db.rbac import PermissionGrantDB
 from st2common.persistence.rbac import UserRoleAssignment
 from st2common.persistence.rbac import Role
+from st2common.persistence.rbac import PermissionGrant
 from st2common.transport.publishers import PoolPublisher
 from tests.base import APIControllerWithRBACTestCase
 from tests.base import BaseActionExecutionControllerTestCase
@@ -60,26 +63,60 @@ class ActionExecutionRBACControllerTestCase(BaseActionExecutionControllerTestCas
         user_1_db = User.add_or_update(user_1_db)
         self.users['multiple_roles'] = user_1_db
 
+        user_2_db = UserDB(name='user_two')
+        user_2_db = User.add_or_update(user_2_db)
+        self.users['user_two'] = user_2_db
+
+        user_3_db = UserDB(name='user_three')
+        user_3_db = User.add_or_update(user_3_db)
+        self.users['user_three'] = user_3_db
+
         # Roles
         roles = ['role_1', 'role_2', 'role_3']
         for role in roles:
             role_db = RoleDB(name=role)
             Role.add_or_update(role_db)
 
+        # action_execute on parent pack
+        grant_1_db = PermissionGrantDB(resource_uid='pack:wolfpack',
+                                       resource_type=ResourceType.PACK,
+                                       permission_types=[PermissionType.ACTION_EXECUTE])
+        grant_1_db = PermissionGrant.add_or_update(grant_1_db)
+        grant_2_db = PermissionGrantDB(resource_uid=None,
+                                     resource_type=ResourceType.EXECUTION,
+                                     permission_types=[PermissionType.EXECUTION_LIST])
+        grant_2_db = PermissionGrant.add_or_update(grant_2_db)
+        permission_grants = [str(grant_1_db.id), str(grant_2_db.id)]
+
+        role_1_db = RoleDB(name='role_4', permission_grants=permission_grants)
+        role_1_db = Role.add_or_update(role_1_db)
+        self.roles['role_4'] = role_1_db
+
         # Role assignments
-        user_db = self.users['multiple_roles']
         role_assignment_db = UserRoleAssignmentDB(
-            user=user_db.name,
+            user=user_1_db.name,
             role='admin',
-            source='assignments/%s.yaml' % user_db.name)
+            source='assignments/%s.yaml' % user_1_db.name)
         UserRoleAssignment.add_or_update(role_assignment_db)
 
         for role in roles:
             role_assignment_db = UserRoleAssignmentDB(
-                user=user_db.name,
+                user=user_1_db.name,
                 role=role,
-                source='assignments/%s.yaml' % user_db.name)
+                source='assignments/%s.yaml' % user_1_db.name)
             UserRoleAssignment.add_or_update(role_assignment_db)
+
+        role_assignment_db = UserRoleAssignmentDB(
+            user=user_2_db.name,
+            role='role_4',
+            source='assignments/%s.yaml' % user_2_db.name)
+        UserRoleAssignment.add_or_update(role_assignment_db)
+
+        role_assignment_db = UserRoleAssignmentDB(
+            user=user_3_db.name,
+            role='role_4',
+            source='assignments/%s.yaml' % user_2_db.name)
+        UserRoleAssignment.add_or_update(role_assignment_db)
 
     def test_post_rbac_info_in_context_success(self):
         # When RBAC is enabled, additional RBAC related info should be included in action_context
@@ -139,9 +176,9 @@ class ActionExecutionRBACControllerTestCase(BaseActionExecutionControllerTestCas
         resp = self.app.get('/v1/actionexecutions?limit=-1')
         self.assertEqual(resp.status_code, http_client.OK)
 
-    def test_get_respective_actions(self):
-        cfg.CONF.set_override(name='permission_isolation', override=True,
-                              group='rbac')
+    def test_get_respective_actions_with_permission_isolation(self):
+        cfg.CONF.set_override(name='permission_isolation', override=True, group='rbac')
+
         data = {
             'action': 'wolfpack.action-1',
             'parameters': {
@@ -156,10 +193,8 @@ class ActionExecutionRBACControllerTestCase(BaseActionExecutionControllerTestCas
         resp = self._do_post(data)
         self.assertEqual(resp.status_code, http_client.CREATED)
 
-        resp1 = self.app.get('/v1/actionexecutions?limit=-1')
-
         # User with multiple roles assignment
-        user_db = self.users['multiple_roles']
+        user_db = self.users['user_two']
         self.use_user(user_db)
 
         resp = self._do_post(data)
@@ -167,45 +202,53 @@ class ActionExecutionRBACControllerTestCase(BaseActionExecutionControllerTestCas
         resp = self._do_post(data)
         self.assertEqual(resp.status_code, http_client.CREATED)
 
-        resp2 = self.app.get('/v1/actionexecutions?limit=-1')
+        # User two
+        user_db = self.users['user_three']
+        self.use_user(user_db)
 
-        self.assertEqual(len(json.loads(resp1.body)), 1)
-        self.assertEqual(len(json.loads(resp2.body)), 2)
+        resp = self._do_post(data)
+        self.assertEqual(resp.status_code, http_client.CREATED)
+        resp = self._do_post(data)
+        self.assertEqual(resp.status_code, http_client.CREATED)
 
-    def test_get_respective_actions_positive_limit(self):
-        cfg.CONF.set_override(name='permission_isolation', override=True,
-                              group='rbac')
-        data = {
-            'action': 'wolfpack.action-1',
-            'parameters': {
-                'actionstr': 'foo'
-            }
-        }
-
-        # User with admin role assignment
+        # 1. Admin can view all
         user_db = self.users['admin']
         self.use_user(user_db)
 
-        resp = self._do_post(data)
-        self.assertEqual(resp.status_code, http_client.CREATED)
+        resp = self.app.get('/v1/actionexecutions?limit=100')
+        self.assertEqual(len(resp.json), (1 + 2 + 2))
+        self.assertEqual(resp.json[0]['context']['user'], 'user_three')
+        self.assertEqual(resp.json[1]['context']['user'], 'user_three')
+        self.assertEqual(resp.json[2]['context']['user'], 'user_two')
+        self.assertEqual(resp.json[3]['context']['user'], 'user_two')
+        self.assertEqual(resp.json[4]['context']['user'], 'admin')
 
-        resp1 = self.app.get('/v1/actionexecutions?limit=1')
-
-        # User with multiple roles assignment
-        user_db = self.users['multiple_roles']
+        # 2. System user can view all
+        user_db = self.users['system_user']
         self.use_user(user_db)
 
-        resp = self._do_post(data)
-        self.assertEqual(resp.status_code, http_client.CREATED)
-        resp = self._do_post(data)
-        self.assertEqual(resp.status_code, http_client.CREATED)
+        resp = self.app.get('/v1/actionexecutions?limit=100')
+        self.assertEqual(len(resp.json), (1 + 2 + 2))
+        self.assertEqual(resp.json[0]['context']['user'], 'user_three')
+        self.assertEqual(resp.json[1]['context']['user'], 'user_three')
+        self.assertEqual(resp.json[2]['context']['user'], 'user_two')
+        self.assertEqual(resp.json[3]['context']['user'], 'user_two')
+        self.assertEqual(resp.json[4]['context']['user'], 'admin')
 
-        resp2 = self.app.get('/v1/actionexecutions?limit=1')
-
-        user_db = self.users['observer']
+        # 3. User two can only view their own
+        user_db = self.users['user_two']
         self.use_user(user_db)
-        resp3 = self.app.get('/v1/actionexecutions?limit=1')
 
-        self.assertEqual(len(json.loads(resp1.body)), 1)
-        self.assertEqual(len(json.loads(resp2.body)), 1)
-        self.assertEqual(len(json.loads(resp3.body)), 0)
+        resp = self.app.get('/v1/actionexecutions?limit=100')
+        self.assertEqual(len(resp.json), 2)
+        self.assertEqual(resp.json[0]['context']['user'], 'user_two')
+        self.assertEqual(resp.json[1]['context']['user'], 'user_two')
+
+        # 4. User three can only view their own
+        user_db = self.users['user_three']
+        self.use_user(user_db)
+
+        resp = self.app.get('/v1/actionexecutions?limit=100')
+        self.assertEqual(len(resp.json), 2)
+        self.assertEqual(resp.json[0]['context']['user'], 'user_three')
+        self.assertEqual(resp.json[1]['context']['user'], 'user_three')
