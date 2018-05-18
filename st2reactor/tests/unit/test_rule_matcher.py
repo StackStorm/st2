71c1ae684307cb62382dc2292618f6251a35e7ac
Tomaz Muraus <tomaz@tomaz.me>
2018-05-18 12:46:13 +0200

    Add test cases for it.
    Also fix existing test cases so they don't rely on each other and can be
    run indepdently of each other.

--- st2reactor/tests/unit/test_rule_matcher.py
+++ st2reactor/tests/unit/test_rule_matcher.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 
 from __future__ import absolute_import
+
 import six
+import mock
 
 from st2common.models.api.rule import RuleAPI
 from st2common.models.db.trigger import (TriggerDB, TriggerTypeDB)
@@ -24,22 +26,131 @@ from st2common.services.triggers import get_trigger_db_by_ref
 from st2common.util import date as date_utils
 import st2reactor.container.utils as container_utils
 from st2reactor.rules.matcher import RulesMatcher
+from st2common.persistence.rule_enforcement import RuleEnforcement
+
 from st2tests.base import DbTestCase
+from st2tests.base import CleanDbTestCase
 from st2tests.fixturesloader import FixturesLoader
 
+__all__ = [
+    'RuleMatcherTestCase',
+    'BackstopRuleMatcherTestCase'
+]
 
-class RuleMatcherTest(DbTestCase):
+# Mock rules
+RULE_1 = {
+    'enabled': True,
+    'name': 'st2.test.rule1',
+    'pack': 'yoyohoneysingh',
+    'trigger': {
+        'type': 'dummy_pack_1.st2.test.trigger1'
+    },
+    'criteria': {
+        'k1': {                     # Missing prefix 'trigger'. This rule won't match.
+            'pattern': 't1_p_v',
+            'type': 'equals'
+        }
+    },
+    'action': {
+        'ref': 'sixpack.st2.test.action',
+        'parameters': {
+            'ip2': '{{rule.k1}}',
+            'ip1': '{{trigger.t1_p}}'
+        }
+    },
+    'id': '23',
+    'description': ''
+}
+
+RULE_2 = {                      # Rule should match.
+    'enabled': True,
+    'name': 'st2.test.rule2',
+    'pack': 'yoyohoneysingh',
+    'trigger': {
+        'type': 'dummy_pack_1.st2.test.trigger1'
+    },
+    'criteria': {
+        'trigger.k1': {
+            'pattern': 't1_p_v',
+            'type': 'equals'
+        }
+    },
+    'action': {
+        'ref': 'sixpack.st2.test.action',
+        'parameters': {
+            'ip2': '{{rule.k1}}',
+            'ip1': '{{trigger.t1_p}}'
+        }
+    },
+    'id': '23',
+    'description': ''
+}
+
+RULE_3 = {
+    'enabled': False,         # Disabled rule shouldn't match.
+    'name': 'st2.test.rule3',
+    'pack': 'yoyohoneysingh',
+    'trigger': {
+        'type': 'dummy_pack_1.st2.test.trigger1'
+    },
+    'criteria': {
+        'trigger.k1': {
+            'pattern': 't1_p_v',
+            'type': 'equals'
+        }
+    },
+    'action': {
+        'ref': 'sixpack.st2.test.action',
+        'parameters': {
+            'ip2': '{{rule.k1}}',
+            'ip1': '{{trigger.t1_p}}'
+        }
+    },
+    'id': '23',
+    'description': ''
+}
+
+RULE_4 = {                      # Rule should match.
+    'enabled': True,
+    'name': 'st2.test.rule4',
+    'pack': 'yoyohoneysingh',
+    'trigger': {
+        'type': 'dummy_pack_1.st2.test.trigger4'
+    },
+    'criteria': {
+        'trigger.k1': {
+            'pattern': 't2_p_v',
+            'type': 'equals'
+        }
+    },
+    'action': {
+        'ref': 'sixpack.st2.test.action',
+        'parameters': {
+            'ip2': '{{rule.k1}}',
+            'ip1': '{{trigger.t1_p}}'
+        }
+    },
+    'id': '23',
+    'description': ''
+}
+
+
+class RuleMatcherTestCase(CleanDbTestCase):
     rules = []
 
     def test_get_matching_rules(self):
         self._setup_sample_trigger('st2.test.trigger1')
+        rule_db_1 = self._setup_sample_rule(RULE_1)
+        rule_db_2 = self._setup_sample_rule(RULE_2)
+        rule_db_3 = self._setup_sample_rule(RULE_3)
+        rules = [rule_db_1, rule_db_2, rule_db_3]
         trigger_instance = container_utils.create_trigger_instance(
             'dummy_pack_1.st2.test.trigger1',
             {'k1': 't1_p_v', 'k2': 'v2'},
             date_utils.get_datetime_utc_now()
         )
+
         trigger = get_trigger_db_by_ref(trigger_instance.trigger)
-        rules = self._get_sample_rules()
         rules_matcher = RulesMatcher(trigger_instance, trigger, rules)
         matching_rules = rules_matcher.get_matching_rules()
         self.assertTrue(matching_rules is not None)
@@ -47,19 +158,119 @@ class RuleMatcherTest(DbTestCase):
 
     def test_trigger_instance_payload_with_special_values(self):
         # Test a rule where TriggerInstance payload contains a dot (".") and $
+        self._setup_sample_trigger('st2.test.trigger1')
         self._setup_sample_trigger('st2.test.trigger2')
+        rule_db_1 = self._setup_sample_rule(RULE_1)
+        rule_db_2 = self._setup_sample_rule(RULE_2)
+        rule_db_3 = self._setup_sample_rule(RULE_3)
+        rules = [rule_db_1, rule_db_2, rule_db_3]
         trigger_instance = container_utils.create_trigger_instance(
             'dummy_pack_1.st2.test.trigger2',
             {'k1': 't1_p_v', 'k2.k2': 'v2', 'k3.more.nested.deep': 'some.value',
              'k4.even.more.nested$': 'foo', 'yep$aaa': 'b'},
             date_utils.get_datetime_utc_now()
         )
+
         trigger = get_trigger_db_by_ref(trigger_instance.trigger)
-        rules = self._get_sample_rules()
         rules_matcher = RulesMatcher(trigger_instance, trigger, rules)
         matching_rules = rules_matcher.get_matching_rules()
         self.assertTrue(matching_rules is not None)
         self.assertEqual(len(matching_rules), 1)
+
+    @mock.patch('st2reactor.rules.matcher.RuleFilter._render_criteria_pattern',
+                mock.Mock(side_effect=Exception('exception in _render_criteria_pattern')))
+    def test_rule_enforcement_is_created_on_exception_1(self):
+        # 1. Exception in _render_criteria_pattern
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(rule_enforcement_dbs, [])
+
+        self._setup_sample_trigger('st2.test.trigger4')
+        rule_4_db = self._setup_sample_rule(RULE_4)
+        rules = [rule_4_db]
+        trigger_instance = container_utils.create_trigger_instance(
+            'dummy_pack_1.st2.test.trigger4',
+            {'k1': 't2_p_v', 'k2': 'v2'},
+            date_utils.get_datetime_utc_now()
+        )
+        trigger = get_trigger_db_by_ref(trigger_instance.trigger)
+
+        rules_matcher = RulesMatcher(trigger_instance, trigger, rules)
+        matching_rules = rules_matcher.get_matching_rules()
+        self.assertEqual(matching_rules, [])
+        self.assertEqual(len(matching_rules), 0)
+
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(len(rule_enforcement_dbs), 1)
+
+        expected_failure = ('Failed to match rule "yoyohoneysingh.st2.test.rule4" against trigger '
+                            'instance "%s": Failed to render pattern value "t2_p_v" for key '
+                            '"trigger.k1"' % (str(trigger_instance.id)))
+        self.assertEqual(rule_enforcement_dbs[0].failure_reason, expected_failure)
+        self.assertEqual(rule_enforcement_dbs[0].trigger_instance_id, str(trigger_instance.id))
+        self.assertEqual(rule_enforcement_dbs[0].rule['id'], str(rule_4_db.id))
+
+    @mock.patch('st2reactor.rules.filter.PayloadLookup.get_value',
+                mock.Mock(side_effect=Exception('exception in get_value')))
+    def test_rule_enforcement_is_created_on_exception_2(self):
+        # 1. Exception in payload_lookup.get_value
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(rule_enforcement_dbs, [])
+
+        self._setup_sample_trigger('st2.test.trigger4')
+        rule_4_db = self._setup_sample_rule(RULE_4)
+        rules = [rule_4_db]
+        trigger_instance = container_utils.create_trigger_instance(
+            'dummy_pack_1.st2.test.trigger4',
+            {'k1': 't2_p_v', 'k2': 'v2'},
+            date_utils.get_datetime_utc_now()
+        )
+        trigger = get_trigger_db_by_ref(trigger_instance.trigger)
+
+        rules_matcher = RulesMatcher(trigger_instance, trigger, rules)
+        matching_rules = rules_matcher.get_matching_rules()
+        self.assertEqual(matching_rules, [])
+        self.assertEqual(len(matching_rules), 0)
+
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(len(rule_enforcement_dbs), 1)
+
+        expected_failure = ('Failed to match rule "yoyohoneysingh.st2.test.rule4" against trigger '
+                            'instance "%s": Failed transforming criteria key trigger.k1' % (str(trigger_instance.id)))
+        self.assertEqual(rule_enforcement_dbs[0].failure_reason, expected_failure)
+        self.assertEqual(rule_enforcement_dbs[0].trigger_instance_id, str(trigger_instance.id))
+        self.assertEqual(rule_enforcement_dbs[0].rule['id'], str(rule_4_db.id))
+
+    @mock.patch('st2common.operators.get_operator',
+                mock.Mock(return_value=mock.Mock(side_effect=Exception('exception in equals'))))
+    def test_rule_enforcement_is_created_on_exception_3(self):
+        # 1. Exception in payload_lookup.get_value
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(rule_enforcement_dbs, [])
+
+        self._setup_sample_trigger('st2.test.trigger4')
+        rule_4_db = self._setup_sample_rule(RULE_4)
+        rules = [rule_4_db]
+        trigger_instance = container_utils.create_trigger_instance(
+            'dummy_pack_1.st2.test.trigger4',
+            {'k1': 't2_p_v', 'k2': 'v2'},
+            date_utils.get_datetime_utc_now()
+        )
+        trigger = get_trigger_db_by_ref(trigger_instance.trigger)
+
+        rules_matcher = RulesMatcher(trigger_instance, trigger, rules)
+        matching_rules = rules_matcher.get_matching_rules()
+        self.assertEqual(matching_rules, [])
+        self.assertEqual(len(matching_rules), 0)
+
+        rule_enforcement_dbs = list(RuleEnforcement.get_all())
+        self.assertEqual(len(rule_enforcement_dbs), 1)
+
+        expected_failure = ('Failed to match rule "yoyohoneysingh.st2.test.rule4" against trigger '
+                            'instance "%s": There might be a problem with the criteria in rule '
+                            'yoyohoneysingh.st2.test.rule4.' % (str(trigger_instance.id)))
+        self.assertEqual(rule_enforcement_dbs[0].failure_reason, expected_failure)
+        self.assertEqual(rule_enforcement_dbs[0].trigger_instance_id, str(trigger_instance.id))
+        self.assertEqual(rule_enforcement_dbs[0].rule['id'], str(rule_4_db.id))
 
     def _setup_sample_trigger(self, name):
         trigtype = TriggerTypeDB(name=name, pack='dummy_pack_1', payload_schema={},
@@ -70,96 +281,11 @@ class RuleMatcherTest(DbTestCase):
                             parameters={})
         Trigger.add_or_update(created)
 
-    def _get_sample_rules(self):
-        if self.rules:
-            # Make sure rules are created only once
-            return self.rules
-
-        RULE_1 = {
-            'enabled': True,
-            'name': 'st2.test.rule1',
-            'pack': 'yoyohoneysingh',
-            'trigger': {
-                'type': 'dummy_pack_1.st2.test.trigger1'
-            },
-            'criteria': {
-                'k1': {                     # Missing prefix 'trigger'. This rule won't match.
-                    'pattern': 't1_p_v',
-                    'type': 'equals'
-                }
-            },
-            'action': {
-                'ref': 'sixpack.st2.test.action',
-                'parameters': {
-                    'ip2': '{{rule.k1}}',
-                    'ip1': '{{trigger.t1_p}}'
-                }
-            },
-            'id': '23',
-            'description': ''
-        }
-        rule_api = RuleAPI(**RULE_1)
+    def _setup_sample_rule(self, rule):
+        rule_api = RuleAPI(**rule)
         rule_db = RuleAPI.to_model(rule_api)
         rule_db = Rule.add_or_update(rule_db)
-        self.rules.append(rule_db)
-
-        RULE_2 = {                      # Rule should match.
-            'enabled': True,
-            'name': 'st2.test.rule2',
-            'pack': 'yoyohoneysingh',
-            'trigger': {
-                'type': 'dummy_pack_1.st2.test.trigger1'
-            },
-            'criteria': {
-                'trigger.k1': {
-                    'pattern': 't1_p_v',
-                    'type': 'equals'
-                }
-            },
-            'action': {
-                'ref': 'sixpack.st2.test.action',
-                'parameters': {
-                    'ip2': '{{rule.k1}}',
-                    'ip1': '{{trigger.t1_p}}'
-                }
-            },
-            'id': '23',
-            'description': ''
-        }
-        rule_api = RuleAPI(**RULE_2)
-        rule_db = RuleAPI.to_model(rule_api)
-        rule_db = Rule.add_or_update(rule_db)
-        self.rules.append(rule_db)
-
-        RULE_3 = {
-            'enabled': False,         # Disabled rule shouldn't match.
-            'name': 'st2.test.rule3',
-            'pack': 'yoyohoneysingh',
-            'trigger': {
-                'type': 'dummy_pack_1.st2.test.trigger1'
-            },
-            'criteria': {
-                'trigger.k1': {
-                    'pattern': 't1_p_v',
-                    'type': 'equals'
-                }
-            },
-            'action': {
-                'ref': 'sixpack.st2.test.action',
-                'parameters': {
-                    'ip2': '{{rule.k1}}',
-                    'ip1': '{{trigger.t1_p}}'
-                }
-            },
-            'id': '23',
-            'description': ''
-        }
-        rule_api = RuleAPI(**RULE_3)
-        rule_db = RuleAPI.to_model(rule_api)
-        rule_db = Rule.add_or_update(rule_db)
-        self.rules.append(rule_db)
-
-        return self.rules
+        return rule_db
 
 
 PACK = 'backstop'
@@ -172,12 +298,12 @@ FIXTURES_RULES = {
 }
 
 
-class BackstopRuleMatcherTest(DbTestCase):
+class BackstopRuleMatcherTestCase(DbTestCase):
     models = None
 
     @classmethod
     def setUpClass(cls):
-        super(BackstopRuleMatcherTest, cls).setUpClass()
+        super(BackstopRuleMatcherTestCase, cls).setUpClass()
         fixturesloader = FixturesLoader()
         # Create TriggerTypes before creation of Rule to avoid failure. Rule requires the
         # Trigger and therefore TriggerType to be created prior to rule creation.
