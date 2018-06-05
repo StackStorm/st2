9c2a075a7d40c405bb6b1fcfef1b8245f1e1bb6a
Tomaz Muraus <tomaz@tomaz.me>
2018-06-05 18:50:30 +0200

    Add new implementation of symetric_encrypt and symetric_decrypt functions which utilize crypto primitives from cryptography library.
    We are moving from keyczar to cryptography, because keyczar doesn't
    support Python 3, but cryptography does.
    
    NOTE: Those functions utilize cryptography library primitives, but they
    follow the same approach as keyczar Encrypt() and Decrypt() methods so
    they are fully compatible with keyczar key files and keyczar encrypted /
    decrypted data.

--- st2common/st2common/util/crypto.py
+++ st2common/st2common/util/crypto.py
@@ -13,40 +13,112 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Module for handling symmetric encryption and decryption of short values (mostly used for encrypted
+datastore values aka secrets).
+
+Note: In the past this module used and relied on keyczar, but since keyczar doesn't support Python
+3, we moved to cryptography.
+
+symmetric_encrypt and symmetric_decrypt functions except values as returned by the AESKey.Encrypt
+and AESKey.Decrypt methods in keyczar. Those functions follow the same approach as keyczar methods,
+but they use and rely on primitives and methods from cryptography library.
+
+This was done to make the keyczar -> cryptography migration fully backward compatible.
+
+Eventually, we should  move to Fernet (https://cryptography.io/en/latest/fernet/) recipe for
+symmetric encryption / decryption, because it offers more robustness and safer defaults (SHA256
+instead of SHA1, etc.).
+"""
+
 from __future__ import absolute_import
+
+import os
+import json
 import binascii
+import base64
+
+from hashlib import sha1
+
+import six
+
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers import algorithms
+from cryptography.hazmat.primitives.ciphers import modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hmac
+from cryptography.hazmat.backends import default_backend
 
 __all__ = [
     'read_crypto_key',
+
     'symmetric_encrypt',
-    'symmetric_decrypt'
+    'symmetric_decrypt',
+
+    'cryptography_symmetric_encrypt',
+    'cryptography_symmetric_decrypt',
+
+    # NOTE: Keyczar functions are here for testing reasons - they are only used by tests
+    'keyczar_symmetric_encrypt',
+    'keyczar_symmetric_decrypt'
 ]
 
+# Keyczar related constants
+KEYCZAR_HEADER_SIZE = 5
+KEYCZAR_AES_BLOCK_SIZE = 16
+KEYCZAR_HLEN = sha1().digest_size
 
-def read_crypto_key(key_path, key_type=None):
+
+class AESKey(object):
     """
-    Return the crypto key given a path to key file and the key type.
+    Class representing AES key object.
+    """
 
-    :param key_path: Absolute path to file containing crypto key.
+    aes_key_string = None
+    hmac_key_string = None
+    hmac_key_size = None
+    mode = None
+    size = None
+
+    def __init__(self, aes_key_string, hmac_key_string, hmac_key_size, mode='CBC', size=128):
+        self.aes_key_string = aes_key_string
+        self.hmac_key_string = hmac_key_string
+        self.hmac_key_size = hmac_key_size
+        self.mode = mode
+        self.size = size
+
+        self.hmac_key_bytes = Base64WSDecode(self.hmac_key_string)
+        self.aes_key_bytes = Base64WSDecode(self.aes_key_string)
+
+    def __repr__(self):
+        return ('<AESKey hmac_key_size=%s,mode=%s,size=%s>' % (self.hmac_key_size, self.mode,
+                                                               self.size))
+
+
+def read_crypto_key(key_path):
+    """
+    Return the crypto key given a path to key file.
+
+    :param key_path: Absolute path to file containing crypto key in Keyczar JSON format.
     :type key_path: ``str``
 
-    :param key_type: Type of crypto key.
-    :type key_type: :class:`keyczar.keys.KeyType`
-
-    :rtype: ``str``
+    :rtype: ``dict``
     """
+    with open(key_path, 'r') as fp:
+        content = fp.read()
 
-    # Late import to avoid very expensive in-direct import (~1 second) when this function
-    # is not called / used
-    from keyczar.keys import AesKey
-    key_type = key_type or AesKey
+    content = json.loads(content)
 
-    with open(key_path) as key_file:
-        key = key_type.Read(key_file.read())
-        return key
+    aes_key = AESKey(aes_key_string=content['aesKeyString'],
+                     hmac_key_string=content['hmacKey']['hmacKeyString'],
+                     hmac_key_size=content['hmacKey']['size'],
+                     mode=content['mode'].upper(),
+                     size=content['size'])
+
+    return aes_key
 
 
-def symmetric_encrypt(encrypt_key, plaintext):
+def keyczar_symmetric_encrypt(encrypt_key, plaintext):
     """
     Encrypt the given message using the encrypt_key. Returns a UTF-8 str
     ready to be stored in database. Note that we convert the hex notation
@@ -57,28 +129,214 @@ def symmetric_encrypt(encrypt_key, plaintext):
     'Initialization Vector' per run and the IV is part of the output.
 
     :param encrypt_key: Symmetric AES key to use for encryption.
-    :type encrypt_key: :class:`keyczar.keys.AesKey`
+    :type encrypt_key: :class:`keyczar.keys.AESKey`
 
     :param plaintext: Plaintext / message to be encrypted.
     :type plaintext: ``str``
 
     :rtype: ``str``
     """
+    from keyczar.keys import AesKey as KeyczarAesKey
+    from keyczar.keys import HmacKey as KeyczarHmacKey
+    from keyczar.keyinfo import GetMode
+
+    encrypt_key = KeyczarAesKey(encrypt_key.aes_key_string,
+                                KeyczarHmacKey(encrypt_key.hmac_key_string,
+                                               encrypt_key.hmac_key_size),
+                                encrypt_key.size,
+                                GetMode(encrypt_key.mode))
+
     return binascii.hexlify(encrypt_key.Encrypt(plaintext)).upper()
 
 
-def symmetric_decrypt(decrypt_key, ciphertext):
+def keyczar_symmetric_decrypt(decrypt_key, ciphertext):
     """
     Decrypt the given crypto text into plain text. Returns the original
     string input. Note that we first convert the string to hex notation
     and then decrypt. This is reverse of the encrypt operation.
 
     :param decrypt_key: Symmetric AES key to use for decryption.
-    :type decrypt_key: :class:`keyczar.keys.AesKey`
+    :type decrypt_key: :class:`keyczar.keys.AESKey`
 
     :param crypto: Crypto text to be decrypted.
     :type crypto: ``str``
 
     :rtype: ``str``
     """
+    from keyczar.keys import AesKey as KeyczarAesKey
+    from keyczar.keys import HmacKey as KeyczarHmacKey
+    from keyczar.keyinfo import GetMode
+
+    decrypt_key = KeyczarAesKey(decrypt_key.aes_key_string,
+                                KeyczarHmacKey(decrypt_key.hmac_key_string,
+                                               decrypt_key.hmac_key_size),
+                                decrypt_key.size,
+                                GetMode(decrypt_key.mode))
+
     return decrypt_key.Decrypt(binascii.unhexlify(ciphertext))
+
+
+def cryptography_symmetric_encrypt(encrypt_key, plaintext):
+    """
+    Encrypt the provided plaintext using AES encryption.
+
+    Note 1: This function returns format fully compatible with Keyczar.Encrypt method.
+
+    Note 2: This function is loosely based on keyczar AESKey.Encrypt (Apache 2.0 license)
+
+    """
+    assert isinstance(encrypt_key, AESKey), 'encrypt_key needs to be AESKey class instance'
+
+    aes_key_bytes = encrypt_key.aes_key_bytes
+    hmac_key_bytes = encrypt_key.hmac_key_bytes
+
+    assert isinstance(aes_key_bytes, six.binary_type)
+    assert isinstance(hmac_key_bytes, six.binary_type)
+
+    # Pad data
+    data = pkcs5_pad(plaintext)
+
+    # Generate IV
+    iv_bytes = os.urandom(KEYCZAR_AES_BLOCK_SIZE)
+
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(aes_key_bytes), modes.CBC(iv_bytes), backend=backend)
+    encryptor = cipher.encryptor()
+
+    # NOTE: We don't care about actual Keyczar header value, we only care about the length (5
+    # bytes) so we simply add 5 0's
+    header = '00000'
+
+    ciphertext_bytes = encryptor.update(data) + encryptor.finalize()
+    msg_bytes = header + iv_bytes + ciphertext_bytes
+
+    # Generate HMAC signature for message (header + IV + ciphertext)
+    h = hmac.HMAC(hmac_key_bytes, hashes.SHA1(), backend=backend)
+    h.update(msg_bytes)
+    sig_bytes = h.finalize()
+
+    result = msg_bytes + sig_bytes
+
+    # Convert resulting byte string to hex notation
+    result = binascii.hexlify(result).upper()
+
+    return result
+
+
+def cryptography_symmetric_decrypt(decrypt_key, ciphertext):
+    """
+    Decrypt the provided ciphertext.
+
+    NOTE 1: This function assumes ciphertext has been encrypted using symmetric AES crypto from
+    keyczar library. Underneath it uses crypto primitives from cryptography library which is Python
+    3 compatible.
+
+    Note 2: This function is loosely based on keyczar AESKey.Decrypt (Apache 2.0 license)
+    """
+    assert isinstance(decrypt_key, AESKey), 'decrypt_key needs to be AESKey class instance'
+
+    aes_key_bytes = decrypt_key.aes_key_bytes
+    hmac_key_bytes = decrypt_key.hmac_key_bytes
+
+    assert isinstance(aes_key_bytes, six.binary_type)
+    assert isinstance(hmac_key_bytes, six.binary_type)
+
+    ciphertext = binascii.unhexlify(ciphertext)
+
+    data_bytes = ciphertext[KEYCZAR_HEADER_SIZE:]  # remove header
+
+    # Verify ciphertext contains IV + HMAC signature
+    if len(data_bytes) < KEYCZAR_AES_BLOCK_SIZE + KEYCZAR_HLEN:
+        raise Exception('Invalid ciphertext (too short)')
+
+    iv_bytes = data_bytes[:KEYCZAR_AES_BLOCK_SIZE]  # first block is IV
+    ciphertext_bytes = data_bytes[KEYCZAR_AES_BLOCK_SIZE:-KEYCZAR_HLEN]  # strip IV and signature
+    signature_bytes = data_bytes[-KEYCZAR_HLEN:]  # last 20 bytes are signature
+
+    # Verify HMAC signature
+    backend = default_backend()
+    h = hmac.HMAC(hmac_key_bytes, hashes.SHA1(), backend=backend)
+    h.update(ciphertext[:-KEYCZAR_HLEN])
+    h.verify(signature_bytes)
+
+    # Decrypt ciphertext
+    cipher = Cipher(algorithms.AES(aes_key_bytes), modes.CBC(iv_bytes), backend=backend)
+
+    decryptor = cipher.decryptor()
+    decrypted = decryptor.update(ciphertext_bytes) + decryptor.finalize()
+
+    # Unpad
+    decrypted = pkcs5_unpad(decrypted)
+    return decrypted
+
+
+def pkcs5_pad(data):
+    """
+    Pad data using PKCS5
+    """
+    pad = KEYCZAR_AES_BLOCK_SIZE - len(data) % KEYCZAR_AES_BLOCK_SIZE
+    data = data + pad * chr(pad)
+    return data
+
+
+def pkcs5_unpad(data):
+    """
+    Unpad data padded using PKCS5.
+    """
+    pad = ord(data[-1])
+    data = data[:-pad]
+    return data
+
+
+def Base64WSEncode(s):
+    """
+    Return Base64 web safe encoding of s. Suppress padding characters (=).
+
+    Uses URL-safe alphabet: - replaces +, _ replaces /. Will convert s of type
+    unicode to string type first.
+
+    @param s: string to encode as Base64
+    @type s: string
+
+    @return: Base64 representation of s.
+    @rtype: string
+
+    NOTE: Taken from keyczar (Apache 2.0 license)
+    """
+    return base64.urlsafe_b64encode(str(s)).replace("=", "")
+
+
+def Base64WSDecode(s):
+    """
+    Return decoded version of given Base64 string. Ignore whitespace.
+
+    Uses URL-safe alphabet: - replaces +, _ replaces /. Will convert s of type
+    unicode to string type first.
+
+    @param s: Base64 string to decode
+    @type s: string
+
+    @return: original string that was encoded as Base64
+    @rtype: string
+
+    @raise Base64DecodingError: If length of string (ignoring whitespace) is one
+      more than a multiple of four.
+
+    NOTE: Taken from keyczar (Apache 2.0 license)
+    """
+    s = ''.join(s.splitlines())
+    s = str(s.replace(" ", ""))  # kill whitespace, make string (not unicode)
+    d = len(s) % 4
+
+    if d == 1:
+        raise ValueError('Base64 decoding errors')
+    elif d == 2:
+        s += "=="
+    elif d == 3:
+        s += "="
+
+    try:
+        return base64.urlsafe_b64decode(s)
+    except TypeError as e:
+        # Decoding raises TypeError if s contains invalid characters.
+        raise ValueError('Base64 decoding error: %s' % (str(e)))
