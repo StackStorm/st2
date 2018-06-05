719569a7da5ece542ebdc8753d403974bf8eb9fc
Winson Chan <m4d.coder@gmail.com>
2018-06-05 20:21:25 +0000

    Add and update integration tests
    Minor adds and updates to the set of integration tests.

--- st2tests/integration/orchestra/test_wiring_data_flow.py
+++ st2tests/integration/orchestra/test_wiring_data_flow.py
@@ -17,6 +17,9 @@
 
 from __future__ import absolute_import
 
+import random
+import string
+
 from integration.orchestra import base
 
 from st2common.constants import action as ac_const
@@ -24,11 +27,11 @@ from st2common.constants import action as ac_const
 
 class WiringTest(base.TestWorkflowExecution):
 
-    def test_sequential(self):
-        wf_name = 'examples.orchestra-sequential'
-        wf_input = {'name': 'Thanos'}
+    def test_data_flow(self):
+        wf_name = 'examples.orchestra-data-flow'
+        wf_input = {'a1': 'fee fi fo fum'}
 
-        expected_output = {'greeting': 'Thanos, All your base are belong to us!'}
+        expected_output = {'a5': wf_input['a1'], 'b5': wf_input['a1']}
         expected_result = {'output': expected_output}
 
         ex = self._execute_workflow(wf_name, wf_input)
@@ -37,21 +40,35 @@ class WiringTest(base.TestWorkflowExecution):
         self.assertEqual(ex.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
         self.assertDictEqual(ex.result, expected_result)
 
-    def test_join(self):
-        wf_name = 'examples.orchestra-join'
+    def test_data_flow_unicode(self):
+        wf_name = 'examples.orchestra-data-flow'
+        wf_input = {'a1': '床前明月光 疑是地上霜 舉頭望明月 低頭思故鄉'}
 
         expected_output = {
-            'messages': [
-                'Fee fi fo fum',
-                'I smell the blood of an English man',
-                'Be alive, or be he dead',
-                'I\'ll grind his bones to make my bread'
-            ]
+            'a5': wf_input['a1'].decode('utf-8'),
+            'b5': wf_input['a1'].decode('utf-8')
         }
 
         expected_result = {'output': expected_output}
 
-        ex = self._execute_workflow(wf_name)
+        ex = self._execute_workflow(wf_name, wf_input)
+        ex = self._wait_for_completion(ex)
+
+        self.assertEqual(ex.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+        self.assertDictEqual(ex.result, expected_result)
+
+    def test_data_flow_big_data_size(self):
+        wf_name = 'examples.orchestra-data-flow'
+
+        data_length = 100000
+        data = ''.join(random.choice(string.ascii_lowercase) for _ in range(data_length))
+
+        wf_input = {'a1': data}
+
+        expected_output = {'a5': wf_input['a1'], 'b5': wf_input['a1']}
+        expected_result = {'output': expected_output}
+
+        ex = self._execute_workflow(wf_name, wf_input)
         ex = self._wait_for_completion(ex)
 
         self.assertEqual(ex.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
