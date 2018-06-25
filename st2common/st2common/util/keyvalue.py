d5b8e8478532a3eda81c3783a4348f27c3a34ba4
bigmstone <matthew99@gmail.com>
2018-06-25 13:09:41 -0500

    Adding ST2KV function and required orchestra_runner modifications.

--- st2common/st2common/util/keyvalue.py
+++ st2common/st2common/util/keyvalue.py
@@ -15,12 +15,43 @@
 
 
 from __future__ import absolute_import
+import six
+import logging
+
+from oslo_config import cfg
+
 from st2common.constants.keyvalue import ALL_SCOPE, DATASTORE_PARENT_SCOPE
 from st2common.constants.keyvalue import DATASTORE_SCOPE_SEPARATOR
+from st2common.rbac import utils as rbac_utils
+from st2common.persistence.keyvalue import KeyValuePair
+from st2common.services.config import deserialize_key_value
+from st2common.constants.keyvalue import (FULL_SYSTEM_SCOPE, FULL_USER_SCOPE, USER_SCOPE,
+                                          ALLOWED_SCOPES)
+from st2common.models.db.auth import UserDB
+from st2common.exceptions.rbac import AccessDeniedError
 
 __all__ = [
     'get_datastore_full_scope'
 ]
+
+LOG = logging.getLogger(__name__)
+
+
+def _validate_scope(scope):
+    if scope not in ALLOWED_SCOPES:
+        msg = 'Scope %s is not in allowed scopes list: %s.' % (scope, ALLOWED_SCOPES)
+        raise ValueError(msg)
+
+
+def _validate_decrypt_query_parameter(decrypt, scope, is_admin, user):
+    """
+    Validate that the provider user is either admin or requesting to decrypt value for
+    themselves.
+    """
+    is_user_scope = (scope == USER_SCOPE or scope == FULL_USER_SCOPE)
+    if decrypt and (not is_user_scope and not is_admin):
+        msg = 'Decrypt option requires administrator access'
+        raise AccessDeniedError(message=msg, user_db=user)
 
 
 def get_datastore_full_scope(scope):
@@ -31,3 +62,48 @@ def get_datastore_full_scope(scope):
         return scope
 
     return '%s%s%s' % (DATASTORE_PARENT_SCOPE, DATASTORE_SCOPE_SEPARATOR, scope)
+
+
+def _derive_scope_and_key(key, user, scope=None):
+    if scope is not None:
+        return scope, key
+    elif key.startswith('system.'):
+        return FULL_SYSTEM_SCOPE, key[key.index('.') + 1:]
+
+    return FULL_USER_SCOPE, '%s:%s' % (user, key)
+
+
+def get_key(key=None, user=None, scope=None, decrypt=False):
+    """Retrieve key from KVP store
+    """
+    if not isinstance(key, six.string_types):
+        raise TypeError('Given key is not typeof string.')
+    if not isinstance(decrypt, bool):
+        raise TypeError('Decrypt parameter is not typeof bool.')
+
+    if not user:
+        user = UserDB(cfg.CONF.system_user.user)
+
+    scope, key_id = _derive_scope_and_key(key, user, scope)
+
+    scope = get_datastore_full_scope(scope)
+
+    LOG.debug('get_key scope: %s', scope)
+
+    _validate_scope(scope=scope)
+
+    is_admin = rbac_utils.user_is_admin(user_db=user)
+
+    # User needs to be either admin or requesting item for itself
+    _validate_decrypt_query_parameter(decrypt=decrypt, scope=scope, is_admin=is_admin,
+                                      user=user)
+
+    value = KeyValuePair.get_by_scope_and_name(scope, key_id)
+
+    if value:
+        return deserialize_key_value(
+            value.value,
+            decrypt
+        )
+
+    return None
