1065abb53a916cc30c45d29eb00f75a782f39d41
Tomaz Muraus <tomaz@tomaz.me>
2018-07-20 16:40:59 +0200

    Add support for installing multiple packs at once to "st2-pack-install" CLI script.

--- st2common/st2common/cmd/install_pack.py
+++ st2common/st2common/cmd/install_pack.py
@@ -42,8 +42,8 @@ def _do_register_cli_opts(opts, ignore_errors=False):
 
 def _register_cli_opts():
     cli_opts = [
-        cfg.StrOpt('pack', default=None, required=True,
-                   help='Name of the pack to install.'),
+        cfg.MultiStrOpt('pack', default=None, required=True, positional=True,
+                        help='Name of the pack to install.'),
         cfg.BoolOpt('verify-ssl', default=True,
                    help=('Verify SSL certificate of the Git repo from which the pack is '
                          'downloaded.')),
@@ -94,29 +94,32 @@ def main(argv):
     common_setup(config=config, setup_db=False, register_mq_exchanges=False,
                  register_internal_trigger_types=False)
 
-    pack = cfg.CONF.pack
+    packs = cfg.CONF.pack
     verify_ssl = cfg.CONF.verify_ssl
     force = cfg.CONF.force
 
     proxy_config = get_and_set_proxy_config()
 
-    # 1. Download the pac
-    LOG.info('Installing pack "%s"' % (pack))
-    result = download_pack(pack=pack, verify_ssl=verify_ssl, force=force,
-                           proxy_config=proxy_config, force_permissions=True)
+    for pack in packs:
+        # 1. Download the pack
+        LOG.info('Installing pack "%s"' % (pack))
+        result = download_pack(pack=pack, verify_ssl=verify_ssl, force=force,
+                               proxy_config=proxy_config, force_permissions=True)
 
-    success = result[2][0]
+        success = result[2][0]
 
-    if success:
-        LOG.info('Successfuly installed pack "%s"' % (pack))
-    else:
-        error = result[2][1]
-        LOG.error('Failed to install pack "%s": %s' % (pack, error))
-        sys.exit(2)
+        if success:
+            LOG.info('Successfuly installed pack "%s"' % (pack))
+        else:
+            error = result[2][1]
+            LOG.error('Failed to install pack "%s": %s' % (pack, error))
+            sys.exit(2)
 
-    # 2. Setup pack virtual environment
-    LOG.info('Setting up virtualenv for pack "%s"' % (pack))
-    setup_pack_virtualenv(pack_name=pack, update=False, logger=LOG,
-                          proxy_config=proxy_config, use_python3=False,
-                          no_download=True)
-    LOG.info('Successfuly set up virtualenv for pack "%s"' % (pack))
+        # 2. Setup pack virtual environment
+        LOG.info('Setting up virtualenv for pack "%s"' % (pack))
+        setup_pack_virtualenv(pack_name=pack, update=False, logger=LOG,
+                              proxy_config=proxy_config, use_python3=False,
+                              no_download=True)
+        LOG.info('Successfuly set up virtualenv for pack "%s"' % (pack))
+
+    return 0
