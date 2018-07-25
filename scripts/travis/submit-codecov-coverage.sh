987064bf68df391c1b2af5c5079a42af298692d1
Tomaz Muraus <tomaz@tomaz.me>
2018-07-25 19:00:37 +0200

    Update scripts so the build is marked as failed if coverage submissions fails.
    Due to the nature of after_success task behavior, that's only possible
    by moving this step into "script" task.
    
    See the following links for details:
    
    * https://docs.travis-ci.com/user/customizing-the-build/#breaking-the-build
    * https://github.com/travis-ci/travis-ci/issues/758

--- scripts/travis/submit-codecov-coverage.sh
+++ scripts/travis/submit-codecov-coverage.sh
@@ -14,12 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+if [ ${TRAVIS_TEST_RESULT} -eq 0 ]; then
+    # 1. Install codecov dependencies
+    # NOTE: We need eventlet installed so coverage can be correctly combined. This is needed because we are covering code which utilizes eventlet.
+    # Without eventlet being available to the coverage command it will fail with "Couldn't trace with concurrency=eventlet, the module isn't installed."
+    pip install eventlet
+    pip install -e "git+https://github.com/StackStorm/codecov-python.git@better_error_output#egg=codecov"
 
-# 1. Install codecov dependencies
-# NOTE: We need eventlet installed so coverage can be correctly combined. This is needed because we are covering code which utilizes eventlet.
-# Without eventlet being available to the coverage command it will fail with "Couldn't trace with concurrency=eventlet, the module isn't installed."
-pip install eventlet
-pip install -e "git+https://github.com/StackStorm/codecov-python.git@better_error_output#egg=codecov"
-
-# 2. Combine coverage report and submit coverage report to codecovs.io
-codecov --required
+    # 2. Combine coverage report and submit coverage report to codecovs.io
+    codecov --required
+    exit $?
+else
+    echo "Build has failed, not submitting coverage"
+    exit 0
+fi
