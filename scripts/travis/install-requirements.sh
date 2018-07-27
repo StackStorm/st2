f374f1240adc8b029002dadd1ba7eb9b49439c71
Tomaz Muraus <tomaz@tomaz.me>
2018-07-27 15:11:44 +0200

    Fix tox task name.

--- scripts/travis/install-requirements.sh
+++ scripts/travis/install-requirements.sh
@@ -6,7 +6,14 @@ if [ "${TASK}" = 'compilepy3 ci-py3-unit' ] || [ "${TASK}" = 'ci-py3-integration
     # NOTE: We create the environment and install the dependencies first. This
     # means that the subsequent tox build / test command has a stable run time
     # since it doesn't depend on dependencies being installed.
-    TOX_TASK=${TASK/compilepy3 /}
+    if [ "${TASK}" = 'compilepy3 ci-py3-unit' ]; then
+        TOX_TASK="py36-unit"
+    fi
+
+    if [ "${TASK}" = 'ci-py3-integration' ]; then
+        TOX_TASK="py36-integration"
+    fi
+
     tox -e ${TOX_TASK} --notest
 else
     make requirements
