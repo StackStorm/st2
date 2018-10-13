ac45bb0b09898be16b2172c21e5955aa1364e31d
aduca <aduca@adobe.com>
2018-10-13 11:14:24 +0300

    Mongo X509 auth support

--- st2common/st2common/persistence/cleanup.py
+++ st2common/st2common/persistence/cleanup.py
@@ -45,7 +45,7 @@ def db_cleanup():
 def db_cleanup_with_retry(db_name, db_host, db_port, username=None, password=None,
                           ssl=False, ssl_keyfile=None,
                           ssl_certfile=None, ssl_cert_reqs=None, ssl_ca_certs=None,
-                          ssl_match_hostname=True):
+                          authentication_mechanism=None, ssl_match_hostname=True):
     """
     This method is a retry version of db_cleanup.
     """
@@ -54,7 +54,7 @@ def db_cleanup_with_retry(db_name, db_host, db_port, username=None, password=Non
                               username=username, password=password,
                               ssl=ssl, ssl_keyfile=ssl_keyfile,
                               ssl_certfile=ssl_certfile, ssl_cert_reqs=ssl_cert_reqs,
-                              ssl_ca_certs=ssl_ca_certs,
+                              ssl_ca_certs=ssl_ca_certs, authentication_mechanism=authentication_mechanism,
                               ssl_match_hostname=ssl_match_hostname)
 
 
