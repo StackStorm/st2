323e069a1768a7555594c40c9ec5abd2d0d06948
Tomaz Muraus <tomaz@tomaz.me>
2018-10-17 14:41:36 +0200

    Fix API to not throw an exception if value of tags is None.

--- st2common/st2common/models/api/tag.py
+++ st2common/st2common/models/api/tag.py
@@ -25,8 +25,10 @@ class TagsHelper(object):
 
     @staticmethod
     def to_model(tags):
+        tags = tags or []
         return [TagField(name=tag.get('name', ''), value=tag.get('value', '')) for tag in tags]
 
     @staticmethod
     def from_model(tags):
+        tags = tags or []
         return [{'name': tag.name, 'value': tag.value} for tag in tags]
