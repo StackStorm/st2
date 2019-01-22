9cec88fb37ec72b11ab1691a76b617e29eb8396d
Tomaz Muraus <tomaz@tomaz.me>
2019-01-22 14:45:28 +0100

    User is already added as part of add-itest-user.sh script, no need to add it twice.

--- scripts/travis/prepare-integration.sh
+++ scripts/travis/prepare-integration.sh
@@ -6,52 +6,6 @@ if [ "$(whoami)" != 'root' ]; then
     exit 2
 fi
 
-# create and configure user
-VIRTUALENV_DIR=virtualenv
-
-create_user() {
-  echo "###########################################################################################"
-  echo "# Creating system user: stanley"
-
-  if (! id stanley 2>/dev/null); then
-    useradd stanley
-  fi
-
-  SYSTEM_HOME=$(echo ~stanley)
-
-  mkdir -p ${SYSTEM_HOME}/.ssh
-
-  if ! test -s ${SYSTEM_HOME}/.ssh/stanley_rsa; then
-    ssh-keygen -f ${SYSTEM_HOME}/.ssh/stanley_rsa -P ""
-  fi
-
-  if ! grep -s -q -f ${SYSTEM_HOME}/.ssh/stanley_rsa.pub ${SYSTEM_HOME}/.ssh/authorized_keys;
-  then
-    # Authorize key-base access
-    cat ${SYSTEM_HOME}/.ssh/stanley_rsa.pub >> ${SYSTEM_HOME}/.ssh/authorized_keys
-  fi
-
-  chmod 0600 ${SYSTEM_HOME}/.ssh/authorized_keys
-  chmod 0700 ${SYSTEM_HOME}/.ssh
-  chown -R stanley:stanley ${SYSTEM_HOME}
-
-  # Enable passwordless sudo
-  local STANLEY_SUDOERS="stanley    ALL=(ALL)       NOPASSWD: SETENV: ALL"
-  if ! grep -s -q ^"${STANLEY_SUDOERS}" /etc/sudoers.d/st2; then
-    echo "${STANLEY_SUDOERS}" >> /etc/sudoers.d/st2
-  fi
-
-  chmod 0440 /etc/sudoers.d/st2
-
-  # Disable requiretty for all users
-  sed -i -r "s/^Defaults\s+\+?requiretty/# Defaults requiretty/g" /etc/sudoers
-}
-
-create_user
-
-# install screen
-apt-get install -y screen
-
 # Activate the virtualenv created during make requirements phase
 source ./virtualenv/bin/activate
 
