af09c7326724ea901665c9d6cf821c6a7397464a
Tomaz Muraus <tomaz@tomaz.me>
2019-01-22 14:13:10 +0100

    Pass --no-dep to python setup.py develop to speed up the build and avoid duplicate dependency installation.

--- scripts/travis/install-requirements.sh
+++ scripts/travis/install-requirements.sh
@@ -3,16 +3,16 @@
 if [ "${TASK}" = 'compilepy3 ci-py3-unit' ] || [ "${TASK}" = 'ci-py3-integration' ]; then
     pip install "tox==3.5.2"
 
-	# Install runners
+    # Install runners
     . virtualenv/bin/activate
 
-	CURRENT_DIR=`pwd`
-	for RUNNER in `ls -d $CURRENT_DIR/contrib/runners/*`
-	do
-	  echo "Installing runner: $RUNNER..."
+    CURRENT_DIR=`pwd`
+    for RUNNER in `ls -d $CURRENT_DIR/contrib/runners/*`
+    do
+      echo "Installing runner: $RUNNER..."
       cd $RUNNER
-      python setup.py develop
-	done
+      python setup.py develop --no-deps
+    done
 
     # NOTE: We create the environment and install the dependencies first. This
     # means that the subsequent tox build / test command has a stable run time
