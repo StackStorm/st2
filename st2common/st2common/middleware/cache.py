391358d2be4d21bfebb68b53947239ec6bd47f6c
bigmstone <matthew99@gmail.com>
2019-03-08 16:17:03 -0600

    Adding cache control header
    For various integrations ST2API should properly tell the client how to
    handle caching. This is especially true for ST2Web and ST2Flow. This
    commit adds cache-control header and sets the header to `no-cache`. This
    way clients always go back to ST2 for their information instead of using
    cached data.

---
+++ st2common/st2common/middleware/cache.py
@@ -0,0 +1,32 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from webob.headers import ResponseHeaders
+
+
+class CacheMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        def custom_start_response(status, headers, exc_info=None):
+            headers = ResponseHeaders(headers)
+
+            headers['Cache-Control'] = 'no-cache'
+
+            return start_response(status, headers._items, exc_info)
+
+        return self.app(environ, custom_start_response)
