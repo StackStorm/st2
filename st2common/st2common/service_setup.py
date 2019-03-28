b07724a676a196e2dbdfea5e0abc867ec071e6e8
Tomaz Muraus <tomaz@tomaz.me>
2019-03-28 10:48:37 +0100

    Log a message if RBAC is configured but a non-enterprise backend is used.

--- st2common/st2common/service_setup.py
+++ st2common/st2common/service_setup.py
@@ -186,6 +186,13 @@ def setup(service, config, setup_db=True, register_mq_exchanges=True,
         register_service_in_service_registry(service=service, capabilities=capabilities,
                                              start_heart=True)
 
+    # RBAC backend check
+    if cfg.CONF.rbac.enable and cfg.CONF.rbac.backend != 'enterprise':
+        msg = ('You have enabled RBAC, but RBAC backend is not set to "enterprise". '
+               'For RBAC to work, you need to install "bwc-enterprise" package and set '
+               '"rbac.backend" config option to "enterprise".')
+        LOG.warn(msg)
+
 
 def teardown():
     """
