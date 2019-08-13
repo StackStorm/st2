15366bb34c3c6c77acaf25da25d49b3a3a151f07
W Chan <m4d.coder@gmail.com>
2019-08-13 00:37:53 +0000

    Add unit tests for the SSO controllers
    Add unit tests to capture not implemented errors, unknown exception, and also SSO verification errors.

---
+++ st2auth/tests/unit/controllers/v1/test_sso.py
@@ -0,0 +1,109 @@
+# Copyright 2019 Extreme Networks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import st2tests.config as tests_config
+tests_config.parse_args()
+
+import json
+import mock
+import urllib
+
+from six.moves import http_client
+
+from st2auth.controllers.v1 import sso as sso_api_controller
+from st2auth.sso import noop
+from st2common.exceptions import auth as auth_exc
+from tests.base import FunctionalTest
+
+
+SSO_V1_PATH = '/v1/sso'
+SSO_CALLBACK_V1_PATH = SSO_V1_PATH + '/callback'
+MOCK_REFERER = 'https://127.0.0.1'
+MOCK_USER = 'stanley'
+
+
+class TestSingleSignOnController(FunctionalTest):
+
+    @mock.patch.object(
+        sso_api_controller.SSO_BACKEND,
+        'get_request_redirect_url',
+        mock.MagicMock(side_effect=Exception('fooobar')))
+    def test_default_backend_unknown_exception(self):
+        expected_error = {'faultstring': 'Internal Server Error'}
+        response = self.app.get(SSO_V1_PATH, expect_errors=True)
+        self.assertTrue(response.status_code, http_client.INTERNAL_SERVER_ERROR)
+        self.assertDictEqual(response.json, expected_error)
+
+    def test_default_backend_not_implemented(self):
+        expected_error = {'faultstring': noop.NOT_IMPLEMENTED_MESSAGE}
+        response = self.app.get(SSO_V1_PATH, expect_errors=True)
+        self.assertTrue(response.status_code, http_client.INTERNAL_SERVER_ERROR)
+        self.assertDictEqual(response.json, expected_error)
+
+    @mock.patch.object(
+        sso_api_controller.SSO_BACKEND,
+        'get_request_redirect_url',
+        mock.MagicMock(return_value='https://127.0.0.1'))
+    def test_idp_redirect(self):
+        response = self.app.get(SSO_V1_PATH, expect_errors=False)
+        self.assertTrue(response.status_code, http_client.TEMPORARY_REDIRECT)
+        self.assertEqual(response.location, 'https://127.0.0.1')
+
+
+class TestIdentityProviderCallbackController(FunctionalTest):
+
+    @mock.patch.object(
+        sso_api_controller.SSO_BACKEND,
+        'verify_response',
+        mock.MagicMock(side_effect=Exception('fooobar')))
+    def test_default_backend_unknown_exception(self):
+        expected_error = {'faultstring': 'Internal Server Error'}
+        response = self.app.post_json(SSO_CALLBACK_V1_PATH, {'foo': 'bar'}, expect_errors=True)
+        self.assertTrue(response.status_code, http_client.INTERNAL_SERVER_ERROR)
+        self.assertDictEqual(response.json, expected_error)
+
+    def test_default_backend_not_implemented(self):
+        expected_error = {'faultstring': noop.NOT_IMPLEMENTED_MESSAGE}
+        response = self.app.post_json(SSO_CALLBACK_V1_PATH, {'foo': 'bar'}, expect_errors=True)
+        self.assertTrue(response.status_code, http_client.INTERNAL_SERVER_ERROR)
+        self.assertDictEqual(response.json, expected_error)
+
+    @mock.patch.object(
+        sso_api_controller.SSO_BACKEND,
+        'verify_response',
+        mock.MagicMock(return_value={'referer': MOCK_REFERER, 'username': MOCK_USER}))
+    def test_idp_callback(self):
+        expected_body = sso_api_controller.CALLBACK_SUCCESS_RESPONSE_BODY % MOCK_REFERER
+        response = self.app.post_json(SSO_CALLBACK_V1_PATH, {'foo': 'bar'}, expect_errors=False)
+        self.assertTrue(response.status_code, http_client.OK)
+        self.assertEqual(expected_body, response.body)
+
+        set_cookies_list = [h for h in response.headerlist if h[0] == 'Set-Cookie']
+        self.assertEqual(len(set_cookies_list), 1)
+        self.assertIn('st2-auth-token', set_cookies_list[0][1])
+
+        cookie = urllib.unquote(set_cookies_list[0][1]).split('=')
+        st2_auth_token = json.loads(cookie[1].split(';')[0])
+        self.assertIn('token', st2_auth_token)
+        self.assertEqual(st2_auth_token['user'], MOCK_USER)
+
+    @mock.patch.object(
+        sso_api_controller.SSO_BACKEND,
+        'verify_response',
+        mock.MagicMock(side_effect=auth_exc.SSOVerificationError('Verification Failed')))
+    def test_idp_callback_verification_failed(self):
+        expected_error = {'faultstring': 'Verification Failed'}
+        response = self.app.post_json(SSO_CALLBACK_V1_PATH, {'foo': 'bar'}, expect_errors=True)
+        self.assertTrue(response.status_code, http_client.UNAUTHORIZED)
+        self.assertDictEqual(response.json, expected_error)
