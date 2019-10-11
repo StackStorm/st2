7ad78a9b2a576ca7a3c4cf79917ecbe0a6354db2
Felipe Oliveira <felipe_nicodemos@hotmail.com>
2019-10-11 10:08:34 -0300

    Add import RuleTypeDB

--- st2common/tests/unit/test_db.py
+++ st2common/tests/unit/test_db.py
@@ -34,7 +34,7 @@ from st2common.exceptions.db import StackStormDBObjectNotFoundError
 from st2common.models.db.trigger import TriggerTypeDB, TriggerDB, TriggerInstanceDB
 from st2common.models.db.rule import RuleDB, ActionExecutionSpecDB
 from st2common.persistence.cleanup import db_cleanup
-from st2common.persistence.rule import Rule
+from st2common.persistence.rule import Rule, RuleTypeDB
 from st2common.persistence.trigger import TriggerType, Trigger, TriggerInstance
 from st2tests import DbTestCase
 
@@ -447,7 +447,7 @@ class ReactorModelTestCase(DbTestCase):
         runnertype = ActionModelTestCase._create_save_runnertype()
         action = ActionModelTestCase._create_save_action(runnertype)
         saved = ReactorModelTestCase._create_save_rule(trigger, action, False)
-        masked = RuleDB._mask_secrets(saved.action)
+        masked = RuleTypeDB._mask_secrets(saved.action)
         for value in masked['parameters']['p4'].values():
             self.assertEqual(value, MASKED_ATTRIBUTE_VALUE)
         ReactorModelTestCase._delete([saved, trigger, action, runnertype, triggertype])
