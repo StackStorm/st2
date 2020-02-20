12d9639ef1d47328283bc78efa91c1bbd4c8d7b5
Jacob Floyd <cognifloyd@gmail.com>
2020-02-19 19:47:51 -0600

    add ssl_certs perms workaround for itests

--- scripts/travis/permissions-workaround.sh
+++ scripts/travis/permissions-workaround.sh
@@ -10,3 +10,5 @@ fi
 # this can't be the travis user because 'stanley' is the hardcoded user in the tests
 # o=other; X=only set execute bit if user execute bit is set (eg on dirs)
 chmod -R o+rwX ${TRAVIS_BUILD_DIR}/StackStorm/st2/st2tests/st2tests/fixtures/packs
+# rabbitmq user needs access to these certs
+chmod -R o+rwX ${TRAVIS_BUILD_DIR}/StackStorm/st2/st2tests/st2tests/fixtures/ssl_certs
