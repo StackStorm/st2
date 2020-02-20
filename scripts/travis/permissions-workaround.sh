9ece70e077acba9ab3c0306bb11ecf5bb92f7b46
Jacob Floyd <cognifloyd@gmail.com>
2020-02-19 19:57:20 -0600

    be less generous in permissions workaround

--- scripts/travis/permissions-workaround.sh
+++ scripts/travis/permissions-workaround.sh
@@ -9,6 +9,6 @@ fi
 # stanley needs to work with some fixtures during the tests
 # this can't be the travis user because 'stanley' is the hardcoded user in the tests
 # o=other; X=only set execute bit if user execute bit is set (eg on dirs)
-chmod -R o+rwX ${TRAVIS_BUILD_DIR}/st2tests/st2tests/fixtures/packs
+chmod -R o+rX ${TRAVIS_BUILD_DIR}/st2tests/st2tests/fixtures/packs
 # rabbitmq user needs access to these certs
-chmod -R o+rwX ${TRAVIS_BUILD_DIR}/st2tests/st2tests/fixtures/ssl_certs
+chmod -R o+rX ${TRAVIS_BUILD_DIR}/st2tests/st2tests/fixtures/ssl_certs
