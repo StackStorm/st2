62fad9984c772a9d9269478c9742ced5c4d19bb8
saucetray <justintimetotheparty@gmail.com>
2020-07-01 11:52:19 -0400

    Returned the original message in the log.

--- st2common/st2common/router.py
+++ st2common/st2common/router.py
@@ -515,7 +515,8 @@ class Router(object):
         try:
             resp = func(**kw)
         except StackStormDBObjectNotFoundError as e:
-            LOG.error(e)
+            LOG.warning('Failed to call controller function "%s" for operation "%s": %s' %
+                       (func.__name__, endpoint['operationId'], six.text_type(e))))
             raise e
         except Exception as e:
             LOG.exception('Failed to call controller function "%s" for operation "%s": %s' %
