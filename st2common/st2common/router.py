a11bf5def15f72f7a98f0ef0a98d072f105ecc76
saucetray <justintimetotheparty@gmail.com>
2020-08-05 12:11:32 -0400

    Removed extra line of log.

--- st2common/st2common/router.py
+++ st2common/st2common/router.py
@@ -516,7 +516,7 @@ class Router(object):
             resp = func(**kw)
         except DataStoreKeyNotFoundError as e:
             LOG.warning('Failed to call controller function "%s" for operation "%s": %s, %s' %
-                       (func.__name__, endpoint['operationId'], six.text_type(e), str(e)))
+                       (func.__name__, endpoint['operationId'], six.text_type(e)))
             raise e
         except Exception as e:
             LOG.exception('Failed to call controller function "%s" for operation "%s": %s' %
