bd77f1e7d63e3aa754a9128b61e6be93b2f27ce2
Amanda McGuinness <amanda.mcguinness@ammeonsolutions.com>
2020-09-09 22:20:20 +0100

    Update test_get_pack_warnings.py

--- contrib/packs/tests/test_get_pack_warnings.py
+++ contrib/packs/tests/test_get_pack_warnings.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 # Copyright 2020 The StackStorm Authors.
-# Copyright 2019 Extreme Networks, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
