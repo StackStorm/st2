aa937a177010d002dae296ec1830cc939edca5da
Tomaz Muraus <tomaz@tomaz.me>
2021-02-14 20:25:51 +0100

    Update comment.

--- st2common/st2common/util/deep_copy.py
+++ st2common/st2common/util/deep_copy.py
@@ -39,12 +39,11 @@ def fast_deepcopy(value, fall_back_to_deepcopy=True):
                                   exception.
     :type fall_back_to_deepcopy: ``bool``
     """
-    # NOTE: ujson round-trip is up to 10 times faster on smaller and larger dicts compared
-    # to copy.deepcopy(), but it has some edge cases with non-simple types such as datetimes -
+    # NOTE: ujson / ujson round-trip is up to 10 times faster on smaller and larger dicts compared
+    # to copy.deepcopy(), but it has some edge cases with non-simple types such as datetimes
     try:
         # NOTE: ujson serialized datetime to seconds since epoch (int) whereas orjson serializes it
         # to a  RFC 3339 string by default
-        # TODO: Should we also handle ObjectID here transparently?
         value = orjson.loads(orjson.dumps(value, default=default))
     except (OverflowError, ValueError) as e:
         # NOTE: ujson doesn't support 5 or 6 bytes utf-8 sequences which we use
