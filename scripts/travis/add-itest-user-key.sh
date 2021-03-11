6f9703d0cf9f35fe28f8a49831f67ceff00170e1
Jacob Floyd <cognifloyd@gmail.com>
2021-03-11 13:06:15 -0600

    use sudo -E to preserve env vars

--- scripts/travis/add-itest-user-key.sh
+++ scripts/travis/add-itest-user-key.sh
@@ -6,8 +6,9 @@ mkdir -p ~/.ssh
 # Generate ssh keys on StackStorm box and copy over public key into remote box.
 ssh-keygen -f ~/.ssh/st2_id_rsa -P ""
 
+# sudo -E = preserve HOME var
 # Authorize key-base acces
-sudo bash -xc "cat ~/.ssh/st2_id_rsa.pub >> ~/.ssh/authorized_keys"
-sudo chmod 0600 ~/.ssh/authorized_keys
-sudo chmod 0700 ~/.ssh
-sudo chown -R "${ST2_CI_USER}:${ST2_CI_USER}" ~/.ssh
+sudo -E bash -xc "cat ~/.ssh/st2_id_rsa.pub >> ~/.ssh/authorized_keys"
+sudo -E chmod 0600 ~/.ssh/authorized_keys
+sudo -E chmod 0700 ~/.ssh
+sudo -E chown -R "${ST2_CI_USER}:${ST2_CI_USER}" ~/.ssh
