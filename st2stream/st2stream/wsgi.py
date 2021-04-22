ae326c936ebdcf9b2dbeba87a9ce4c04812ab84d
Tomaz Muraus <tomaz@tomaz.me>
2021-04-22 13:06:52 +0200

    Update wsgi.py
    Perform monkey patch as the very first thing - even before importing os module.

--- st2stream/st2stream/wsgi.py
+++ st2stream/st2stream/wsgi.py
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from st2common.util.monkey_patch import monkey_patch
 
 monkey_patch()
+
+import os
 
 from st2stream import app
 
