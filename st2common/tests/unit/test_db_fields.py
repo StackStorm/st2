62f7e35dc2907c70aa3e1d554a3ce5d668b089e2
guzzijones12 <guzzijones12@gmail.com>
2021-05-14 13:14:27 +0000

    add no header test for serialize set

--- st2common/tests/unit/test_db_fields.py
+++ st2common/tests/unit/test_db_fields.py
@@ -78,6 +78,11 @@ class JSONDictFieldTestCase(unittest2.TestCase):
         result = field.to_mongo({"test": {1, 2}})
         self.assertTrue(isinstance(result, bytes))
 
+    def test_header_set_to_mongo(self):
+        field = JSONDictField(use_header=True)
+        result = field.to_mongo({"test": {1, 2}})
+        self.assertTrue(isinstance(result, bytes))
+
     def test_to_mongo(self):
         field = JSONDictField(use_header=False)
         result = field.to_mongo(MOCK_DATA_DICT)
