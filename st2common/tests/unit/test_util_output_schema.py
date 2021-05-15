4ef3765037f88ea89e054e1811c7ef394c6419d4
mahesh-orch <81608498+mahesh-orch@users.noreply.github.com>
2021-05-15 21:52:16 +0530

    added unit tests for mask secret output

--- st2common/tests/unit/test_util_output_schema.py
+++ st2common/tests/unit/test_util_output_schema.py
@@ -12,7 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import absolute_import
 import copy
+import mock
 import unittest2
 
 from st2common.util import output_schema
@@ -21,6 +23,14 @@ from st2common.constants.action import (
     LIVEACTION_STATUS_SUCCEEDED,
     LIVEACTION_STATUS_FAILED,
 )
+from st2common.constants.secrets import MASKED_ATTRIBUTE_VALUE
+from st2common.exceptions.db import StackStormDBObjectNotFoundError
+from st2common.models.db.execution import ActionExecutionDB
+from st2common.persistence.execution import ActionExecution
+from st2common.transport.publishers import PoolPublisher
+
+from st2tests import DbTestCase
+
 
 ACTION_RESULT = {
     "output": {
@@ -56,6 +66,59 @@ RUNNER_SCHEMA_FAIL = {
 
 ACTION_SCHEMA_FAIL = {
     "not_a_key_you_have": {"type": "string"},
+}
+
+OUTPUT_SCHEMA_RESULT_1 = {
+    "stdout": "",
+    "stderr": "",
+    "result": {
+        "os_secret_param": "to_be_masked",
+    },
+}
+
+OUTPUT_SCHEMA_RESULT_2 = {
+    "stdout": "",
+    "stderr": "",
+    "result": {
+        "os_non_secret_param": "not_to_be_masked",
+    },
+}
+
+OUTPUT_SCHEMA_LIVEACTION_1 = {
+    "action": "core.ask",
+    "parameters": {},
+}
+
+OUTPUT_SCHEMA_LIVEACTION_2 = {
+    "action": "core.ask",
+    "parameters": {},
+}
+
+ACTIONEXECUTIONS = {
+    "execution_1": {
+        "action": {
+            "uid": "action:core:ask",
+            "output_schema": {
+                "os_secret_param": {"type": "string", "required": True, "secret": True},
+            },
+        },
+        "status": "succeeded",
+        "runner": {"name": "inquirer"},
+        "liveaction": OUTPUT_SCHEMA_LIVEACTION_1,
+        "result": OUTPUT_SCHEMA_RESULT_1,
+    },
+    "execution_2": {
+        "action": {
+            "uid": "action:core:ask",
+            "output_schema": {
+                "os_non_secret_param": {"type": "string", "required": True},
+            },
+        },
+        "status": "succeeded",
+        "runner": {"name": "inquirer"},
+        "liveaction": OUTPUT_SCHEMA_LIVEACTION_2,
+        "result": OUTPUT_SCHEMA_RESULT_2,
+    },
 }
 
 OUTPUT_KEY = "output"
@@ -117,3 +180,75 @@ class OutputSchemaTestCase(unittest2.TestCase):
         self.assertIn(expected_result["error"], result["error"])
         self.assertEqual(result["message"], expected_result["message"])
         self.assertEqual(status, LIVEACTION_STATUS_FAILED)
+    
+    
+    @mock.patch.object(PoolPublisher, "publish", mock.MagicMock())
+class ActionExecutionModelTest(DbTestCase):
+    def setUp(self):
+
+        self.executions = {}
+
+        for name, execution in ACTIONEXECUTIONS.items():
+
+            created = ActionExecutionDB()
+            created.action = execution["action"]
+            created.status = execution["status"]
+            created.runner = execution["runner"]
+            created.liveaction = execution["liveaction"]
+            created.result = execution["result"]
+            saved = ActionExecutionModelTest._save_execution(created)
+            retrieved = ActionExecution.get_by_id(saved.id)
+            self.assertEqual(
+                saved.action, retrieved.action, "Same action was not returned."
+            )
+
+            self.executions[name] = retrieved
+
+    def tearDown(self):
+
+        for name, execution in self.executions.items():
+            ActionExecutionModelTest._delete([execution])
+            try:
+                retrieved = ActionExecution.get_by_id(execution.id)
+            except StackStormDBObjectNotFoundError:
+                retrieved = None
+            self.assertIsNone(retrieved, "managed to retrieve after failure.")
+
+    def test_output_schema_secret_param_masking(self):
+        """
+        Test that the parameter marked secret as true in output schema is masked in the output
+        result. Here the parameter in output schema is marked secret as true and we are
+        asserting this is masked in the output result.
+        """
+
+        masked = self.executions["execution_1"].mask_secrets(
+            self.executions["execution_1"].to_serializable_dict()
+        )
+        print("masked: ", masked)
+        self.assertEqual(
+            masked["result"]["result"]["os_secret_param"], MASKED_ATTRIBUTE_VALUE
+        )
+
+    def test_output_schema_non_secret_param_not_masking(self):
+        """
+        Test that the parameters is marked secret as true in output schema is not masked in
+        the output result. Here the parameter in output schema is not marked secret as
+        true and we are asserting this isn't masked in the output result.
+        """
+
+        non_masked = self.executions["execution_2"].mask_secrets(
+            self.executions["execution_2"].to_serializable_dict()
+        )
+        print("non_masked: ", non_masked)
+        self.assertEqual(
+            non_masked["result"]["result"]["os_non_secret_param"], "not_to_be_masked"
+        )
+
+    @staticmethod
+    def _save_execution(execution):
+        return ActionExecution.add_or_update(execution)
+
+    @staticmethod
+    def _delete(model_objects):
+        for model_object in model_objects:
+            model_object.delete()
