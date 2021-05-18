66afa347dfb8890d7731ec9ae29be88d64397275
mahesh-orch <81608498+mahesh-orch@users.noreply.github.com>
2021-05-18 21:53:06 +0530

    adding a workflow unit test for ouptut schema

--- contrib/runners/orquesta_runner/tests/unit/test_data_flow.py
+++ contrib/runners/orquesta_runner/tests/unit/test_data_flow.py
@@ -201,3 +201,98 @@ class OrquestaRunnerTest(st2tests.ExecutionDbTestCase):
 
     def test_unicode_string(self):
         self.assert_data_flow("床前明月光 疑是地上霜 舉頭望明月 低頭思故鄉")
+    
+    def test_workflow_for_action_with_output_schema(self, data):
+        wf_meta = base.get_wf_fixture_meta_data(TEST_PACK_PATH, "secret-params-flow.yaml")
+        wf_input = {"a1": data}
+        lv_ac_db = lv_db_models.LiveActionDB(
+            action=wf_meta["name"], parameters=wf_input
+        )
+        lv_ac_db, ac_ex_db = ac_svc.request(lv_ac_db)
+
+        # Assert action execution is running.
+        lv_ac_db = lv_db_access.LiveAction.get_by_id(str(lv_ac_db.id))
+        self.assertEqual(
+            lv_ac_db.status, ac_const.LIVEACTION_STATUS_RUNNING, lv_ac_db.result
+        )
+        wf_ex_db = wf_db_access.WorkflowExecution.query(
+            action_execution=str(ac_ex_db.id)
+        )[0]
+        self.assertEqual(wf_ex_db.status, ac_const.LIVEACTION_STATUS_RUNNING)
+
+        # Assert task1 is already completed.
+        query_filters = {"workflow_execution": str(wf_ex_db.id), "task_id": "task1"}
+        tk1_ex_db = wf_db_access.TaskExecution.query(**query_filters)[0]
+        tk1_ac_ex_db = ex_db_access.ActionExecution.query(
+            task_execution=str(tk1_ex_db.id)
+        )[0]
+        tk1_lv_ac_db = lv_db_access.LiveAction.get_by_id(tk1_ac_ex_db.liveaction["id"])
+        self.assertEqual(tk1_lv_ac_db.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+
+        # Manually handle action execution completion.
+        wf_svc.handle_action_execution_completion(tk1_ac_ex_db)
+
+        # Assert task1 succeeded and workflow is still running.
+        tk1_ex_db = wf_db_access.TaskExecution.get_by_id(tk1_ex_db.id)
+        self.assertEqual(tk1_ex_db.status, wf_statuses.SUCCEEDED)
+        wf_ex_db = wf_db_access.WorkflowExecution.get_by_id(wf_ex_db.id)
+        self.assertEqual(wf_ex_db.status, wf_statuses.RUNNING)
+
+        # Assert task2 is already completed.
+        query_filters = {"workflow_execution": str(wf_ex_db.id), "task_id": "task2"}
+        tk2_ex_db = wf_db_access.TaskExecution.query(**query_filters)[0]
+        tk2_ac_ex_db = ex_db_access.ActionExecution.query(
+            task_execution=str(tk2_ex_db.id)
+        )[0]
+        tk2_lv_ac_db = lv_db_access.LiveAction.get_by_id(tk2_ac_ex_db.liveaction["id"])
+        self.assertEqual(tk2_lv_ac_db.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+
+        # Manually handle action execution completion.
+        wf_svc.handle_action_execution_completion(tk2_ac_ex_db)
+
+        # Assert task2 succeeded and workflow is still running.
+        tk2_ex_db = wf_db_access.TaskExecution.get_by_id(tk2_ex_db.id)
+        self.assertEqual(tk2_ex_db.status, wf_statuses.SUCCEEDED)
+        wf_ex_db = wf_db_access.WorkflowExecution.get_by_id(wf_ex_db.id)
+        self.assertEqual(wf_ex_db.status, wf_statuses.RUNNING)
+
+        # Assert task3 is already completed.
+        query_filters = {"workflow_execution": str(wf_ex_db.id), "task_id": "task3"}
+        tk3_ex_db = wf_db_access.TaskExecution.query(**query_filters)[0]
+        # tk3_ac_ex_db = ex_db_access.ActionExecution.query(
+        #     task_execution=str(tk3_ex_db.id)
+        # )[0]
+        # tk3_lv_ac_db = lv_db_access.LiveAction.get_by_id(tk3_ac_ex_db.liveaction["id"])
+        # self.assertEqual(tk3_lv_ac_db.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+
+        # # Manually handle action execution completion.
+        # wf_svc.handle_action_execution_completion(tk3_ac_ex_db)
+
+        # # Assert task3 succeeded and workflow is completed.
+        # tk3_ex_db = wf_db_access.TaskExecution.get_by_id(tk3_ex_db.id)
+        # self.assertEqual(tk3_ex_db.status, wf_statuses.SUCCEEDED)
+        # wf_ex_db = wf_db_access.WorkflowExecution.get_by_id(wf_ex_db.id)
+        # self.assertEqual(wf_ex_db.status, wf_statuses.SUCCEEDED)
+        # lv_ac_db = lv_db_access.LiveAction.get_by_id(str(lv_ac_db.id))
+        # self.assertEqual(lv_ac_db.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+        # ac_ex_db = ex_db_access.ActionExecution.get_by_id(str(ac_ex_db.id))
+        # self.assertEqual(ac_ex_db.status, ac_const.LIVEACTION_STATUS_SUCCEEDED)
+
+        # # Check workflow output.
+        # expected_output = {
+        #     "secretParam": wf_input["secretParam"] if six.PY3 else wf_input["secretParam"].decode("utf-8"),
+        # }
+
+        # self.assertDictEqual(wf_ex_db.output, expected_output)
+
+        # # Check liveaction and action execution result.
+        # expected_result = {"output": expected_output}
+
+        # self.assertDictEqual(lv_ac_db.result, expected_result)
+        # self.assertDictEqual(ac_ex_db.result, expected_result)
+
+    def test_string(self):
+        self.assert_data_flow("xyz")
+
+    def test_unicode_string(self):
+        self.assert_data_flow("床前明月光 疑是地上霜 舉頭望明月 低頭思故鄉")
