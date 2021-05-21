a445005f09ac13b789da5f646eb2b3f1b2887ede
Jacob Floyd <cognifloyd@gmail.com>
2021-05-21 12:57:43 -0500

    pants: combine mongo_is_running rules

--- pants-plugins/uses_services/mongo.py
+++ pants-plugins/uses_services/mongo.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 from textwrap import dedent
 
 from pants.backend.python.goals.pytest_runner import (
@@ -34,13 +32,9 @@ class UsesMongoRequest(PytestPluginSetupRequest):
         return uses is not None and "mongo" in uses
 
 
-@dataclass(frozen=True)
-class MongoStatus:
-    is_running: bool
-
-
 @rule(desc="Test to see if mongodb is running and accessible for tests.")
-async def mongo_is_running() -> MongoStatus:
+async def mongo_is_running(request: UsesMongoRequest, platform: Platform) -> PytestPluginSetup:
+
     # These config opts are used via oslo_config.cfg.CONF.database.{host,port,db_name,connection_timeout}
     # These config opts currently hard-coded in:
     #   for unit tests: st2tests/st2tests/config.py
@@ -101,129 +95,124 @@ async def mongo_is_running() -> MongoStatus:
             level=LogLevel.DEBUG,
         )
     )
-    return MongoStatus(result.exit_code == 0)
+    is_running = result.exit_code == 0
 
+    if is_running:
+        return PytestPluginSetup()
 
-@rule("Report that mongodb is required for test runs.")
-async def assert_mongo_is_running(
-    request: UsesMongoRequest, mongo_status: MongoStatus, platform: Platform
-) -> PytestPluginSetup:
-    if not mongo_status.is_running:
-        if platform.distro in ["centos", "rhel"] or "rhel" in platform.distro_like:
-            instructions = dedent(
-                f"""\
-                If mongo is installed, but not running try:
+    if platform.distro in ["centos", "rhel"] or "rhel" in platform.distro_like:
+        instructions = dedent(
+            f"""\
+            If mongo is installed, but not running try:
 
-                """
-            )
+            """
+        )
 
-            if platform.distro_major_version == "7":
-                instructions += "\nservice mongo start\n"
-            else:
-                instructions += "\nsystemctl start mongod\n"
-
-            instructions += dedent(
-                """
-                If mongo is not installed, this is one way to install it:
-
-                # Add key and repo for the latest stable MongoDB (4.0)
-                rpm --import https://www.mongodb.org/static/pgp/server-4.0.asc
-                sh -c "cat <<EOT > /etc/yum.repos.d/mongodb-org-4.repo
-                [mongodb-org-4]
-                name=MongoDB Repository
-                baseurl=https://repo.mongodb.org/yum/redhat/${OSRELEASE_VERSION}/mongodb-org/4.0/x86_64/
-                gpgcheck=1
-                enabled=1
-                gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
-                EOT"
-                # install mongo
-                yum install mongodb-org
-                # Don't forget to start mongo.
-                """
-            )
-        elif platform.distro in ["ubuntu", "debian"] or "debian" in platform.distro_like:
-            instructions = dedent(
-                """\
-                If mongo is installed, but not running try:
-
-                systemctl start mongod
-
-                If mongo is not installed, this is one way to install it:
-
-                apt-get install mongodb mongodb-server
-                # Don't forget to start mongo.
-                """
-            )
-        elif platform.os == "Linux":
-            instructions = dedent(
-                f"""\
-                You are on Linux using {platform.distro_name}, which is not
-                one of our generally supported distributions. We recommend
-                you use vagrant for local development with something like:
-
-                vagrant init stackstorm/st2
-                vagrant up
-                vagrant ssh
-
-                Please see: https://docs.stackstorm.com/install/vagrant.html
-
-                For anyone who wants to attempt local development without vagrant,
-                you are pretty much on your own. At a minimum you need to install
-                and start mongo with something like:
-
-                systemctl start mongod
-
-                We would be interested to hear about alternative distros people
-                are using for development. If you are able, please let us know
-                on slack which distro you are using:
-
-                Arch: {platform.arch}
-                Distro: {platform.distro}
-                Distro Codename: {platform.distro_codename}
-                Distro Version: {platform.distro_version}
-
-                Thanks and Good Luck!
-                """
-            )
-        elif platform.os == "Darwin":  # MacOS
-            instructions = dedent(
-                """\
-                You are on Mac OS. Generally we recommend using vagrant for local
-                development on Mac OS with something like:
-
-                vagrant init stackstorm/st2
-                vagrant up
-                vagrant ssh
-
-                Please see: https://docs.stackstorm.com/install/vagrant.html
-
-                For anyone who wants to attempt local development without vagrant,
-                you may run into some speed bumps. Others StackStorm developers have
-                been known to use Mac OS for development, so feel free to ask for
-                help in slack. At a minimum you need to install and start mongo.
-                """
-            )
+        if platform.distro_major_version == "7":
+            instructions += "\nservice mongo start\n"
         else:
-            instructions = dedent(
-                """\
-                You are not on Linux. In this case we recommend using vagrant
-                for local development with something like:
+            instructions += "\nsystemctl start mongod\n"
 
-                vagrant init stackstorm/st2
-                vagrant up
-                vagrant ssh
+        instructions += dedent(
+            """
+            If mongo is not installed, this is one way to install it:
 
-                Please see: https://docs.stackstorm.com/install/vagrant.html
+            # Add key and repo for the latest stable MongoDB (4.0)
+            rpm --import https://www.mongodb.org/static/pgp/server-4.0.asc
+            sh -c "cat <<EOT > /etc/yum.repos.d/mongodb-org-4.repo
+            [mongodb-org-4]
+            name=MongoDB Repository
+            baseurl=https://repo.mongodb.org/yum/redhat/${OSRELEASE_VERSION}/mongodb-org/4.0/x86_64/
+            gpgcheck=1
+            enabled=1
+            gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
+            EOT"
+            # install mongo
+            yum install mongodb-org
+            # Don't forget to start mongo.
+            """
+        )
+    elif platform.distro in ["ubuntu", "debian"] or "debian" in platform.distro_like:
+        instructions = dedent(
+            """\
+            If mongo is installed, but not running try:
 
-                For anyone who wants to attempt local development without vagrant,
-                you are pretty much on your own. At a minimum you need to install
-                and start mongo. Good luck!
+            systemctl start mongod
+
+            If mongo is not installed, this is one way to install it:
+
+            apt-get install mongodb mongodb-server
+            # Don't forget to start mongo.
+            """
+        )
+    elif platform.os == "Linux":
+        instructions = dedent(
+            f"""\
+            You are on Linux using {platform.distro_name}, which is not
+            one of our generally supported distributions. We recommend
+            you use vagrant for local development with something like:
+
+            vagrant init stackstorm/st2
+            vagrant up
+            vagrant ssh
+
+            Please see: https://docs.stackstorm.com/install/vagrant.html
+
+            For anyone who wants to attempt local development without vagrant,
+            you are pretty much on your own. At a minimum you need to install
+            and start mongo with something like:
+
+            systemctl start mongod
+
+            We would be interested to hear about alternative distros people
+            are using for development. If you are able, please let us know
+            on slack which distro you are using:
+
+            Arch: {platform.arch}
+            Distro: {platform.distro}
+            Distro Codename: {platform.distro_codename}
+            Distro Version: {platform.distro_version}
+
+            Thanks and Good Luck!
+            """
+        )
+    elif platform.os == "Darwin":  # MacOS
+        instructions = dedent(
+            """\
+            You are on Mac OS. Generally we recommend using vagrant for local
+            development on Mac OS with something like:
+
+            vagrant init stackstorm/st2
+            vagrant up
+            vagrant ssh
+
+            Please see: https://docs.stackstorm.com/install/vagrant.html
+
+            For anyone who wants to attempt local development without vagrant,
+            you may run into some speed bumps. Others StackStorm developers have
+            been known to use Mac OS for development, so feel free to ask for
+            help in slack. At a minimum you need to install and start mongo.
+            """
+        )
+    else:
+        instructions = dedent(
+            """\
+            You are not on Linux. In this case we recommend using vagrant
+            for local development with something like:
+
+            vagrant init stackstorm/st2
+            vagrant up
+            vagrant ssh
+
+            Please see: https://docs.stackstorm.com/install/vagrant.html
+
+            For anyone who wants to attempt local development without vagrant,
+            you are pretty much on your own. At a minimum you need to install
+            and start mongo. Good luck!
                 """
-            )
+        )
 
-        raise ServiceMissingError("mongo", platform, instructions)
-
-    return PytestPluginSetup()
+    raise ServiceMissingError("mongo", platform, instructions)
 
 
 def rules():
