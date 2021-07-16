f6b91b9048efd2e4175166a0eeef7a7f5e532f19
mahesh-orch <81608498+mahesh-orch@users.noreply.github.com>
2021-07-16 10:29:49 +0530

    Updating actions.py

--- st2api/st2api/controllers/v1/actions.py
+++ st2api/st2api/controllers/v1/actions.py
@@ -241,8 +241,12 @@ class ActionsController(resource.ContentPackResourceController):
         pack_name = action_db["pack"]
         entry_point = action_db["entry_point"]
         metadate_file = action_db["metadata_file"]
-        action_entrypoint_file_path = os.path.join(BASE_PATH, "packs", pack_name, "actions", entry_point)
-        action_metadata_file_path = os.path.join(BASE_PATH, "packs", pack_name, metadate_file)
+        action_entrypoint_file_path = os.path.join(
+            BASE_PATH, "packs", pack_name, "actions", entry_point
+        )
+        action_metadata_file_path = os.path.join(
+            BASE_PATH, "packs", pack_name, metadate_file
+        )
 
         try:
             Action.delete(action_db)
