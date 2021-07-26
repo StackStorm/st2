0769eb125f0752ca207742142e1b528e3d00d9e6
mahesh-orch <81608498+mahesh-orch@users.noreply.github.com>
2021-07-26 17:38:54 +0530

    Updating /st2common/services/packs.py

--- st2common/st2common/services/packs.py
+++ st2common/st2common/services/packs.py
@@ -282,9 +282,7 @@ def delete_action_files_from_pack(pack_name, entry_point, metadata_file):
                 e,
             )
             if os.path.isfile(action_metadata_file_path):
-                msg = (
-                    'Delete operation unsuccessful. "{0}" file still exists on disk'.format(
-                        action_metadata_file_path
-                    )
+                msg = 'Delete operation unsuccessful. "{0}" file still exists on disk'.format(
+                    action_metadata_file_path
                 )
                 abort(http_client.INTERNAL_SERVER_ERROR, six.text_type(msg))
