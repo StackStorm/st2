5e3d37baadedf3d2c25b98455f1cbbc6359a8abe
blag <blag@users.noreply.github.com>
2021-07-27 13:39:22 -0700

    Format pythonpath.py with black

--- contrib/debug/actions/pythonpath.py
+++ contrib/debug/actions/pythonpath.py
@@ -6,10 +6,10 @@ from st2common.runners.base_action import Action
 
 class PythonPathAction(Action):
     def run(self, *args, **kwargs):
-        pythonpath = os.environ.get('PYTHONPATH')
+        pythonpath = os.environ.get("PYTHONPATH")
         if pythonpath:
-            pythonpath = pythonpath.split(':')
+            pythonpath = pythonpath.split(":")
         return {
             "PYTHONPATH": pythonpath,
-            'sys.path': sys.path,
+            "sys.path": sys.path,
         }
