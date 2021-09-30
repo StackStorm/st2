088198263774507f4800f6c9656fa6ce41783ffe
ashwini-orchestral <72917414+ashwini-orchestral@users.noreply.github.com>
2021-09-30 21:37:11 +0530

    Modified unit tests for multiple users and roles kvp

--- st2common/tests/unit/services/test_keyvalue.py
+++ st2common/tests/unit/services/test_keyvalue.py
@@ -14,16 +14,25 @@
 # limitations under the License.
 
 from __future__ import absolute_import
-import unittest2
-import mock
+from st2tests.api import FunctionalTest
 
-from st2common.constants.keyvalue import SYSTEM_SCOPE, USER_SCOPE
+from st2common.constants.keyvalue import SYSTEM_SCOPE, USER_SCOPE, FULL_SYSTEM_SCOPE
 from st2common.exceptions.keyvalue import InvalidScopeException, InvalidUserException
 from st2common.services.keyvalues import get_key_reference
 from st2common.services.keyvalues import get_all_system_kvp_names_for_user
+from st2common.persistence.auth import User
+from st2common.models.db.auth import UserDB
+from st2common.models.db.rbac import UserRoleAssignmentDB
+from st2common.models.db.rbac import PermissionGrantDB
+from st2common.rbac.types import PermissionType
+from st2common.rbac.types import ResourceType
+from st2common.persistence.rbac import UserRoleAssignment
+from st2common.persistence.rbac import PermissionGrant
+from st2common.persistence.rbac import Role
+from st2common.models.db.rbac import RoleDB
 
 
-class KeyValueServicesTest(unittest2.TestCase):
+class KeyValueServicesTest(FunctionalTest):
     def test_get_key_reference_system_scope(self):
         ref = get_key_reference(scope=SYSTEM_SCOPE, name="foo")
         self.assertEqual(ref, "foo")
@@ -44,13 +53,54 @@ class KeyValueServicesTest(unittest2.TestCase):
             InvalidScopeException, get_key_reference, scope="sketchy", name="foo"
         )
 
-    @mock.patch("st2common.services.keyvalues.get_uids")
-    def test_get_all_system_kvp_names_for_user(self, mock_get_uids):
-        mock_get_uids.return_value = [
-            "key_value_pair:st2kv.system:key2",
-            "key_value_pair:st2kv.system:",
-            "key_value_pair:st2kv.system:key4",
-            "key_value_pair:st2kv.system:key2",
-        ]
-        key_list = get_all_system_kvp_names_for_user(user="stanley")
+    def test_get_all_system_kvp_names_for_user(self):
+        kvp_1_uid = "%s:%s:key2" % (ResourceType.KEY_VALUE_PAIR, FULL_SYSTEM_SCOPE)
+        kvp_2_uid = "%s:%s:key4" % (ResourceType.KEY_VALUE_PAIR, FULL_SYSTEM_SCOPE)
+        kvp_3_uid = "%s:%s:echo" % (ResourceType.ACTION, "core")
+        kvp_4_uid = "%s:%s:new_action" % (ResourceType.ACTION, "dummy")
+
+        # Setup user, grant, role, and assignment records
+        user_db = UserDB(name="system_key1_user")
+        user_db = User.add_or_update(user_db)
+
+        # role assignment
+        grant_db = PermissionGrantDB(
+            resource_uid=kvp_1_uid,
+            resource_type=ResourceType.KEY_VALUE_PAIR,
+            permission_types=[PermissionType.KEY_VALUE_PAIR_LIST],
+        )
+        grant_db = PermissionGrant.add_or_update(grant_db)
+        grant_1_db = PermissionGrantDB(
+            resource_uid=kvp_2_uid,
+            resource_type=ResourceType.KEY_VALUE_PAIR,
+            permission_types=[PermissionType.KEY_VALUE_PAIR_VIEW],
+        )
+        grant_1_db = PermissionGrant.add_or_update(grant_1_db)
+        grant_2_db = PermissionGrantDB(
+            resource_uid=kvp_3_uid,
+            resource_type=ResourceType.ACTION,
+            permission_types=[PermissionType.ACTION_VIEW],
+        )
+        grant_2_db = PermissionGrant.add_or_update(grant_2_db)
+        grant_3_db = PermissionGrantDB(
+            resource_uid=kvp_4_uid,
+            resource_type=ResourceType.ACTION,
+            permission_types=[PermissionType.ACTION_LIST],
+        )
+        grant_3_db = PermissionGrant.add_or_update(grant_3_db)
+
+        role_db = RoleDB(
+            name="custom_role_system_keys_grant",
+            permission_grants=[str(grant_db.id), str(grant_1_db.id), str(grant_2_db.id), str(grant_3_db.id)],
+        )
+        role_db = Role.add_or_update(role_db)
+
+        role_assignment_db = UserRoleAssignmentDB(
+            user=user_db.name,
+            role=role_db.name,
+            source="assignments/%s.yaml" % user_db.name,
+        )
+        UserRoleAssignment.add_or_update(role_assignment_db)
+
+        key_list = get_all_system_kvp_names_for_user(user=user_db.name)
         self.assertEqual(key_list, ["key2", "key4"])
