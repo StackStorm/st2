559bbcc2e072e3831cb2aad90950829070abef21
wingiti <73697233+wingiti@users.noreply.github.com>
2021-10-12 22:10:27 +0200

    Update sandboxing.py
    idea how to use site-package of custom python version for action runners.

--- st2common/st2common/util/sandboxing.py
+++ st2common/st2common/util/sandboxing.py
@@ -137,6 +137,23 @@ def get_sandbox_python_path_for_python_action(
 
     pack_base_path = get_pack_base_path(pack_name=pack)
     virtualenv_path = get_sandbox_virtualenv_path(pack=pack)
+    custom_python_binary = cfg.CONF.actionrunner.python_binary
+
+    # Add the custom python's site-packages directory in front of the Python
+    # system site-packages.
+    if custom_python_binary and os.path.isfile(custom_python_binary):
+        python_version = custom_python_binary.rsplit("/", 1)[1]
+        system_prefix_dirs = ['/usr/lib', '/usr/local/lib']
+        system_dir_names = ['site-packages', 'dist-packages']
+        for system_prefix_dir in system_prefix_dirs:
+            for system_dir_name in system_dir_names:
+                if os.path.isdir(os.path.join(system_prefix_dir, python_version, system_dir_name)):
+                    custom_python_site_packages_dir = os.path.join(system_prefix_dir,
+                                                                   python_version,
+                                                                   system_dir_name)
+                    existing_sandbox_python_path = sandbox_python_path
+                    sandbox_python_path = "{}: {}".format(custom_python_site_packages_dir,
+                                                          existing_sandbox_python_path)
 
     if virtualenv_path and os.path.isdir(virtualenv_path):
         pack_virtualenv_lib_path = os.path.join(virtualenv_path, "lib")
