91ca4c21fd7923586bed5cc2f4a84a9a8f00d194
Jacob Floyd <cognifloyd@gmail.com>
2021-10-18 13:35:43 -0500

    Update contrib/core/actions/inject_trigger.py

--- contrib/core/actions/inject_trigger.py
+++ contrib/core/actions/inject_trigger.py
@@ -41,9 +41,7 @@ class InjectTriggerAction(Action):
 
         # Raise an error if neither trigger nor trigger_name are specified
         if not trigger and not trigger_name:
-            raise ValueError(
-                "You must include the `trigger_name` parameter."
-            )
+            raise ValueError("You must include the `trigger_name` parameter.")
 
         trigger = trigger if trigger else trigger_name
         self.logger.debug(
