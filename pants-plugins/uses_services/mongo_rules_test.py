7d9320d63cb24960285238965b7c0244d4264074
Jacob Floyd <cognifloyd@gmail.com>
2023-01-07 00:44:26 -0600

    add test for uses_services plugin mongo rule

---
+++ pants-plugins/uses_services/mongo_rules_test.py
@@ -0,0 +1,193 @@
+# Copyright 2023 The StackStorm Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import os
+
+import pytest
+
+from pants.backend.python import target_types_rules
+from pants.backend.python.target_types import PythonSourcesGeneratorTarget
+
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.addresses import Address
+from pants.engine.internals.scheduler import ExecutionError
+from pants.engine.fs import CreateDigest, Digest, FileContent, Snapshot
+from pants.engine.target import Target
+from pants.core.goals.fmt import FmtResult
+from pants.testutil.rule_runner import QueryRule, RuleRunner
+
+from .exceptions import ServiceMissingError
+from .mongo_rules import (
+    MongoIsRunning,
+    PytestUsesMongoRequest,
+    UsesMongoRequest,
+    rules as mongo_rules
+)
+from .platform_rules import Platform
+from .target_types import UsesServicesField
+
+
+class MockTarget(Target):
+    alias = "mock_target"
+    core_fields = (UsesServicesField,)
+
+
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    return RuleRunner(
+        rules=[
+            *mongo_rules(),
+            # *target_types_rules.rules(),
+            QueryRule(MongoIsRunning, (UsesMongoRequest, Platform)),
+            # QueryRule(PytestPluginSetup, (PytestUsesMongoRequest)),
+        ],
+        target_types=[
+            # MockTarget,
+            # PythonTestsGeneratorTarget,
+            # PythonTestTarget,
+        ],
+    )
+
+
+def run_mongo_is_running(
+    rule_runner: RuleRunner,
+    uses_mongo_request: UsesMongoRequest,
+    mock_platform: Platform,
+    *,
+    extra_args: list[str] | None = None,
+) -> MongoIsRunning:
+    rule_runner.set_options(
+        [
+            "--backend-packages=uses_services",
+            *(extra_args or ()),
+        ],
+        env_inherit={"PATH", "PYENV_ROOT", "HOME"},
+    )
+    result = rule_runner.request(
+        MongoIsRunning,
+        [uses_mongo_request, mock_platform],
+    )
+    return result
+
+
+def platform(
+    arch="",
+    os="",
+    distro="",
+    distro_name="",
+    distro_codename="",
+    distro_like="",
+    distro_major_version="",
+    distro_version="",
+    mac_release="",
+    win_release="",
+) -> Platform:
+    """Create a Platform with all values defaulted to the empty string."""
+    return Platform(
+        arch=arch,
+        os=os,
+        distro=distro,
+        distro_name=distro_name,
+        distro_codename=distro_codename,
+        distro_like=distro_like,
+        distro_major_version=distro_major_version,
+        distro_version=distro_version,
+        mac_release=mac_release,
+        win_release=win_release,
+    )
+
+
+# TODO: this requires that mongo be running...
+#def test_is_running(rule_runner: RuleRunner) -> None:
+#    request = UsesMongoRequest()
+#    mock_platform = platform()
+
+    # we are asserting that this does not raise an exception
+#    is_running = run_mongo_is_running(rule_runner, request, mock_platform)
+#    assert is_running
+
+
+@pytest.mark.parametrize(
+    "mock_platform",
+    (
+        platform(),  # empty
+        # platform(
+        #    arch="x86_64",
+        #    os="Linux",
+        #    distro="",
+        #    distro_name="",
+        #    distro_codename="",
+        #    distro_like="",
+        #    distro_major_version="",
+        #    distro_version="",
+        # ),
+        platform(
+            arch="x86_64",
+            os="Linux",
+            distro="centos",
+            distro_name="Centos Linux",
+            distro_codename="Core",
+            distro_like="rhel fedora",
+            distro_major_version="7",
+            distro_version="7",
+        ),
+        platform(
+            arch="x86_64",
+            os="Linux",
+            distro="ubuntu",
+            distro_name="Ubuntu",
+            distro_codename="xenial",
+            distro_like="debian",
+            distro_major_version="16",
+            distro_version="16.04",
+        ),
+        platform(
+            arch="x86_64",
+            os="Linux",
+            distro="gentoo",
+            distro_name="Gentoo",
+            distro_codename="n/a",
+            distro_major_version="2",
+            distro_version="2.7",
+        ),
+        platform(
+            arch="x86_64",
+            os="Darwin",
+            distro="darwin",
+            distro_name="Darwin",
+            distro_major_version="19",
+            distro_version="19.6.0",
+            mac_release="10.15.7",
+        ),
+    ),
+)
+def test_not_running(rule_runner: RuleRunner, mock_platform: Platform) -> None:
+    request = UsesMongoRequest(
+        db_host="127.100.20.7",
+        db_port=10, # unassigned port, unlikely to be used
+    )
+
+    with pytest.raises(ExecutionError) as exception_info:
+        run_mongo_is_running(rule_runner, request, mock_platform)
+
+    execution_error = exception_info.value
+    assert len(execution_error.wrapped_exceptions) == 1
+
+    exc = execution_error.wrapped_exceptions[0]
+    assert isinstance(exc, ServiceMissingError)
+    
+    assert exc.service == "mongo"
+    assert "The mongo service does not seem to be running" in str(exc)
+    assert exc.instructions != ""
