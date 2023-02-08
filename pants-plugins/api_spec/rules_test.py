11187ac057ada71e2235f8a328d2a23bb9755f0f
Jacob Floyd <cognifloyd@gmail.com>
2023-02-08 10:26:43 -0600

    update pants-plugins/api_spec to pants 2.15
    The Fmt/Lint plugin API changed in pants 2.15
    
    It now requires a Subsystem with a SkipOption.
    I took advantage of this to consolidate the constants and all
    the address and pex request definitions in the Subsystem subclass.
    
    It now requires 2 rules instead of 1: to partition, then fmt/lint.
    That means the structure of the rule requests had to change
    slightly. The tests also had to account for the modified rules.

--- pants-plugins/api_spec/rules_test.py
+++ pants-plugins/api_spec/rules_test.py
@@ -22,24 +22,23 @@ import pytest
 from pants.backend.python import target_types_rules
 from pants.backend.python.target_types import PythonSourcesGeneratorTarget
 
+from pants.core.util_rules.config_files import rules as config_files_rules
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.addresses import Address
 from pants.engine.fs import CreateDigest, Digest, EMPTY_DIGEST, FileContent, Snapshot
 from pants.engine.target import Target
 from pants.core.goals.fmt import FmtResult
-from pants.core.goals.lint import LintResult, LintResults
+from pants.core.goals.lint import LintResult
+from pants.core.util_rules.partitions import Partitions
 from pants.testutil.rule_runner import QueryRule, RuleRunner
 
 from .rules import (
-    CMD_DIR,
-    CMD_SOURCE_ROOT,
-    GENERATE_CMD,
-    VALIDATE_CMD,
     APISpecFieldSet,
     GenerateAPISpecViaFmtTargetsRequest,
     ValidateAPISpecRequest,
     rules as api_spec_rules,
 )
+from .subsystem import GenerateApiSpec, ValidateApiSpec
 from .target_types import APISpec
 
 
@@ -49,8 +48,16 @@ def rule_runner() -> RuleRunner:
         rules=[
             *api_spec_rules(),
             *target_types_rules.rules(),
-            QueryRule(FmtResult, (GenerateAPISpecViaFmtTargetsRequest,)),
-            QueryRule(LintResults, (ValidateAPISpecRequest,)),
+            *config_files_rules(),
+            # generate
+            QueryRule(
+                Partitions, (GenerateAPISpecViaFmtTargetsRequest.PartitionRequest,)
+            ),
+            QueryRule(FmtResult, (GenerateAPISpecViaFmtTargetsRequest.Batch,)),
+            # validate
+            QueryRule(Partitions, (ValidateAPISpecRequest.PartitionRequest,)),
+            QueryRule(LintResult, (ValidateAPISpecRequest.Batch,)),
+            # etc
             QueryRule(SourceFiles, (SourceFilesRequest,)),
         ],
         target_types=[APISpec, PythonSourcesGeneratorTarget],
@@ -66,23 +73,35 @@ def run_st2_generate_api_spec(
     rule_runner.set_options(
         [
             "--backend-packages=api_spec",
-            f"--source-root-patterns=/{CMD_SOURCE_ROOT}",
+            f"--source-root-patterns=/{GenerateApiSpec.source_root}",
             *(extra_args or ()),
         ],
         env_inherit={"PATH", "PYENV_ROOT", "HOME"},
     )
-    field_sets = [APISpecFieldSet.create(tgt) for tgt in targets]
+    field_sets = tuple(APISpecFieldSet.create(tgt) for tgt in targets)
     input_sources = rule_runner.request(
         SourceFiles,
         [
             SourceFilesRequest(field_set.source for field_set in field_sets),
         ],
     )
+
+    # run generate_api_spec_partitioner rule
+    partitions = rule_runner.request(
+        Partitions,
+        [GenerateAPISpecViaFmtTargetsRequest.PartitionRequest(field_sets)],
+    )
+    assert len(partitions) == 1
+
+    # run generate_api_spec_via_fmt rule
     fmt_result = rule_runner.request(
         FmtResult,
         [
-            GenerateAPISpecViaFmtTargetsRequest(
-                field_sets, snapshot=input_sources.snapshot
+            GenerateAPISpecViaFmtTargetsRequest.Batch(
+                tool_name="",
+                elements=partitions[0].elements,  # ie: files
+                partition_metadata=partitions[0].metadata,
+                snapshot=input_sources.snapshot,
             ),
         ],
     )
@@ -98,19 +117,32 @@ def run_st2_validate_api_spec(
     rule_runner.set_options(
         [
             "--backend-packages=api_spec",
-            f"--source-root-patterns=/{CMD_SOURCE_ROOT}",
+            f"--source-root-patterns=/{ValidateApiSpec.source_root}",
             *(extra_args or ()),
         ],
         env_inherit={"PATH", "PYENV_ROOT", "HOME"},
     )
-    field_sets = [APISpecFieldSet.create(tgt) for tgt in targets]
-    lint_results = rule_runner.request(
-        LintResults,
+    field_sets = tuple(APISpecFieldSet.create(tgt) for tgt in targets)
+
+    # run validate_api_spec_partitioner rule
+    partitions = rule_runner.request(
+        Partitions,
+        [ValidateAPISpecRequest.PartitionRequest(field_sets)],
+    )
+    assert len(partitions) == 1
+
+    # run validate_api_spec_via_fmt rule
+    lint_result = rule_runner.request(
+        LintResult,
         [
-            ValidateAPISpecRequest(field_sets),
+            ValidateAPISpecRequest.Batch(
+                tool_name="",
+                elements=partitions[0].elements,  # ie: field_sets
+                partition_metadata=partitions[0].metadata,
+            ),
         ],
     )
-    return lint_results.results
+    return lint_result
 
 
 # copied from pantsbuild/pants.git/src/python/pants/backend/python/lint/black/rules_integration_test.py
@@ -144,14 +176,15 @@ def write_generate_files(
         f"{api_spec_dir}/{api_spec_file}": before,
         f"{api_spec_dir}/BUILD": f"api_spec(name='t', source='{api_spec_file}')",
         # add in the target that's hard-coded in the generate_api_spec_via_fmt rule
-        f"{CMD_DIR}/{GENERATE_CMD}.py": GENERATE_API_SPEC_PY.format(
+        f"{GenerateApiSpec.directory}/{GenerateApiSpec.cmd}.py": GENERATE_API_SPEC_PY.format(
             api_spec_dir=api_spec_dir, api_spec_text=after
         ),
-        f"{CMD_DIR}/BUILD": "python_sources()",
+        f"{GenerateApiSpec.directory}/BUILD": "python_sources()",
+        GenerateApiSpec.config_file: "",
     }
 
-    module = CMD_DIR
-    while module != CMD_SOURCE_ROOT:
+    module = GenerateApiSpec.directory
+    while module != GenerateApiSpec.source_root:
         files[f"{module}/__init__.py"] = ""
         module = os.path.dirname(module)
 
@@ -217,14 +250,15 @@ def write_validate_files(
         f"{api_spec_dir}/{api_spec_file}": contents,
         f"{api_spec_dir}/BUILD": f"api_spec(name='t', source='{api_spec_file}')",
         # add in the target that's hard-coded in the generate_api_spec_via_fmt rule
-        f"{CMD_DIR}/{VALIDATE_CMD}.py": VALIDATE_API_SPEC_PY.format(
+        f"{ValidateApiSpec.directory}/{ValidateApiSpec.cmd}.py": VALIDATE_API_SPEC_PY.format(
             api_spec_dir=api_spec_dir, rc=rc
         ),
-        f"{CMD_DIR}/BUILD": "python_sources()",
+        f"{ValidateApiSpec.directory}/BUILD": "python_sources()",
+        ValidateApiSpec.config_file: "",
     }
 
-    module = CMD_DIR
-    while module != CMD_SOURCE_ROOT:
+    module = ValidateApiSpec.directory
+    while module != ValidateApiSpec.source_root:
         files[f"{module}/__init__.py"] = ""
         module = os.path.dirname(module)
 
@@ -244,9 +278,8 @@ def test_validate_passed(rule_runner: RuleRunner) -> None:
         Address("my_dir", target_name="t", relative_file_path="dummy.yaml")
     )
     lint_result = run_st2_validate_api_spec(rule_runner, [tgt])
-    assert len(lint_result) == 1
-    assert lint_result[0].exit_code == 0
-    assert lint_result[0].report == EMPTY_DIGEST
+    assert lint_result.exit_code == 0
+    assert lint_result.report == EMPTY_DIGEST
 
 
 def test_validate_failed(rule_runner: RuleRunner) -> None:
@@ -262,6 +295,5 @@ def test_validate_failed(rule_runner: RuleRunner) -> None:
         Address("my_dir", target_name="t", relative_file_path="dummy.yaml")
     )
     lint_result = run_st2_validate_api_spec(rule_runner, [tgt])
-    assert len(lint_result) == 1
-    assert lint_result[0].exit_code == 1
-    assert lint_result[0].report == EMPTY_DIGEST
+    assert lint_result.exit_code == 1
+    assert lint_result.report == EMPTY_DIGEST
