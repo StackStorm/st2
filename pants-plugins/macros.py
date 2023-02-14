97d6ca9eff097d2d196aea858392a42525df0efe
Jacob Floyd <cognifloyd@gmail.com>
2023-02-14 03:23:49 -0600

    Add st2_*_python_distribution to pants-plugins/macros

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -13,6 +13,79 @@
 # limitations under the License.
 
 
+def st2_runner_python_distribution(**kwargs):
+    runner_name = kwargs.pop("runner_name")
+    description = kwargs.pop("description")
+
+    kwargs["provides"] = python_artifact(  # noqa: F821
+        name=f"stackstorm-runner-{runner_name.replace('_', '-')}",
+        description=description,
+        version_file=f"{runner_name}_runner/__init__.py",  # custom for our release plugin
+        # test_suite="tests",
+        zip_safe=kwargs.pop(
+            "zip_safe", True
+        ),  # most runners are safe to run from a zipapp
+    )
+
+    dependencies = kwargs.pop("dependencies", [])
+    for dep in [f"./{runner_name}_runner"]:
+        if dep not in dependencies:
+            dependencies.append(dep)
+    kwargs["dependencies"] = dependencies
+
+    repositories = kwargs.pop("repositories", [])
+    for repo in ["@pypi"]:
+        if repo not in repositories:
+            repositories.append(repo)
+    kwargs["repositories"] = repositories
+
+    python_distribution(**kwargs)  # noqa: F821
+
+
+def st2_component_python_distribution(**kwargs):
+    st2_component = kwargs.pop("component_name")
+
+    description = (
+        f"{st2_component} StackStorm event-driven automation platform component"
+    )
+
+    scripts = kwargs.pop("scripts", [])
+
+    kwargs["provides"] = python_artifact(  # noqa: F821
+        name=st2_component,
+        description=description,
+        scripts=[
+            script[:-6] if script.endswith(":shell") else script for script in scripts
+        ],
+        version_file=f"{st2_component}/__init__.py",  # custom for our release plugin
+        # test_suite=st2_component,
+        zip_safe=False,  # We rely on __file__ to load many things, so st2 should not run from a zipapp
+    )
+
+    dependencies = kwargs.pop("dependencies", [])
+
+    for dep in [st2_component] + scripts:
+        dep = f"./{dep}" if dep[0] != ":" else dep
+        if dep not in dependencies:
+            dependencies.append(dep)
+
+        # see st2_shell_sources_and_resources below
+        if dep.endswith(":shell"):
+            dep_res = f"{dep}_resources"
+            if dep_res not in dependencies:
+                dependencies.append(dep_res)
+
+    kwargs["dependencies"] = dependencies
+
+    repositories = kwargs.pop("repositories", [])
+    for repo in ["@pypi"]:
+        if repo not in repositories:
+            repositories.append(repo)
+    kwargs["repositories"] = repositories
+
+    python_distribution(**kwargs)  # noqa: F821
+
+
 def st2_shell_sources_and_resources(**kwargs):
     """This creates a shell_sources and a resources target.
 
