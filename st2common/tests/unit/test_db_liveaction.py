c1888dc00b50e88d4631fc577057cb24a1291378
guzzijones <guzzijones12@gmail.com>
2023-07-19 16:53:52 +0000

    black formatting fix

--- st2common/tests/unit/test_db_liveaction.py
+++ st2common/tests/unit/test_db_liveaction.py
@@ -155,7 +155,9 @@ class LiveActionModelTest(DbTestCase):
         self.assertEqual(
             saved.action, retrieved.action, "Same triggertype was not returned."
         )
-        import pdb; pdb.set_trace()
+        import pdb
+
+        pdb.set_trace()
         masked = retrieved.mask_secrets(retrieved.to_serializable_dict())
         for value in masked["parameters"]["response"].values():
             self.assertEqual(value, MASKED_ATTRIBUTE_VALUE)
