70fac7c055fd7ee3b3b13a998f55fc5a63ef27d0
Carlos <nzlosh@yahoo.com>
2024-03-14 23:19:04 +0100

    fmt

--- st2client/tests/unit/test_commands.py
+++ st2client/tests/unit/test_commands.py
@@ -494,7 +494,12 @@ class CommandsHelpStringTestCase(BaseCLITestCase):
             self.assertIn(" ".join(command), stdout)
             # argparse on py3.8/py3.9 has a different output to py3.10 so the check for
             # optional arguments covers both formats.
-            assert isinstance(re.search("(optional arguments:|options:)", stdout), re.Match) is True
+            assert (
+                isinstance(
+                    re.search("(optional arguments:|options:)", stdout), re.Match
+                )
+                is True
+            )
 
             # Reset stdout and stderr after each iteration
             self._reset_output_streams()
@@ -515,8 +520,12 @@ class CommandsHelpStringTestCase(BaseCLITestCase):
             self.assertIn(" ".join(command), stdout)
             # argparse on py3.8/py3.9 has a different output to py3.10 so the check for
             # optional arguments covers both formats.
-            assert isinstance(re.search("(optional arguments:|options:)", stdout), re.Match) is True
-
+            assert (
+                isinstance(
+                    re.search("(optional arguments:|options:)", stdout), re.Match
+                )
+                is True
+            )
 
             # Verify that the actual help usage string was triggered and not the invalid
             # "too few arguments" which would indicate command doesn't actually correctly handle
