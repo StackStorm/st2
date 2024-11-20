2720022921b8d563036ba290bd15f7c336de35f1
guzzijones12 <guzzijones12@gmail.com>
2024-11-20 14:04:46 -0500

    fix test hanging due to monkey patch needed

--- st2common/tests/unit/migrations/test_v35_migrate_db_dict_field_values.py
+++ st2common/tests/unit/migrations/test_v35_migrate_db_dict_field_values.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from st2common.util.monkey_patch import monkey_patch
+
+monkey_patch()
 
 import os
 import sys
