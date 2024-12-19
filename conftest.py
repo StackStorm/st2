784dc7f166befb92d12e5922d4d85d25a46d1da8
Jacob Floyd <cognifloyd@gmail.com>
2024-12-19 11:07:00 -0600

    Use file path to calculate group instead of test id
    This way each file's tests will run together.

--- conftest.py
+++ conftest.py
@@ -11,8 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import hashlib
+from typing import Iterable, List, Sequence
 
-
+from _pytest import nodes
 from _pytest.config import create_terminal_writer
 
 
@@ -31,15 +33,23 @@ def pytest_unconfigure(config):
 # TODO: Remove everything below here when we get rid of the Makefile
 # everything below this is based on (MIT licensed) by mark-adams:
 # https://github.com/mark-adams/pytest-test-groups/blob/5eca437ef95d23e8674b9e8765ce16005159d334/pytest_test_groups/__init__.py
+# with some inspiration from (MIT licensed) by Adam Gleave:
+# https://github.com/AdamGleave/pytest-shard/blob/64610a08dac6b0511b6d51cf895d0e1040d162ad/pytest_shard/pytest_shard.py
 
 
-def get_group(items, group_count, group_id):
+def get_group(items: Iterable[nodes.Node], group_count: int, group_id: int) -> Sequence[nodes.Node]:
     """Get the items from the passed in group based on group count."""
-    if not (1 <= group_id <= group_count):
+    if not (0 <= group_id < group_count):
         raise ValueError("Invalid test-group argument")
 
-    start = group_id - 1
-    return items[start:len(items):group_count]
+    def get_group_id(node: nodes.Node) -> int:
+        # use the file path instead of node id, so all tests in a file run together.
+        path_bytes = str(node.path).encode()
+        digest_bytes = hashlib.sha256(path_bytes).digest()
+        digest = int.from_bytes(digest_bytes, "little")
+        return digest % group_count
+
+    return [item for item in items if get_group_id(item) == group_id]
 
 
 def pytest_addoption(parser):
@@ -50,7 +60,7 @@ def pytest_addoption(parser):
                     help='The group of tests that should be executed')
 
 
-def pytest_collection_modifyitems(session, config, items):
+def pytest_collection_modifyitems(session, config, items: List[nodes.Node]):
     group_count = config.getoption('test-group-count')
     group_id = config.getoption('test-group')
 
