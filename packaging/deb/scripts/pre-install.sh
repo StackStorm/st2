8b411233110642b9989edf35cdd930806c86b52c
Jacob Floyd <cognifloyd@gmail.com>
2025-01-07 13:57:48 -0600

    Packaging: */pre-install.sh harmonize RESET_PERMS for rpm/deb

--- packaging/deb/scripts/pre-install.sh
+++ packaging/deb/scripts/pre-install.sh
@@ -23,9 +23,9 @@ RESET_PERMS=$(cat <<EHD | sed 's/\s\+/ /g'
 ug+rw root:_packsgroup /opt/stackstorm/packs
 ug+rw root:_packsgroup /usr/share/doc/st2/examples
 ug+rw root:_packsgroup /opt/stackstorm/virtualenvs
-755 _st2user:root      /opt/stackstorm/configs
-755 _st2user:root      /opt/stackstorm/overrides
-755 _st2user:root      /opt/stackstorm/exports
+755   _st2user:root    /opt/stackstorm/configs
+755   _st2user:root    /opt/stackstorm/exports
+755   _st2user:root    /opt/stackstorm/overrides
 EHD
 )
 
