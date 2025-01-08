4ca7ec9f060e77b0f9cdf20070660f3132e1f2bd
Jacob Floyd <cognifloyd@gmail.com>
2025-01-07 18:40:29 -0600

    Packaging: Improve rpm scriptlet / deb maintainer script docs
    Document all scripts, and update deb docs to better describe
    when various args might be used and add some missing variants.

--- packaging/rpm/scripts/post-install.sh
+++ packaging/rpm/scripts/post-install.sh
@@ -1,5 +1,11 @@
 set -e
 
+# This %post scriptlet gets one argument, $1, the number of packages of
+# this name that will be left on the system when this script completes. So:
+#   * on install: $1 = 1
+#   * on upgrade: $1 > 1
+# https://docs.fedoraproject.org/en-US/packaging-guidelines/Scriptlets/#_syntax
+
 # from %post in st2-packages.git/packages/st2/rpm/st2.spec
 %service_post st2actionrunner st2api st2stream st2auth st2notifier st2workflowengine
 %service_post st2rulesengine st2timersengine st2sensorcontainer st2garbagecollector
--- packaging/rpm/scripts/post-remove.sh
+++ packaging/rpm/scripts/post-remove.sh
@@ -1,5 +1,11 @@
 set -e
 
+# This %postun scriptlet gets one argument, $1, the number of packages of
+# this name that will be left on the system when this script completes. So:
+#   * on upgrade:   $1 > 0
+#   * on uninstall: $1 = 0
+# https://docs.fedoraproject.org/en-US/packaging-guidelines/Scriptlets/#_syntax
+
 # from %postun in st2-packages.git/packages/st2/rpm/st2.spec
 %service_postun st2actionrunner %{worker_name} st2api st2stream st2auth st2notifier st2workflowengine
 %service_postun st2rulesengine st2timersengine st2sensorcontainer st2garbagecollector
