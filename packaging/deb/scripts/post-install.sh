1efa769b47839057ce52b231a90066c740029d54
Jacob Floyd <cognifloyd@gmail.com>
2025-01-08 15:39:07 -0600

    Packaging: Build st2 virtualenv in post-install.sh scripts

--- packaging/deb/scripts/post-install.sh
+++ packaging/deb/scripts/post-install.sh
@@ -83,8 +83,15 @@ systemd_enable_and_restart() {
 		fi
 }
 
+rebuild_st2_venv() {
+    /opt/stackstorm/install/st2.pex
+}
+
 case "$1" in
     configure)
+        # Fail install if venv build fails
+        rebuild_st2_venv || exit $?
+
         systemd_enable_and_restart ${_ST2_SERVICES}
     ;;
     abort-upgrade|abort-remove|abort-deconfigure)
